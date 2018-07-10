971be6e078751d0bf9acdd0240bea784bb904216
Micaël Bergeron <mbergeron@gitlab.com>
2018-07-10 16:01:10 -0400

    refactor the tests

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='meltano-common',
       version='0.1.0-alpha0',
@@ -7,12 +7,13 @@ setup(name='meltano-common',
       author='Micaël Bergeron',
       author_email='mbergeron@gitlab.com',
       url='https://gitlab.com/meltano/meltano',
-      packages=['elt'],
+      packages=find_packages(),
       install_requires=[
           "psycopg2>=2.7.4",
           "configparser",
           "SQLAlchemy",
           "pandas",
-          "pyarrow"
+          "pyarrow",
+          "pyyaml"
       ]
      )
