11852e7856796a3322bd5c9ad07600a1a5bacf74
Micaël Bergeron <mbergeron@gitlab.com>
2018-07-10 11:47:44 -0400

    declare `meltano-common` deps

--- setup.py
+++ setup.py
@@ -2,10 +2,15 @@
 from distutils.core import setup
 
 setup(name='meltano-extract-common',
-      version='0.1.0-dev',
+      version='0.1.0-alpha0',
       description='Meltano Extract framework.',
       author='Micaël Bergeron',
       author_email='mbergeron@gitlab.com',
       url='https://gitlab.com/meltano/meltano',
       packages=['elt'],
+      install_requires=[
+          "configparser",
+          "SQLAlchemy",
+          "psycopg2>=2.7.4"
+      ]
      )
