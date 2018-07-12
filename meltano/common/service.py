827b554dacba6b071bd284520d4c8dcb77f65166
Micaël Bergeron <mbergeron@gitlab.com>
2018-07-12 16:38:04 -0400

    add register_extractor

--- meltano/common/service.py
+++ meltano/common/service.py
@@ -2,14 +2,23 @@ from meltano.schema.serializers import MeltanoSerializer
 
 class MeltanoService:
     loaders = dict()
+    extractors = dict()
 
     @classmethod
     def register_loader(cls, loader_uri, loader_class):
-        # TODO test if class is correct
+        # TODO test if loader_class > MeltanoLoader
         cls.loaders[loader_uri] = loader_class
+
+    @classmethod
+    def register_extractor(cls, extractor_uri, extractor_class):
+        # TODO test if extractor_class > MeltanoExtractor
+        cls.extractors[extractor_uri] = extractor_class
 
     def create_loader(self, loader_uri, stream):
         return MeltanoService.loaders[loader_uri](stream, self)
+
+    def create_extractor(self, extractor_uri, stream):
+        return MeltanoService.extractors[extractor_uri](stream, self)
 
     def load_schema(self, schema_name, schema_file):
         serializer = MeltanoSerializer(schema_name)
