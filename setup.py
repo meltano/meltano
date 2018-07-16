e48f498658eece7f847e65d6bb2bc5c6e5943c64
Alex Zamai <zamaialexander@gmail.com>
2018-07-16 20:43:27 +0300

    removed dependencies from pipfile since they should be in the setup py

--- setup.py
+++ setup.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-setup(name='meltano-common',
-      version='0.2.0-dev',
-      description='Meltano shared module.',
-      author='Meltano Team & Contributors',
-      author_email='meltano@gitlab.com',
-      url='https://gitlab.com/meltano/meltano',
-      packages=[
-          'meltano.schema',
-          'meltano.stream',
-          'meltano.common',
-      ],
-      install_requires=[
-          "psycopg2>=2.7.4",
-          "configparser",
-          "SQLAlchemy",
-          "pandas",
-          "pyarrow",
-          "pyyaml",
-          "requests",
-          "attrs"
-      ]
-     )
+setup(
+    name='meltano-common',
+    version='0.2.0-dev',
+    description='Meltano shared module.',
+    author='Meltano Team & Contributors',
+    author_email='meltano@gitlab.com',
+    url='https://gitlab.com/meltano/meltano',
+    packages=[
+        'meltano.schema',
+        'meltano.stream',
+        'meltano.common',
+    ],
+    install_requires=[
+        "psycopg2>=2.7.4",
+        "configparser",
+        "SQLAlchemy",
+        "pandas",
+        "pyarrow",
+        "pyyaml",
+        "requests",
+        "attrs"
+    ]
+)
