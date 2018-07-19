4a194b9c76c09f728f86f6dbb0191354697213df
Micaël Bergeron <mbergeron@gitlab.com>
2018-07-19 16:27:39 -0400

    bump to 0.3.0-dev3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='meltano-common',
-    version='0.3.0-dev2',
+    version='0.3.0-dev3',
     description='Meltano shared module.',
     author='Meltano Team & Contributors',
     author_email='meltano@gitlab.com',
