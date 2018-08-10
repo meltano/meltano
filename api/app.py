57173525ddcc88c57009e39e331b9626e0981263
Alex Zamai <zamaialexander@gmail.com>
2018-08-10 18:30:34 +0300

    Fixed import from the parent directory

--- api/app.py
+++ api/app.py
@@ -1,25 +1,22 @@
+import sys
 import os
 import logging
 import datetime
 from logging.handlers import RotatingFileHandler
+
 from flask import Flask, request
 from flask import jsonify
-
-import sys, os
-sys.path.append(os.path.abspath(os.path.join('..', 'meltano')))
-
-from meltano.cli import extract
-
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))  # needed to import from the parent folder
+from cli import extract
 from external_connector import ExternalConnector
 
-app = Flask(__name__) 
+app = Flask(__name__)
 
 app.config.from_object('config')
 if os.environ['FLASK_ENV'] == 'development':
-  CORS(app)
+    CORS(app)
 
 db = SQLAlchemy(app)
 
@@ -31,31 +28,36 @@ logger.addHandler(handler)
 now = str(datetime.datetime.utcnow().strftime('%b %d %Y %I:%M:%S:%f'))
 logger.warning('Melt started at: {}'.format(now))
 
+
 @app.before_request
 def before_request():
-  logger.info('[{}] request: {}'.format(request.remote_addr, now))
+    logger.info('[{}] request: {}'.format(request.remote_addr, now))
+
 
 from controllers import projects
 from controllers import repos
 from controllers import settings
 from controllers import sql
 from controllers import orchestrations
+
 app.register_blueprint(projects.bp)
 app.register_blueprint(repos.bp)
 app.register_blueprint(settings.bp)
 app.register_blueprint(sql.bp)
 app.register_blueprint(orchestrations.bp)
 
+
 @app.route("/")
 def hello():
-  return jsonify({"hello": 1})
+    return jsonify({"hello": 1})
+
 
 @app.route("/drop_it_like_its_hot")
 def reset_db():
-  try:
-    db.drop_all()
-  except sqlalchemy.exc.OperationalError as err:
-    logging.error("Failed drop database.")
-    
-  db.create_all()
-  return jsonify({"dropped_it":"like_its_hot"})
+    try:
+        db.drop_all()
+    except sqlalchemy.exc.OperationalError as err:
+        logging.error("Failed drop database.")
+
+    db.create_all()
+    return jsonify({"dropped_it": "like_its_hot"})
