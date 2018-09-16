d496b76441c64804c3eb07a57ad56c4a20fdad2c
Jacob Schatz <jschatz@gitlab.com>
2018-09-16 14:18:09 +0000

    Revert "Merge branch 'docker' into 'master'"
    This reverts merge request !52

---
+++ api/app.py
@@ -0,0 +1,62 @@
+import sqlalchemy
+import sys
+import os
+import logging
+import datetime
+from logging.handlers import RotatingFileHandler
+
+from flask import Flask, request
+from flask import jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))  # needed to import from the parent folder
+from external_connector import ExternalConnector
+
+app = Flask(__name__)
+
+meltano_home_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+meltano_model_path = os.path.join(meltano_home_path, 'model')
+meltano_load_path = os.path.join(meltano_home_path, 'load')
+meltano_transform_path = os.path.join(meltano_home_path, 'transform')
+
+app.config.from_object('config')
+if os.environ['FLASK_ENV'] == 'development':
+    CORS(app)
+
+db = SQLAlchemy(app)
+
+connector = ExternalConnector()
+
+logger = logging.getLogger('melt_logger')
+handler = RotatingFileHandler(app.config['LOG_PATH'], maxBytes=2000, backupCount=10)
+logger.addHandler(handler)
+now = str(datetime.datetime.utcnow().strftime('%b %d %Y %I:%M:%S:%f'))
+logger.warning(f'Melt started at: {now}')
+
+
+@app.before_request
+def before_request():
+    logger.info(f'[{request.remote_addr}] request: {now}')
+
+@app.errorhandler(500)
+def internal_error(exception):
+    logger.info(f'[{request.remote_addr}] request: {now}, error: {exception}')
+    return render_template('500.html'), 500
+
+
+from controllers import projects
+from controllers import repos
+from controllers import settings
+from controllers import sql
+from controllers import orchestrations
+
+app.register_blueprint(projects.bp)
+app.register_blueprint(repos.bp)
+app.register_blueprint(settings.bp)
+app.register_blueprint(sql.bp)
+app.register_blueprint(orchestrations.bp)
+
+
+@app.route("/")
+def hello():
+    return jsonify({"hello": 1})
---
+++ api/controllers/join.py
@@ -0,0 +1,56 @@
+from enum import Enum
+from models.data import View, Dimension
+from .substitution import Substitution
+
+class JoinType(Enum):
+  left_join = 'left'
+  inner_join = 'inner'
+  full_outer_join = 'full_outer'
+  cross_join = 'cross'
+
+class Join():
+  def __init__(self, join, view, table):
+    self.table = table
+    self.view = view
+    join_type = self.get_join_type(join)
+    join_sql = self.join_sql(join, table)
+    self.sql = ' '.join([join_type.value, join_sql])
+
+  def get_join_type(self, join):
+    if not 'type' in join.settings:
+      return JoinType.left_join
+    else:
+      type_of_join = join.settings['type'].lower()
+      if type_of_join == JoinType.inner_join.value:
+        return JoinType.inner_join
+      elif type_of_join == JoinType.full_outer_join.value:
+        return JoinType.full_outer_join
+      elif type_of_join == JoinType.cross_join.value:
+        return JoinType.cross_join
+
+  def join_sql(self, join, table):
+    sql = join.settings['sql_on']
+    results = Substitution.placeholder_match(sql)
+    related_view = View.query.filter(View.name == join.name).first()
+    def dimension_actual_name(result):
+      (joined_view, joined_dimension) = result.split('.')
+      queried_view = self.view
+      if queried_view.name != joined_view:
+        queried_view = related_view
+      _dimensions = Dimension.query.filter(Dimension.name == joined_dimension).all()
+      queried_dimension = Dimension.query\
+        .join(View, Dimension.view_id == View.id)\
+        .filter(Dimension.name == joined_dimension)\
+        .filter(View.id == queried_view.id)\
+        .first()
+
+      return queried_dimension.table_column_name
+
+    inner_results = list(map(dimension_actual_name, results.inner))
+
+    Substitution.variable_replace(results)
+
+    for i, result in enumerate(results.outer):
+      sql = sql.replace(result, inner_results[i])
+    table_name = related_view.settings['sql_table_name']
+    return f'{table_name} AS {join.name} ON {sql}'
