b846fe55c1eba5f4fd1f8005d1c7841e9e932c25
Thomas La Piana <tlapiana@gitlab.com>
2018-10-03 14:49:34 -0500

    added the generate_statefile function

--- extract/singer/marketo/marketo_utils.py
+++ extract/singer/marketo/marketo_utils.py
@@ -1,4 +1,5 @@
 #!/usr/bin/ python3
+from datetime import datetime
 import json
 import logging
 from os import environ as env
@@ -35,6 +36,33 @@ class MarketoUtils(object):
             logging.info('Keyfile written successfully.')
         else:
             return keyfile_dict
+
+    def generate_statefile(self, output_file: str='state.json',
+                           start_time: str=datetime.utcnow().isoformat()):
+        """
+        Generate a statefile for tap-marketo for incremental loading.
+        """
+
+        endpoint_list = ['activity_types',
+                         'activities',
+                         'leads',
+                         'lists']
+
+        # Expected start_time format: 2018-01-01T01:00:00.000000
+        input_format = '%Y-%m-%dT%H:%M:%S.%f'
+        output_format = '%Y-%m-%dT%H:%M:%SZ'
+        parsed_datetime = datetime.strptime(start_time, input_format)
+        formatted_start_time = datetime.strftime(parsed_datetime, output_format)
+
+        statefile_dict = {endpoint: formatted_start_time
+                          for endpoint in endpoint_list}
+
+        if output_file:
+            with open(output_file, 'w') as statefile:
+                json.dump(statefile_dict, statefile, sort_keys=True, indent=2)
+            logging.info('Statefile written successfully.')
+        else:
+            return statefile_dict
 
 
 if __name__ == '__main__':
