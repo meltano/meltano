c89d27e7730122f22d140cdde95d3235b2729dc8
mbergeron <mbergeron@gitlab.com>
2018-10-03 10:33:10 -0400

    moving back stuff into the cli
      - meltano schema create

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 import setuptools
 
 with open("README.md", "r") as fh:
@@ -8,8 +7,8 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="meltano",
     version="0.0.1",
-    author="Meltano Team",
-    author_email="info@gitlab.com",
+    author='Meltano Team & Contributors',
+    author_email="meltano@gitlab.com",
     description="Meltano",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -58,6 +57,11 @@ setuptools.setup(
             'flask-cors',
             'flask-sqlalchemy',
             'flask',
+        ]
+    },
+    entry_points={
+        'console_scripts': [
+            "meltano = meltano.cli:main"
         ]
     }
 )
---
+++ src/meltano/cli/params.py
@@ -0,0 +1,34 @@
+import functools
+import click
+
+from meltano.support.utils import pop_all
+from meltano.support.db import DB
+
+
+def db_options(func):
+    @click.option('-H', '--host',
+                  envvar='PG_ADDRESS',
+                  default='localhost',
+                  help="Database address.")
+
+    @click.option('-p', '--port',
+                  type=int,
+                  envvar='PG_PORT',
+                  default=5432)
+
+    @click.option('-d', '-db', 'database',
+                  envvar='PG_DATABASE',
+                  help="Database to import the data to.")
+
+    @click.option('-u', '--username',
+                  envvar='PG_USERNAME',
+                  default=lambda: os.getenv('USER', ''),
+                  help="Specifies the user to connect to the database with.")
+
+    @click.password_option(envvar='PG_PASSWORD')
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        config = pop_all(("host", "port", "database", "username", "password"), kwargs)
+        DB.setup(**config)
+        return func(*args, **kwargs)
+    return wrapper
