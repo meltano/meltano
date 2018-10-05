54384724a49dc19306846e4e2259afd26d842f3f
mbergeron <mbergeron@gitlab.com>
2018-10-05 10:00:08 -0400

    fix the successive runs

--- orchestrate/singer/meltano-singer.py
+++ orchestrate/singer/meltano-singer.py
@@ -6,10 +6,10 @@ from pathlib import Path
 
 import click
 
-VENVS_DIR = Path("/venvs")
+VENVS_DIR = Path(os.getenv("SINGER_VENVS_DIR", "/venvs"))
 RUN_DIR = Path(os.getenv("SINGER_RUN_DIR"))
-TAP_CONFIG_DIR = Path("/etc/singer/tap")
-TARGET_CONFIG_DIR = Path("/etc/singer/target")
+TAP_CONFIG_DIR = Path(os.getenv("SINGER_TAP_CONFIG_DIR", "/etc/singer/tap"))
+TARGET_CONFIG_DIR = Path(os.getenv("SINGER_TARGET_CONFIG_DIR", "/etc/singer/target"))
 
 
 def envsubst(src: Path, dst: Path):
@@ -19,7 +19,7 @@ def envsubst(src: Path, dst: Path):
 
 
 def file_has_data(file: Path):
-    return file.exists() and file.stat().st_size() > 0
+    return file.exists() and file.stat().st_size > 0
 
 
 class SingerRunner():
@@ -34,6 +34,9 @@ class SingerRunner():
         'state': RUN_DIR / "new_state.json"
     }
 
+    def __init__(self, **config):
+        self.tap_output_path = config.get("tap_output_path")
+
 
     def exec_path(self, name) -> Path:
         return VENVS_DIR / name / "bin" / name
@@ -42,68 +45,74 @@ class SingerRunner():
     def prepare(self, tap: str, target: str):
         config_files = {
             self.tap_files['config']: TAP_CONFIG_DIR / "{}.config.json".format(tap),
-            self.target_files['config']: TARGET_CONFIG_DIR / "{}.config.json".format(target)
+            self.tap_files['catalog']: TAP_CONFIG_DIR / "{}.properties.json".format(tap),
+            self.target_files['config']: TARGET_CONFIG_DIR / "{}.config.json".format(target),
         }
-
-        self.tap_files['catalog'].unlink()
-        self.tap_files['catalog'].symlink_to(TAP_CONFIG_DIR / "{}.properties.json".format(tap))
 
         for dst, src in config_files.items():
             envsubst(src, dst)
 
 
     def invoke(self, tap: str, target: str):
-       tap_args = [
-           self.exec_path(tap),
-           "-c", self.tap_files['config'],
-           "--catalog", self.tap_files['catalog'],
-       ]
+        tap_args = [
+            self.exec_path(tap),
+            "-c", self.tap_files['config'],
+            "--catalog", self.tap_files['catalog'],
+        ]
 
-       if file_has_data(self.tap_files['state']):
-           tap_args += ["--state", self.tap_files['state']]
+        if file_has_data(self.tap_files['state']):
+            tap_args += ["--state", self.tap_files['state']]
 
-       target_args = [
-           self.exec_path(target),
-           "-c", self.target_files['config'],
-       ]
+        tee_args = [
+            "tee",
+        ]
 
-       p_target = subprocess.Popen(map(str, target_args),
-                                   stdin=subprocess.PIPE,
-                                   stdout=self.target_files['state'].open("w+"))
+        if self.tap_output_path:
+            tee_args += [self.tap_output_path]
 
-       p_tap = subprocess.Popen(map(str, tap_args),
-                                stdout=p_target.stdin)
+        target_args = [
+            self.exec_path(target),
+            "-c", self.target_files['config'],
+        ]
 
-       # Polling would probably be better
-       tap_code = p_tap.wait()
-       target_code = p_target.wait()
+        p_target = subprocess.Popen(map(str, target_args),
+                                    stdin=subprocess.PIPE,
+                                    stdout=self.target_files['state'].open("w+"))
 
-       if tap_code != 0:
-           raise "Tap exited with {}".format(tap_code)
+        p_tee = subprocess.Popen(tee_args,
+                                 stdin=subprocess.PIPE,
+                                 stdout=p_target.stdin)
 
-       if target_code != 0:
-           raise "Target exited with {}".format(target_code)
+        p_tap = subprocess.Popen(map(str, tap_args),
+                                 stdout=p_tee.stdin)
 
+        # Polling would probably be better
+        tap_code = p_tap.wait()
+        target_code = p_target.wait()
+
+        if tap_code != 0:
+            raise "Tap exited with {}".format(tap_code)
+
+        if target_code != 0:
+            raise "Target exited with {}".format(target_code)
 
     def run(self, tap: str, target: str):
         self.prepare(tap, target)
         self.invoke(tap, target)
         self.bookmark()
 
-
     def bookmark(self):
         if not file_has_data(self.target_files['state']):
             return
 
-        self.target_files['state'].replate(self.tap_files['state'])
-
+        self.target_files['state'].replace(self.tap_files['state'])
 
 
 @click.command()
 @click.argument("tap")
 @click.argument("target")
 def main(tap, target):
-    runner = SingerRunner()
+    runner = SingerRunner(tap_output_path=os.getenv("TAP_OUTPUT"))
     runner.run(tap, target)
 
 
