38f90050529d2ffadb93837dbc0ea4d7ef1913ad
mbergeron <mbergeron@gitlab.com>
2018-10-11 12:39:02 -0400

    fixing the extract arguments order

--- src/meltano/cli/extract.py
+++ src/meltano/cli/extract.py
@@ -6,8 +6,8 @@ from meltano.support.runner.singer import SingerRunner
 
 @cli.command()
 @db_options
-@click.argument('extractor_name')
 @click.argument('job_id', envvar="MELTANO_JOB_ID")
+@click.argument('extractor_name')
 @click.option('--loader_name', help="Which loader should be used in this extraction", required=True)
 @click.option('--tap-output', help="Output tap stream to this file.")
 def extract(job_id, extractor_name, loader_name, tap_output):
