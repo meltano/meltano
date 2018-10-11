d0135d5a731da9c8ea0ed9b1b42a1eadd0e38a48
Micaël Bergeron <mbergeron@gitlab.com>
2018-10-11 20:23:46 +0000

    Fix singer_runner

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -40,7 +40,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://gitlab.com/meltano/meltano",
     package_dir={'': 'src'},
-    packages=find_packages(),
+    packages=find_namespace_packages(where='src'),
     package_data={
         'meltano.api': [
             'node_modules',
