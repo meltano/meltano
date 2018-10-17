5018eed66f80984377a00a66607d3013a196bd04
Jacob Schatz <jschatz@gitlab.com>
2018-10-17 14:59:39 -0400

    Clean up based on review

--- src/meltano/cli/initialize.py
+++ src/meltano/cli/initialize.py
@@ -10,27 +10,24 @@ ALL = "all"
 
 initialize_file = os.path.join(os.path.dirname(__file__), "initialize.yml")
 
-
 @cli.command()
 @click.argument("project_name")
-def initialize(project_name):
+def init(project_name):
     project = project_name.lower()
-    with open(initialize_file) as f:
-        data = yaml.load(f)
-        try:
-            os.mkdir(project)
-        except Exception as e:
-            raise e
-        created_output(project, "", True)
-        for key in data.keys():
-            if key.startswith("/"):
-                current_dir = key[1:]
-                os.mkdir(os.path.join(".", project, current_dir))
-                created_output(project, current_dir, False, True)
-            else:
-                with open(join_with_project_base(project, key), "w") as f:
-                    f.write(data.get(key))
-                    created_output(project, key, False, True)
+    default_project_yaml = yaml.load(open(initialize_file))
+    try:
+        os.mkdir(project)
+    except Exception as e:
+        click.secho(f"Directory {project} already exists!", fg="red")
+        raise click.Abort()
+
+    project_echo(project, "", True)
+    for name in default_project_yaml.keys():
+        if name.startswith("/"):
+            create_dir(project, name)
+        else:
+            create_file(project, name, default_project_yaml[name])
+
     click.secho(f"🚀\t{project}", fg="green", nl=False)
     click.echo(" has been created. Next steps:")
     click.secho("1.\tRun", nl=False)
@@ -39,8 +36,17 @@ def initialize(project_name):
     click.secho("\thttps://gitlab.com/meltano/meltano/blob/master/README.md", fg="red")
     click.echo("3.\tEdit the meltano.yml file.")
 
+def create_file(project, name, content):
+    with open(join_with_project_base(project, name), "w") as f:
+        f.write(content)
+        project_echo(project, name, False, True)
 
-def created_output(project, filename="", star=False, check=False):
+def create_dir(project, name):
+    current_dir = name[1:]
+    os.mkdir(os.path.join(".", project, current_dir))
+    project_echo(project, current_dir, False, True)
+
+def project_echo(project, filename="", star=False, check=False):
     star = "⭐" if star else ""
     check = "✅" if check else ""
     click.secho(f"{star}{check}\tCreated", fg="blue", nl=False)
