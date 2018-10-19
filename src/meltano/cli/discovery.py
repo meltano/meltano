2fe846deac508026253f13288c26ceb5f3d6ce83
Jacob Schatz <jschatz@gitlab.com>
2018-10-19 10:34:39 -0400

    Refactor discovery service

--- src/meltano/cli/discovery.py
+++ src/meltano/cli/discovery.py
@@ -3,27 +3,10 @@ import json
 import click
 from urllib.parse import urlparse
 from . import cli
-
-EXTRACTORS = "extractors"
-LOADERS = "loaders"
-ALL = "all"
-
-discovery_file = os.path.join(os.path.dirname(__file__), "discovery.json")
-
+from ..support.plugin_discovery_service import PluginDiscoveryService as pds
 
 @cli.command()
-@click.argument("plugin_type", type=click.Choice([EXTRACTORS, LOADERS, ALL]))
+@click.argument("plugin_type", type=click.Choice([pds.EXTRACTORS, pds.LOADERS, pds.ALL]))
 def discover(plugin_type):
-    with open(discovery_file) as f:
-        data = json.load(f)
-
-        if plugin_type == ALL:
-            list_discovery(EXTRACTORS, data)
-            list_discovery(LOADERS, data)
-        else:
-            list_discovery(plugin_type, data)
-
-
-def list_discovery(discovery, data):
-    click.echo(click.style(discovery.title(), fg="green"))
-    click.echo("\n".join(data.get(discovery).keys()))
+    discover_service = pds()
+    discover_service.discover(plugin_type)
