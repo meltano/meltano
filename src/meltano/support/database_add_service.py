d94ee1a753b8c6a2e53391c90ff5312ced5aae0b
Jacob Schatz <jschatz@gitlab.com>
2018-10-24 17:37:02 -0400

    Fixed based on feedback.

--- src/meltano/support/database_add_service.py
+++ src/meltano/support/database_add_service.py
@@ -5,26 +5,26 @@ import yaml
 
 class DatabaseAddService:
     def __init__(self):
-        self.env_file = os.path.join("./", ".meltano")
+        self.meltano_secret_dir = os.path.join("./", ".meltano")
 
-    def env_param(self, name, upper=True):
+    def environmentalize(self, name, upper=True):
         s = re.sub(r"[^\w\s]", "", name)
         s = re.sub(r"\s+", "_", s)
         return s.upper() if upper else s.lower()
 
-    def get_env_file_path(self, **kwargs):
+    def get_db_vars_file_path(self, **kwargs):
         return os.path.join(
-            self.env_file, f".database_{kwargs['root_name'].lower()}.yml"
+            self.meltano_secret_dir, f".database_{kwargs['root_name'].lower()}.yml"
         )
 
     def add_additional_kwargs(self, **kwargs):
-        root_name = self.env_param(kwargs["name"])
+        root_name = self.environmentalize(kwargs["name"])
         kwargs["root_name"] = root_name
-        kwargs["name"] = self.env_param(kwargs["name"], False)
+        kwargs["name"] = self.environmentalize(kwargs["name"], False)
         return kwargs
 
     def add(self, **kwargs):
         kwargs = self.add_additional_kwargs(**kwargs)
 
-        with open(self.get_env_file_path(**kwargs), "w") as f:
+        with open(self.get_db_vars_file_path(**kwargs), "w") as f:
             f.write(yaml.dump(kwargs))
