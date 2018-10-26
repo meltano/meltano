5b620f28745383eb584a50871a40afbe18ccac25
Jacob Schatz <jschatz@gitlab.com>
2018-10-26 13:44:53 -0400

    Linted

--- src/meltano/cli/list.py
+++ src/meltano/cli/list.py
@@ -3,18 +3,21 @@ from . import cli
 from meltano.core.plugin import PluginType
 from meltano.core.plugin_discovery_service import PluginDiscoveryService
 
+
 def all_command():
     discovery_service = PluginDiscoveryService()
-    click.secho('Extractors', fg="green")
+    click.secho("Extractors", fg="green")
     click.echo("\n".join(discovery_service.list(PluginType.EXTRACTORS)))
-    click.secho('Loaders', fg="green")
+    click.secho("Loaders", fg="green")
     click.echo("\n".join(discovery_service.list(PluginType.LOADERS)))
+
 
 @cli.group(invoke_without_command=True)
 @click.pass_context
 def list(ctx):
     if ctx.invoked_subcommand is None:
         all_command()
+
 
 @list.command()
 def extractors():
@@ -26,6 +29,7 @@ def extractors():
 def loaders():
     discovery_service = PluginDiscoveryService()
     click.echo("\n".join(discovery_service.list(PluginType.LOADERS)))
+
 
 @list.command()
 def all():
