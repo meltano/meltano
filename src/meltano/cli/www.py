beb25d223a36a2e22d368a9e2bb1a85f1edb3330
Jacob Schatz <jschatz@gitlab.com>
2018-11-02 06:32:15 -0400

    Linted

--- src/meltano/cli/www.py
+++ src/meltano/cli/www.py
@@ -5,17 +5,19 @@ import click
 from . import cli
 from meltano.api import app
 
+
 @cli.command()
-@click.option('--debug/--no-debug', default=True, help='Run webserver in debug mode')
-@click.option('--port', default=5001, help='Port to run webserver on')
-@click.option('--reload/--no-reload', default=False, help='To reload the server or not on file changes')
-@click.option('--hostname', default='localhost', help='The hostname of the webserver')
+@click.option("--debug/--no-debug", default=True, help="Run webserver in debug mode")
+@click.option("--port", default=5001, help="Port to run webserver on")
+@click.option(
+    "--reload/--no-reload",
+    default=False,
+    help="To reload the server or not on file changes",
+)
+@click.option("--hostname", default="localhost", help="The hostname of the webserver")
 def www(debug, port, reload, hostname):
     # todo: run gunicorn if not in debug mode
     if debug:
-      app.run(debug=debug, use_reloader=reload,
-                port=port, host=hostname)
+        app.run(debug=debug, use_reloader=reload, port=port, host=hostname)
     else:
-      pass
-
-
+        pass
