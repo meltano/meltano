0984e8ff5b9cb772649a736f4160a17e494bf07d
mbergeron <mbergeron@gitlab.com>
2018-11-02 15:35:44 -0400

    use `python -m venv` instead of `virtualenv`

--- src/meltano/core/venv_service.py
+++ src/meltano/core/venv_service.py
@@ -9,7 +9,7 @@ class VenvService:
     def create(self, namespace="", name=""):
         venv_path = self.project.venvs_dir(namespace, name)
         run_venv = subprocess.run(
-            ["virtualenv", venv_path],
+            ["python", "-m", "venv", venv_path],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             universal_newlines=True,
