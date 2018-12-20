fdb573d2e735d5b175a5d8dc9789afc69914099f
Jacob Schatz <jschatz@gitlab.com>
2018-12-20 05:04:14 -0500

    linted again

--- src/meltano/api/controllers/settings.py
+++ src/meltano/api/controllers/settings.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request
 from .settingshelper import SettingsHelper
+
 settingsBP = Blueprint("settings", __name__, url_prefix="/settings")
 
 
--- src/meltano/api/controllers/settingshelper.py
+++ src/meltano/api/controllers/settingshelper.py
@@ -3,6 +3,7 @@ import json
 from os.path import join
 from pathlib import Path
 
+
 class SettingsHelper:
     def __init__(self):
         self.meltano_model_path = join(os.getcwd(), "model")
@@ -10,14 +11,14 @@ class SettingsHelper:
         self.settings_file_path = Path(self.meltano_model_path).joinpath("settings.ma")
         if not self.settings_file_path.is_file():
             setting_file = open(self.settings_file_path, "w")
-            setting_file.write(json.dumps({"settings":{"connections": []}}))
+            setting_file.write(json.dumps({"settings": {"connections": []}}))
             setting_file.close()
 
     def get_connections(self):
-        settings_file = json.loads(open(self.settings_file_path, 'r').read())
+        settings_file = json.loads(open(self.settings_file_path, "r").read())
         return settings_file
 
     def set_connections(self, contents):
-        settings_file = open(self.settings_file_path, 'w')
+        settings_file = open(self.settings_file_path, "w")
         settings_file.write(json.dumps({"settings": contents}))
         settings_file.close()
