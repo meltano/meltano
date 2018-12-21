b228698996b7bf642f154c3af9aa36bc2b0ae3a1
mbergeron <mbergeron@gitlab.com>
2018-12-21 15:34:25 -0500

    Bump version: 0.3.1 → 0.3.2

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ dev_requires = [
 
 setup(
     name="meltano",
-    version="0.3.1",
+    version="0.3.2",
     author='Meltano Team & Contributors',
     author_email="meltano@gitlab.com",
     description="Meltano",
