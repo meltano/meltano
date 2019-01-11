dd583ab02451db67c093276fe1b0c47028b66fe2
derek-knox <dknox@gitlab.com>
2019-01-10 18:01:43 -0800

    table name fix and TODO marker (two table parameters existed due to refactor so I updated the names to distinguish and added a comment regarding the WIP 173-joins-in-meltano-with-pypika

--- src/meltano/api/controllers/joinhelper.py
+++ src/meltano/api/controllers/joinhelper.py
@@ -15,9 +15,10 @@ class JoinHelper:
     def get_join(join):
         return {"get_join": "get_join"}
 
-    def get_dimensions(join, table, table):
+    def get_dimensions(join, table, table_todo):
+        # TODO table_todo renamed due to terminology refactor. Unsure if we'll need these JoinHelper methods as Micaël is working on joins (173-joins-in-meltano-with-pypika)
         dimensions = AnalysisHelper.dimensions_from_names(join["dimensions"], table)
-        return AnalysisHelper.dimensions(dimensions, table)
+        return AnalysisHelper.dimensions(dimensions, table_todo)
 
     def get_table(join):
         return {}
