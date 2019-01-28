c1a4d7e6730c8585ecd5ee3b53c444fc85092600
derek-knox <dknox@gitlab.com>
2019-01-28 06:38:14 -0800

    added dashboards route and placeholder component

---
+++ src/analyze/src/components/dashboards/Dashboards.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <section class="section">
+      <div class="columns">
+        <nav class="panel column is-one-quarter">
+          <p class="panel-heading">
+            Dashboards
+          </p>
+        </nav>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Dashboards',
+}
+
+console.log('Dashboards loaded');
+</script>
+
+<style lang="scss" scoped>
+
+</style>
