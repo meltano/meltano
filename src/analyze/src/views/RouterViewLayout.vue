f55b1908ccdaa13742cee2debf7a7a1ddca8eeb7
derek-knox <dknox@gitlab.com>
2019-02-12 08:52:29 -0800

    initial RouterViewLayout addition and implementation in Repo.vue, next step is to update remaining page-level views to leverage this container component

---
+++ src/analyze/src/views/RouterViewLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="columns is-gapless">
+    <aside class="column is-one-quarter has-background-white-bis menu">
+      <slot name="left"></slot>
+    </aside>
+    <section class="column is-three-quarters">
+      <slot name="right"></slot>
+    </section>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'RouterViewLayout',
+};
+
+</script>
+
+<style lang="scss" scoped>
+</style>
