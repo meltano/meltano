17ade6084e04d9458a346847b51c10c5191eb872
mbergeron <mbergeron@gitlab.com>
2019-02-12 10:01:25 -0500

    add documentation and changelog

--- src/meltano/api/wsgi/config.py
+++ src/meltano/api/wsgi/config.py
@@ -2,6 +2,9 @@ from meltano.core.project import Project
 from meltano.api.workers import MeltanoBackgroundCompiler
 
 
+# for now Meltano only works on :5000
+# see https://gitlab.com/meltano/meltano/issues/375
+bind = ["0.0.0.0:5000"]
 project = Project.find()
 compiler = MeltanoBackgroundCompiler(project)
 
