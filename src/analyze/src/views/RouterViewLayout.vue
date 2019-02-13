d48c76175a698d9473f7c919deb166e5c6fcfeec
derek-knox <dknox@gitlab.com>
2019-02-13 10:17:20 -0800

    temp CSS placeholder for left RouterViewLayout component so CSS doesn't look broken

--- src/analyze/src/views/RouterViewLayout.vue
+++ src/analyze/src/views/RouterViewLayout.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="columns is-gapless">
     <aside class="column is-one-quarter vh-scrollable has-background-white-bis">
-      <slot name="left"></slot>
-    </aside>
+      <div class='box is-radiusless is-shadowless'>
+        <slot name="left"></slot>
+      </div>    </aside>
     <section class="column is-three-quarters vh-scrollable">
       <div class='box is-radiusless is-shadowless'>
         <slot name="right"></slot>
