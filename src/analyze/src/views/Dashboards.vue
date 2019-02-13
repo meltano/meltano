0039c482e20bf5f1d9c35996042c3d19bafe6a68
derek-knox <dknox@gitlab.com>
2019-02-13 05:23:59 -0800

    updated each view to leverage the RouterViewLayout for a consistent scaffold

--- src/analyze/src/views/Dashboards.vue
+++ src/analyze/src/views/Dashboards.vue
@@ -1,89 +1,89 @@
 <template>
-  <div class="container">
-    <section class="section">
-      <div class="columns">
+  <router-view-layout>
 
-        <nav class="panel column is-one-quarter">
-          <p class="panel-heading">
-            Dashboards
-          </p>
+    <div slot='left'>
 
-          <div class='panel-block'>
-            <a class='button is-secondary is-fullwidth'
-                @click="setAddDashboard(true)">Create Dashboard</a>
-          </div>
+      <nav class="panel">
+        <p class="panel-heading">
+          Dashboards
+        </p>
 
-          <div v-for="dashboard in dashboards"
-              class='panel-block'
-              :class="{'is-active': isActive(dashboard)}"
-              :key="dashboard.id"
-              @click="getDashboard(dashboard)">
-            <div>
-              <div>{{dashboard.name}}</div>
-              <div v-if="dashboard.id === activeDashboard.id">
-                <small>Reports ({{activeDashboard.reportIds.length}})</small>
-                <ul>
-                  <li v-for="report in reports" :key="report.id">
-                    <label for="'checkbox-' + report.id"
-                            @click="toggleReportInDashboard(report)">
-                      <input type="checkbox"
-                            :id="'checkbox-' + report.id"
-                            :checked="isReportInActiveDashboard(report)">
-                      {{report.name}}</label>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
+        <div class='panel-block'>
+          <a class='button is-secondary is-fullwidth'
+              @click="setAddDashboard(true)">Create Dashboard</a>
+        </div>
 
-        </nav>
-
-        <div class="column is-three-quarters">
-
-          <div v-if="isAddDashboard">
-            <div class="field">
-              <label class="label">Name</label>
-              <div class="control">
-                <input class="input"
-                        type="text"
-                        placeholder="Name your dashboard"
-                        v-model="saveDashboardSettings.name">
-              </div>
-            </div>
-            <div class="field">
-              <label class="label">Description</label>
-              <div class="control">
-                <textarea class="textarea"
-                          placeholder="Describe your dashboard for easier reference later"
-                          v-model="saveDashboardSettings.description"></textarea>
-              </div>
-            </div>
-            <div class="field is-grouped">
-              <div class="control">
-                <button class="button is-link" @click="saveDashboard">Save</button>
-              </div>
-            </div>
-          </div>
-
-          <div v-if="!isAddDashboard">
-            <h1><strong>{{activeDashboard.name}}</strong></h1>
-            <div v-for="report in activeDashboardReports" :key="report.id">
-              <p>{{report.name}}</p>
-              <chart :chart-type='report.chartType'
-                      :results='report.queryResults'
-                      :result-aggregates='report.queryResultAggregates'></chart>
+        <div v-for="dashboard in dashboards"
+            class='panel-block'
+            :class="{'is-active': isActive(dashboard)}"
+            :key="dashboard.id"
+            @click="getDashboard(dashboard)">
+          <div>
+            <div>{{dashboard.name}}</div>
+            <div v-if="dashboard.id === activeDashboard.id">
+              <small>Reports ({{activeDashboard.reportIds.length}})</small>
+              <ul>
+                <li v-for="report in reports" :key="report.id">
+                  <label for="'checkbox-' + report.id"
+                          @click="toggleReportInDashboard(report)">
+                    <input type="checkbox"
+                          :id="'checkbox-' + report.id"
+                          :checked="isReportInActiveDashboard(report)">
+                    {{report.name}}</label>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
 
+      </nav>
+
+    </div>
+
+    <div slot='right'>
+      <div v-if="isAddDashboard">
+        <div class="field">
+          <label class="label">Name</label>
+          <div class="control">
+            <input class="input"
+                    type="text"
+                    placeholder="Name your dashboard"
+                    v-model="saveDashboardSettings.name">
+          </div>
+        </div>
+        <div class="field">
+          <label class="label">Description</label>
+          <div class="control">
+            <textarea class="textarea"
+                      placeholder="Describe your dashboard for easier reference later"
+                      v-model="saveDashboardSettings.description"></textarea>
+          </div>
+        </div>
+        <div class="field is-grouped">
+          <div class="control">
+            <button class="button is-link" @click="saveDashboard">Save</button>
+          </div>
+        </div>
       </div>
-    </section>
-  </div>
+
+      <div v-if="!isAddDashboard">
+        <h1><strong>{{activeDashboard.name}}</strong></h1>
+        <div v-for="report in activeDashboardReports" :key="report.id">
+          <p>{{report.name}}</p>
+          <chart :chart-type='report.chartType'
+                  :results='report.queryResults'
+                  :result-aggregates='report.queryResultAggregates'></chart>
+        </div>
+      </div>
+    </div>
+
+  </router-view-layout>
 </template>
 
 <script>
 import { mapState, mapActions } from 'vuex';
 import Chart from '../components/designs/Chart';
+import RouterViewLayout from '@/views/RouterViewLayout';
 
 export default {
   name: 'Dashboards',
@@ -93,6 +93,7 @@ export default {
   },
   components: {
     Chart,
+    RouterViewLayout,
   },
   computed: {
     ...mapState('dashboards', [
