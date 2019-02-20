277fea7b88440b9355bbca509166e0660bb29903
Ben Hong <bhong@gitlab.com>
2019-02-20 11:44:56 -0500

    config (#315): Update output directory

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -32,7 +32,7 @@ module.exports = {
 		['meta', { name: 'msapplication-TileColor', content: '#da532c' }],
 		['meta', { name: 'theme-color', content: '#ffffff' }]
 	],
-	dest: './docs/public',
+	dest: 'public',
 	themeConfig: {
 		nav: [
 			{ text: 'Home', link: '/' },
