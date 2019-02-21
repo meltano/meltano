00c543b6fa55a77b313e3c42924f804ff8152a41
Ben Hong <bhong@gitlab.com>
2019-02-21 13:38:55 -0500

    feature (#241): Add option alternative for version

--- src/meltano/cli/cli.py
+++ src/meltano/cli/cli.py
@@ -8,7 +8,7 @@ warnings.filterwarnings("ignore", category=UserWarning, module="psycopg2")
 
 from meltano.core.project import Project, ProjectNotFound
 from meltano.core.utils import setup_logging
-
+from meltano import __version__
 
 LEVELS = {
     "debug": logging.DEBUG,
@@ -18,9 +18,19 @@ LEVELS = {
     "critical": logging.CRITICAL,
 }
 
+def print_version(ctx, param, value):
+    """
+    Print out Meltano version currently in use
+    """
+    if not value or ctx.resilient_parsing:
+        return
+    click.echo(__version__)
+    ctx.exit()
 
 @click.group(invoke_without_command=True)
 @click.option("--log-level", type=click.Choice(LEVELS.keys()), default="info")
+@click.option('--version', is_flag=True, callback=print_version,
+              expose_value=False, is_eager=True)
 @click.pass_context
 def cli(ctx, log_level):
     setup_logging(log_level=LEVELS[log_level])
