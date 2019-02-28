568c1fdb57bbc81e0a7266a10b6515db529c2832
Derek Knox <dknox@gitlab.com>
2019-02-28 23:45:25 +0000

    fixed Settings markup so the left panel displays like the left panel of the Files page, removed anchor tag from Files page heading as most aren't currently designed to go anywhere

--- src/analyze/src/views/Settings.vue
+++ src/analyze/src/views/Settings.vue
@@ -3,16 +3,17 @@
 
     <div slot='left'>
       <template v-for="category in categories">
-        <div :key="category.name">
-          <p class="menu-label">
-            {{category.name}}
-          </p>
-          <ul class="menu-list">
-            <router-link tag="li" :key="page.href" :to="page.href" v-for="page in category.pages">
-              <a>{{page.title}}</a>
-            </router-link>
-          </ul>
-        </div>
+
+        <!-- eslint-disable-next-line vue/require-v-for-key -->
+        <p class="menu-label">
+          {{category.name}}
+        </p>
+        <!-- eslint-disable-next-line vue/require-v-for-key -->
+        <ul class="menu-list">
+          <router-link tag="li" :key="page.href" :to="page.href" v-for="page in category.pages">
+            <a>{{page.title}}</a>
+          </router-link>
+        </ul>
       </template>
     </div>
 
