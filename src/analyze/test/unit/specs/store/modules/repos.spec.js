3c1725fb8dcd8d2faa3712fd7bf00ee1f45ec9df
derek-knox <dknox@gitlab.com>
2019-03-01 14:09:25 -0800

    improved test grouping using describe

--- src/analyze/test/unit/specs/store/modules/repos.spec.js
+++ src/analyze/test/unit/specs/store/modules/repos.spec.js
@@ -1,16 +1,26 @@
 import repos from '@/store/modules/repos';
 
-test('initial state is valid', () => {
-  const state = {
-    activeView: { is_markdown: false, file: '', populated: false },
-    loadingValidation: false,
-    loadingUpdate: false,
-    models: [],
-    validated: false,
-    navbarClicked: false,
-    errors: [],
-    files: {},
-  };
+describe('repos Vuex store', () => {
+  test('initial state is valid', () => {
+    const state = {
+      activeView: { is_markdown: false, file: '', populated: false },
+      loadingValidation: false,
+      loadingUpdate: false,
+      models: [],
+      validated: false,
+      navbarClicked: false,
+      errors: [],
+      files: {},
+    };
 
-  expect(repos.state).toMatchObject(state);
+    expect(repos.state).toMatchObject(state);
+  });
+
+  test('hasMarkdown', () => {
+    const state = {
+      activeView: { is_markdown: true, populated: true },
+    };
+
+    expect(repos.getters.hasMarkdown).toBeInstanceOf(Function);
+  });
 });
