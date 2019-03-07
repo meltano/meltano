3b9e940f56ccebd44d05dca0067c3abe687fe2d3
mbergeron <mbergeron@gitlab.com>
2019-03-07 15:18:53 -0500

    add tests

--- tests/meltano/api/controllers/test_repos.py
+++ tests/meltano/api/controllers/test_repos.py
@@ -16,3 +16,17 @@ def test_index(api, app):
     assert_has_items(payload["topics"], 1)
     assert_has_items(payload["dashboards"], 0)
     assert_has_items(payload["documents"], 1)
+
+
+def test_models(api, app):
+    with app.test_request_context():
+        res = api.get(url_for("repos.models"))
+
+    payload = res.json
+
+    # we have topic
+    assert payload.keys()
+
+    # each topics has designs
+    for topic_def in payload.values():
+        assert topic_def["designs"]
