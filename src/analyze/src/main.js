af98c00b6bdce213e05aec69390bd6270b0b1ea4
derek-knox <dknox@gitlab.com>
2019-03-11 06:26:30 -0700

    removed Vue.filter calls from main.js in favor of each component loading its needed filter(s), refactored to leverage filter directory and utils for consistent approach to filters

--- src/analyze/src/main.js
+++ src/analyze/src/main.js
@@ -53,15 +53,3 @@ new Vue({
   router,
   render: h => h(App),
 });
-
-Vue.filter('capitalize', (value) => {
-  if (!value) {
-    return '';
-  }
-  let newVal = value;
-  newVal = newVal.toString();
-  return newVal.charAt(0).toUpperCase() + newVal.slice(1);
-});
-
-Vue.filter('camelToRegular', value => value.replace(/([A-Z])/g, ' $1'));
-Vue.filter('underscoreToSpace', value => value.replace(/_/g, ' '));
