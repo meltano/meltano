8ec49f9ccfb7733539aa13c6ee77272e1c542a79
Jacob Schatz <jschatz@gitlab.com>
2019-03-12 14:40:50 -0400

    Bump version: 0.15.0 → 0.15.1

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ dev_requires = [
 
 setup(
     name="meltano",
-    version="0.15.0",
+    version="0.15.1",
     author='Meltano Team & Contributors',
     author_email="meltano@gitlab.com",
     description="Meltano",
