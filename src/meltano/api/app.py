2ba378c8ddabfd078b0876ef86e2a37e0c8b97d9
mbergeron <mbergeron@gitlab.com>
2019-03-12 16:29:58 -0400

    cherry: fix the api database being mislocated

--- src/meltano/api/app.py
+++ src/meltano/api/app.py
@@ -23,11 +23,12 @@ logger = logging.getLogger(__name__)
 
 
 def create_app(config={}):
+    project = Project.find()
+
     app = Flask(__name__)
     app.config.from_object(reload(default_config))
     app.config.update(**config)
-
-    project = Project.find()
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{project.root.joinpath('meltano.db')}"
 
     # Initial compilation
     compiler = ProjectCompiler(project)
