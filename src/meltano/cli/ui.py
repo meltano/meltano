18a5450e767a4c9aecb7c702f754cc11c46ca148
mbergeron <mbergeron@gitlab.com>
2019-03-13 14:38:08 -0400

    fix meltano/meltano image
    it can now run any meltano command:
    
    `docker run … meltano/meltano --help`

--- src/meltano/cli/ui.py
+++ src/meltano/cli/ui.py
@@ -11,13 +11,22 @@ from meltano.core.tracking import GoogleAnalyticsTracker
 @cli.command()
 @project
 @click.option("--debug/--no-debug", default=True, help="Run webserver in debug mode")
-@click.option("--port", default=5000, help="Port to run webserver on")
+@click.option("--port",
+              default=5000,
+              help="Port to run webserver on",
+              envvar="MELTANO_API_PORT",
+              type=int)
 @click.option(
     "--reload/--no-reload",
     default=True,
-    help="To reload the server or not on file changes",
+    help="To reload the server or not on file changes"
 )
-@click.option("--hostname", default="0.0.0.0", help="The hostname of the webserver")
+@click.option(
+    "--hostname",
+    default="0.0.0.0",
+    help="The hostname (or IP address) to bind on",
+    envvar="MELTANO_API_HOSTNAME",
+)
 def ui(project, debug, port, reload, hostname):
     tracker = GoogleAnalyticsTracker(project)
     tracker.track_meltano_ui()
