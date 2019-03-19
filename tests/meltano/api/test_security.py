17516db79be797cdf7ae87ff7b62b6316a57c3f5
mbergeron <mbergeron@gitlab.com>
2019-03-19 13:10:48 -0400

    add more complete tests on settings.py

--- tests/meltano/api/test_security.py
+++ tests/meltano/api/test_security.py
@@ -1,12 +1,14 @@
 import pytest
 import gitlab
-
+import urllib
 from unittest import mock
 from sqlalchemy.orm import joinedload
 from meltano.api.security import FreeUser, users
 from meltano.api.security.oauth import gitlab_token_identity, OAuthError
 from meltano.api.models.security import db, User
 from meltano.api.models.oauth import OAuth
+
+from flask import url_for
 from flask_login import current_user
 from flask_security import login_user, logout_user, AnonymousUser
 
@@ -22,13 +24,13 @@ def gitlab_client():
 
 
 class TestSecurity:
-    @pytest.mark.parametrize(
-        "authentication,current_user_cls", [(False, FreeUser), (True, AnonymousUser)]
-    )
-    def test_auth_mode(self, monkeypatch, create_app, authentication, current_user_cls):
-        app = create_app(MELTANO_AUTHENTICATION=authentication)
+    @pytest.fixture
+    def app(self, create_app):
+        return create_app(MELTANO_AUTHENTICATION=True)
+
+    def test_auth_mode(self, app):
         with app.test_request_context("/"):
-            assert isinstance(current_user._get_current_object(), current_user_cls)
+            assert isinstance(current_user._get_current_object(), AnonymousUser)
 
     @mock.patch("gitlab.Gitlab", return_value=gitlab_client())
     def test_gitlab_token_identity_creates_user(self, gitlab, app):
@@ -78,6 +80,27 @@ class TestSecurity:
             identity = gitlab_token_identity(token)
             assert identity.user == user
 
+    @pytest.mark.usefixtures("app_context")
+    def test_bootstrap(self, app, api, impersonate):
+        with app.test_request_context():
+            with impersonate(users.get_user("alice")):
+                res = api.get(url_for("security.bootstrap_app"))
+                url = urllib.parse.urlparse(res.location)
+                query = urllib.parse.parse_qs(url.query)
+
+                assert res.status_code == 302
+                assert url.netloc == "localhost"
+                assert query["auth_token"]
+
+    @pytest.mark.usefixtures("app_context")
+    def test_bootstrap_unauthenticated(self, app, api):
+        with app.test_request_context():
+            res = api.get(url_for("security.bootstrap_app"))
+            url = urllib.parse.urlparse(res.location)
+
+            assert res.status_code == 302
+            assert res.location.startswith(url_for("security.login", _external=True))
+
 
 class TestSingleUser:
     @pytest.fixture
@@ -91,3 +114,15 @@ class TestSingleUser:
         role = users.find_or_create_role("this_is_a_test")
 
         assert role in FreeUser().roles
+
+    def test_auth_mode(self, app):
+        with app.test_request_context("/"):
+            assert isinstance(current_user._get_current_object(), FreeUser)
+
+    @pytest.mark.usefixtures("app_context")
+    def test_bootstrap(self, app, api):
+        with app.test_request_context():
+            res = api.get(url_for("security.bootstrap_app"))
+
+            assert res.status_code == 302
+            assert res.location == url_for("root.default", _external=True)
