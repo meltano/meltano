f511e809f11887ddf2b5c609aa8fd439e1125d42
Jacob Schatz <jschatz@gitlab.com>
2019-03-22 09:26:39 -0400

    If no designs empty joins

--- src/analyze/src/store/modules/designs.js
+++ src/analyze/src/store/modules/designs.js
@@ -70,6 +70,7 @@ const helpers = {
       delete filters[prop].sql;
     });
 
+    if(!state.design.joins) state.design.joins = [];
     const joins = state.design.joins
       .map((j) => {
         const table = j.related_table;
