621541c49157e531ca00a8a8c4a7bf8e8d5b07f9
derek-knox <dknox@gitlab.com>
2019-03-27 09:39:24 -0700

    lint fix

--- tests/meltano/api/controllers/test_repos.py
+++ tests/meltano/api/controllers/test_repos.py
@@ -2,6 +2,7 @@ import json
 import pytest
 from flask import url_for
 
+
 def assert_has_items(entry, count):
     return len(entry["items"]) == count
 
@@ -34,14 +35,16 @@ def test_models(api, app):
 
 def test_design_read(api, app):
     with app.test_request_context():
-        res = api.get(url_for("repos.design_read", topic_name="carbon", design_name="region"))
+        res = api.get(
+            url_for("repos.design_read", topic_name="carbon", design_name="region")
+        )
 
     json_data = json.loads(res.data)
 
-    assert 'description' in json_data
-    assert 'from' in json_data
-    assert 'graph' in json_data
-    assert 'joins' in json_data
-    assert 'label' in json_data
-    assert 'name' in json_data
-    assert 'related_table' in json_data
+    assert "description" in json_data
+    assert "from" in json_data
+    assert "graph" in json_data
+    assert "joins" in json_data
+    assert "label" in json_data
+    assert "name" in json_data
+    assert "related_table" in json_data
