1b0738380ca50646352597c8f93353fa638d4440
derek-knox <dknox@gitlab.com>
2019-04-01 07:07:13 -0700

    updated nightwatch to 1.0+ (and additionally remove selenium as its no longer required or recommended), fixed initial config, still working through proper hand-off off globals to individual tests

--- src/analyze/test/e2e/specs/test.js
+++ src/analyze/test/e2e/specs/test.js
@@ -9,11 +9,8 @@ module.exports = {
     const devServer = browser.globals.devServerURL;
 
     browser
-      .url(devServer)
+      .url('http://localhost:8080')
       .waitForElementVisible('#app', 5000)
-      .assert.elementPresent('.hello')
-      .assert.containsText('h1', 'Welcome to Your Vue.js App')
-      .assert.elementCount('img', 1)
       .end();
   },
 };
