1970a5973734a9c2f6632fba8891ae79b9e6f2e3
derek-knox <dknox@gitlab.com>
2019-05-16 11:57:07 -0700

    css refactor to leverage bulma syntax vs custom style

--- src/analyze/src/views/NotFound.vue
+++ src/analyze/src/views/NotFound.vue
@@ -5,54 +5,20 @@ export default {
 </script>
 
 <template>
-  <main :class="$style.wrapper">
-    <div :class="$style.content">
-      <h1 :class="$style.h1">404</h1>
-      <p :class="$style.text">Oops... Seems this page is missing...</p>
-      <ul :class="$style.list">
-        <li><a href="/" :class="$style.button">Go back home</a></li>
-      </ul>
+  <section class="hero is-fullheight">
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <h1 class="title">
+          404
+        </h1>
+        <h2 class="subtitle">
+          Oops... Seems this page is missing...
+        </h2>
+        <a href="/" class="button">Go back home</a>
+      </div>
     </div>
-  </main>
+  </section>
 </template>
 
-<style lang="scss" module>
-@import "../scss/bulma-preset-overrides.scss";
-
-.wrapper {
-  height: calc(100vh - $navbar-height);
-  display: flex;
-  justify-content: center;
-}
-
-.content {
-  text-align: center;
-}
-
-.h1 {
-  font-size: 5rem;
-  font-weight: bold;
-  margin-top: 3rem;
-}
-
-.text {
-  font-size: 1.5rem;
-}
-
-.list {
-  margin-top: 3rem;
-}
-
-.button {
-  padding: 20px;
-  background-color: rgb(36, 102, 167);
-  color: #fff;
-  border-radius: 8px;
-  transition: background 0.2s ease-in;
-
-  &:hover {
-    color: #fff;
-    background-color: #0F3B66;
-  }
-}
+<style lang="scss">
 </style>
