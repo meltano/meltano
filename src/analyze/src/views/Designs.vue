b7fb42b4cc3e6edfad8af560bdea4c4ac864b3ee
derek-knox <dknox@gitlab.com>
2019-05-24 12:35:49 -0700

    minor copy update and code example update in addition to br tag for better code snippet readability across liquid browser width

--- src/analyze/src/views/Designs.vue
+++ src/analyze/src/views/Designs.vue
@@ -261,8 +261,8 @@ export default {
             <h3>Analyses</h3>
 
             <div class="notification is-small">
-              <p>This UI temporarily requires <code>meltano add model ...</code> to be executed from the CLI to properly display the analysis options below.</p>
-              <p>Learn how to do this <a href="https://meltano.com/docs/tutorial.html#run-elt-extract-load-transform">in the Meltano documentation</a>.</p>
+              <p>This UI temporarily requires <br/> <code>meltano add model [name_of_model]</code> to be executed from the CLI to properly display the analysis options below.</p>
+              <p>You can check for available models by running <br/> <code>meltano discover models</code> in your project and then use <br/> <code>meltano add model [name_of_model]</code> to add the models.</p>
               <p class='is-italic'>The work for replacing this temporary UI is in this <a href="https://gitlab.com/meltano/meltano/issues/651">issue</a>.</p>
             </div>
 
