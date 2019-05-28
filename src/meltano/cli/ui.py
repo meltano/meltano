641f5d5d001c04137b69e28f19a9a68a040dca7c
mbergeron <mbergeron@gitlab.com>
2019-05-28 13:57:53 -0400

    make code reload optional

--- src/meltano/cli/ui.py
+++ src/meltano/cli/ui.py
@@ -34,4 +34,4 @@ def ui(project, debug, port, reload, hostname):
     tracker.track_meltano_ui()
 
     # todo: run gunicorn if not in debug mode
-    start(project, debug=debug, use_reloader=True, port=port, host=hostname)
+    start(project, debug=debug, use_reloader=reload, port=port, host=hostname)
