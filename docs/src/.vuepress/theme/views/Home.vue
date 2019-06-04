6aafc8248359cf0f0fba6ce4f6bc755fa10834c5
Danielle Morrill <daniellemorrill@Danielles-MBP.lan>
2019-06-04 13:12:18 -0600

    resize hero boxes to give text more room

--- docs/src/.vuepress/theme/views/Home.vue
+++ docs/src/.vuepress/theme/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home" style="background:#e5e4e2; max-width:none; padding-right:0px;">
     <div class="hero" style="min-height:450px;">
     
-      <div style="float:left; text-align:left; width:50%;">
+      <div style="float:left; text-align:left; width:60%;">
         <h1 style="font-size: 36px; text-align: left; padding-top: 100px; padding-left: 35px; font-weight:400;">Open source data dashboarding</h1>
         <p style="padding-left:35px; padding-right: 20px;">Meltano is a free alternative to expensive SaaS tools. We provide an integrated workflow for modeling, extracting, loading, transforming, analyzing, notebooking, and orchestrating your data.</p> 
      
@@ -18,8 +18,8 @@
 
       </div>
 
-      <div style="float:right; text-align:left; width:50%;">
-        <img src="../assets/happy-meltano-user.jpg" alt="Happy Meltano User Says Hi" />
+      <div style="float:right; text-align:left; width:40%;">
+        <img style="float:right; max-height:360px;" src="../assets/happy-meltano-user.jpg" alt="Happy Meltano User Says Hi" />
       </div>
       
     </div>
