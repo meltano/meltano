cbd51980f3f2f69223c83d57977fb93d70d48174
Danielle Morrill <daniellemorrill@Danielles-MBP.lan>
2019-06-04 12:46:46 -0600

    test

--- docs/src/.vuepress/theme/views/Home.vue
+++ docs/src/.vuepress/theme/views/Home.vue
@@ -18,8 +18,8 @@
 
       </div>
 
-      <div style="float:right; text-align:left; width:50%; background-image: url("../assets/img/happy-meltano-user.jpg"); background-position:right; background-size: contain; background-repeat: no-repeat; height:400px;">
-         
+      <div style="float:right; text-align:left; width:50%; background-image:url("../assets/img/happy-meltano-user.jpg"); background-position:right; background-size:contain; background-repeat: no-repeat; height:400px;">
+         <p>test</p>
       </div>
       
     </div>
