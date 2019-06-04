c8069edd736f3b0832f6d50b14cabd08045ece03
Danielle Morrill <daniellemorrill@Danielles-MBP.lan>
2019-06-04 13:20:45 -0600

    attempt to disable external link icon

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: 'Meltano',
-  description: 'Open source data dashboarding software',
+  description: 'Open source data dashboarding',
   head: [
     [
       'link',
@@ -37,7 +37,7 @@ module.exports = {
     nav: [
       { text: 'Documentation', link: '/docs/' },
       { text: 'Blog', link: 'https://www.meltano.com/blog', disableIcon: true },
-      { text: 'Newsletter', link: 'https://docs.google.com/forms/d/e/1FAIpQLSdyiKeMwO2qB4hJZnjBhVExBZwPATgz6_QTJjXKLiTNPmhW9w/viewform?usp=sf_link' },
+      { text: 'Newsletter', link: 'https://docs.google.com/forms/d/e/1FAIpQLSdyiKeMwO2qB4hJZnjBhVExBZwPATgz6_QTJjXKLiTNPmhW9w/viewform?usp=sf_link',  disableIcon: true },
       { text: 'Press', link: '/press/' }
     ],
     sidebar: {
