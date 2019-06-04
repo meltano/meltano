418de8ff99ae72b7ead20f47e02dd0c27f55b94f
Danielle Morrill <daniellemorrill@Danielles-MBP.lan>
2019-06-04 14:39:28 -0600

    inline styles cleanup

--- docs/src/.vuepress/theme/views/Home.vue
+++ docs/src/.vuepress/theme/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <div class="hero homepage-cta">
     
-      <div class="hero-left" style="float:left; text-align:left; width:60%;">
+      <div class="hero-left">
         <h1 style="font-size: 36px; text-align: left; padding-top: 120px; padding-left: 35px; font-weight:400;">Open source data dashboarding</h1>
         <p style="padding-left:35px; padding-right: 20px;">Meltano is a free alternative to expensive SaaS tools. We provide an integrated workflow for modeling, extracting, loading, transforming, analyzing, notebooking, and orchestrating your data.</p> 
      
