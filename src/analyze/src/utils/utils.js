2ef58c7c3a8b05531e8f92994b176838a6ae96d3
derek-knox <dknox@gitlab.com>
2019-06-13 09:26:11 -0700

    updated inputs to consume installed extractors/loaders, updated transforms sub UI to <select> (for now), added date picker for Start Date (catchup date)

--- src/analyze/src/utils/utils.js
+++ src/analyze/src/utils/utils.js
@@ -115,4 +115,25 @@ export default {
   underscoreToSpace(value) {
     return value.replace(/_/g, ' ');
   },
+
+  // Date Utils
+  getTodayYYYYMMDD() {
+    return this.formatDateYYYYMMDD(new Date());
+  },
+
+  formatDateYYYYMMDD(date) {
+    const d = new Date(date);
+    let month = `${d.getMonth() + 1}`;
+    let day = `${d.getDate()}`;
+    const year = d.getFullYear();
+
+    if (month.length < 2) {
+      month = `0${month}`;
+    }
+    if (day.length < 2) {
+      day = `0${day}`;
+    }
+
+    return [year, month, day].join('-');
+  },
 };
