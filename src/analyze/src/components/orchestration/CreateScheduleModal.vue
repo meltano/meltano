fe1f026ca759055e29c47d1f4836587afb48bb90
derek-knox <dknox@gitlab.com>
2019-06-13 12:53:05 -0700

    updated pipelines vs create pipeline distinct UIs

--- src/analyze/src/components/orchestration/CreateScheduleModal.vue
+++ src/analyze/src/components/orchestration/CreateScheduleModal.vue
@@ -2,14 +2,110 @@
 
   <div class="modal is-active">
     <div class="modal-background" @click="close"></div>
-    <div class="modal-card">
+    <div class="modal-card is-wide">
       <header class="modal-card-head">
         <p class="modal-card-title">Create Schedule</p>
         <button class="delete" aria-label="close" @click="close"></button>
       </header>
       <section class="modal-card-body">
 
-        <p>Winning...</p>
+        <table class="table pipelines-table is-fullwidth">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th class='has-text-centered'>Extractor</th>
+              <th class='has-text-centered'>Loader</th>
+              <th class='has-text-centered'>Transform</th>
+              <th class='has-text-centered'>Interval</th>
+              <th class='has-text-centered'>Catch-up Date</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <th>
+                <p class="control is-expanded">
+                  <input class="input" type="text" placeholder="Name">
+                </p>
+              </th>
+              <td>
+                <p class="control is-expanded">
+                  <span class="select is-fullwidth">
+                    <select>
+                      <option
+                        v-for="extractor in installedPlugins.extractors"
+                        :key='extractor.name'>{{extractor.name}}</option>
+                    </select>
+                  </span>
+                </p>
+              </td>
+              <td>
+                <p class="control is-expanded">
+                  <span class="select is-fullwidth">
+                    <select>
+                      <option
+                        v-for="loader in installedPlugins.loaders"
+                        :key='loader.name'>{{loader.name}}</option>
+                    </select>
+                  </span>
+                </p>
+              </td>
+              <td>
+                <p class="control">
+                  <span class="select is-fullwidth">
+                    <select>
+                      <option>Skip</option>
+                      <option>Only</option>
+                      <option>Run</option>
+                    </select>
+                  </span>
+                </p>
+              </td>
+              <td>
+                <p class="control is-expanded">
+                  <span class="select is-fullwidth">
+                    <select>
+                      <option>None</option>
+                      <option>@once</option>
+                      <option>@hourly</option>
+                      <option>@daily</option>
+                      <option>@weekly</option>
+                      <option>@monthly</option>
+                      <option>@yearly</option>
+                    </select>
+                  </span>
+                </p>
+              </td>
+              <td>
+                <p class="control is-expanded">
+                  <Dropdown label="None" is-right-aligned is-full-width>
+                    <div class="dropdown-content" slot-scope="{ dropdownForceClose }">
+                      <a
+                        class="dropdown-item"
+                        @click="dropdownForceClose();">
+                        None
+                      </a>
+                      <hr class="dropdown-divider">
+                      <div>
+                        <div class="dropdown-item">
+                          <span class='is-size-7'>mm/dd/yyyy</span>
+                          <input
+                            type="date"
+                            id="catchup-start"
+                            name="catchup-start"
+                            :value="todaysDate"
+                            required pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}"
+                            min="2000-01-01"
+                            :max="todaysDate">
+                        </div>
+                      </div>
+                    </div>
+                  </Dropdown>
+                </p>
+              </td>
+            </tr>
+          </tbody>
+        </table>
 
       </section>
       <footer class="modal-card-foot buttons is-right">
@@ -25,8 +121,28 @@
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
+import Dropdown from '@/components/generic/Dropdown';
+
+import utils from '@/utils/utils';
+
 export default {
   name: 'CreateScheduleModal',
+  components: {
+    Dropdown,
+  },
+  created() {
+    this.$store.dispatch('plugins/getInstalledPlugins');
+  },
+  computed: {
+    ...mapState('plugins', [
+      'installedPlugins',
+    ]),
+    todaysDate() {
+      return utils.getTodayYYYYMMDD();
+    },
+  },
   methods: {
     close() {
       if (this.prevRoute) {
--- src/analyze/src/components/orchestration/PipelineSchedules.vue
+++ src/analyze/src/components/orchestration/PipelineSchedules.vue
@@ -51,102 +51,14 @@
             <th class='has-text-centered'>Loader</th>
             <th class='has-text-centered'>Transform</th>
             <th class='has-text-centered'>Interval</th>
-            <th class='has-text-centered'>Start Date</th>
+            <th class='has-text-centered'>Catch-up Date</th>
             <th class='has-text-centered'></th>
           </tr>
         </thead>
         <tbody>
 
           <template v-for="pipeline in [{id: 1}, {id: 2}]">
-            <tr :key='pipeline.id'>
-              <th>
-                <p class="control is-expanded">
-                  <input class="input" type="text" placeholder="Name">
-                </p>
-              </th>
-              <td>
-                <p class="control is-expanded">
-                  <span class="select is-fullwidth">
-                    <select>
-                      <option
-                        v-for="extractor in installedPlugins.extractors"
-                        :key='extractor.name'>{{extractor.name}}</option>
-                    </select>
-                  </span>
-                </p>
-              </td>
-              <td>
-                <p class="control is-expanded">
-                  <span class="select is-fullwidth">
-                    <select>
-                      <option
-                        v-for="loader in installedPlugins.loaders"
-                        :key='loader.name'>{{loader.name}}</option>
-                    </select>
-                  </span>
-                </p>
-              </td>
-              <td>
-                <p class="control">
-                  <span class="select is-fullwidth">
-                    <select>
-                      <option>Skip</option>
-                      <option>Only</option>
-                      <option>Run</option>
-                    </select>
-                  </span>
-                </p>
-              </td>
-              <td>
-                <p class="control is-expanded">
-                  <span class="select is-fullwidth">
-                    <select>
-                      <option>None</option>
-                      <option>@once</option>
-                      <option>@hourly</option>
-                      <option>@daily</option>
-                      <option>@weekly</option>
-                      <option>@monthly</option>
-                      <option>@yearly</option>
-                    </select>
-                  </span>
-                </p>
-              </td>
-              <td>
-                <p class="control is-expanded">
-                  <Dropdown label="None" is-right-aligned is-full-width>
-                    <div class="dropdown-content" slot-scope="{ dropdownForceClose }">
-                      <a
-                        class="dropdown-item"
-                        @click="dropdownForceClose();">
-                        None
-                      </a>
-                      <hr class="dropdown-divider">
-                      <div>
-                        <div class="dropdown-item">
-                          <span class='is-size-7'>mm/dd/yyyy</span>
-                          <input
-                            type="date"
-                            :id="`catchup-start-${pipeline.id}`"
-                            name="catchup-start"
-                            :value="todaysDate"
-                            required pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}"
-                            min="2000-01-01"
-                            :max="todaysDate">
-                        </div>
-                      </div>
-                    </div>
-                  </Dropdown>
-                </p>
-              </td>
-              <td>
-                <p class="control">
-                  <button disabled class="button is-interactive-primary is-fullwidth">
-                    Save
-                  </button>
-                </p>
-              </td>
-            </tr>
+            <div :key='pipeline.id'>Pipeline {{pipeline.id}}</div>
           </template>
 
         </tbody>
@@ -159,28 +71,12 @@
 <script>
 import { mapState } from 'vuex';
 
-import Dropdown from '@/components/generic/Dropdown';
-
-import utils from '@/utils/utils';
-
 export default {
   name: 'PipelineSchedules',
-  components: {
-    Dropdown,
-  },
-  created() {
-    this.$store.dispatch('plugins/getInstalledPlugins');
-  },
   computed: {
     ...mapState('configuration', [
       'pipelines',
     ]),
-    ...mapState('plugins', [
-      'installedPlugins',
-    ]),
-    todaysDate() {
-      return utils.getTodayYYYYMMDD();
-    },
   },
   methods: {
     createPipeline() {
