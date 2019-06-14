a37958b9d966705bd3956fe2284d4729a24b174c
derek-knox <dknox@gitlab.com>
2019-06-14 14:50:26 -0700

    updated message to be closable

--- src/analyze/src/components/Airflow.vue
+++ src/analyze/src/components/Airflow.vue
@@ -1,20 +1,23 @@
 <template>
+<div>
+
+  <ClosableMessage title='Notice'>
+    <p>You are now looking at the Airflow UI. See the <a target="_blank" href="https://airflow.apache.org/ui.html">documentation</a> for more details.</p>
+  </ClosableMessage>
+
   <div class="proxy-container">
-
-    <article class="message is-warning is-small">
-      <div class="message-header">
-        <p>Warning</p>
-      </div>
-      <div class="message-body">
-        <p>You are now looking at the Airflow UI. See the <a target="_blank" href="https://airflow.apache.org/ui.html">documentation</a> for more details.</p>
-      </div>
-    </article>
-
     <iframe class="proxy" :src="airflowUrl" />
   </div>
+
+</div>
 </template>
 <script>
+import ClosableMessage from '@/components/generic/ClosableMessage';
+
 export default {
+  components: {
+    ClosableMessage,
+  },
   computed: {
     airflowUrl() {
       return FLASK.airflowUrl;
