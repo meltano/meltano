0eb6f35daacaaa2ee24fc33ed75c1202bdc1025e
derek-knox <dknox@gitlab.com>
2019-07-09 07:11:53 -0700

    small ux add for filter count and attribute count to be consistent with results count

--- src/analyze/src/components/analyze/Design.vue
+++ src/analyze/src/components/analyze/Design.vue
@@ -48,6 +48,7 @@ export default {
       'filterOptions',
     ]),
     ...mapGetters('designs', [
+      'attributesCount',
       'currentModelLabel',
       'currentDesignLabel',
       'hasChartableResults',
@@ -357,10 +358,15 @@ export default {
             </div>
             <div class="column">
               <div class="field">
-                <label class="label">Filters</label>
+                <label class="label">
+                  <span>Filters</span>
+                  <span
+                    v-if='filtersCount > 0'
+                    class='has-text-weight-light has-text-grey-light is-size-7'>({{filtersCount}})</span>
+                </label>
                 <div class="control is-expanded">
                   <Dropdown
-                    :label="hasFilters ? `${filtersCount} Total` : 'None'"
+                    :label="hasFilters ? 'Edit' : 'None'"
                     button-classes='is-small'
                     is-full-width>
                     <div class="dropdown-content">
@@ -375,7 +381,12 @@ export default {
           </div>
 
           <div class="field">
-            <label class="label">Attributes</label>
+            <label class="label">
+              <span>Attributes</span>
+              <span
+                v-if='attributesCount > 0'
+                class='has-text-weight-light has-text-grey-light is-size-7'>({{attributesCount}})</span>
+            </label>
           </div>
 
           <nav class="panel is-unselectable">
