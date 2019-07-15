b24ef46e0dec741a83c86755b90ca884e0c0005a
Ben Hong <bencodezen@gmail.com>
2019-07-15 05:14:42 -0400

    :art: (#342): Set linting and prettier config

--- src/webapp/.eslintrc.js
+++ src/webapp/.eslintrc.js
@@ -3,12 +3,23 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
+  extends: ['plugin:vue/essential', '@vue/prettier'],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // disallow reassignment of function parameters
+    // disallow parameter object manipulation except for specific exclusions
+    'no-param-reassign': 0,
+    curly: ['warn', 'all'],
+    'brace-style': ['error', '1tbs'],
+    'max-len': ['error', { code: 200, ignoreUrls: true }],
+    'no-prototype-builtins': 0
   },
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint'
+  },
+  globals: {
+    process: false,
+    FLASK: false
   }
-};
+}
