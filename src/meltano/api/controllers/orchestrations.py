d996f40846c13181c437e1e14fea730b5dabbd9b
derek-knox <dknox@gitlab.com>
2019-08-16 11:02:02 -0700

    use Micaeël's recommended approach for defaulting config settings vs manual re-iteration

--- src/meltano/api/controllers/orchestrations.py
+++ src/meltano/api/controllers/orchestrations.py
@@ -83,15 +83,8 @@ def get_plugin_configuration() -> Response:
     settings = PluginSettingsService(db.session, project)
 
     config = flatten(
-        settings.as_config(plugin, sources=[PluginSettingValueSource.DB]), reducer="dot"
+        settings.as_config(plugin), reducer="dot"
     )
-
-    # Apply default config
-    for setting in settings.get_definition(plugin).settings:
-        key = setting["name"]
-        if not key in config:
-            if "value" in setting:
-                config[key] = setting["value"]
 
     return jsonify(
         {
