6de0485117738ed4e795c336e8e7b4fdc9e86e6b
derek-knox <dknox@gitlab.com>
2019-08-27 06:42:52 -0700

    improved Airflow UI update triggers to only display the iframe when completely installed, refactored ClosableMessage to Message for consistent use (easy to update or delete while also alleviating a UX expectation of persistance of a closed ClosableMessage that would otherwise display again on session refresh)

---
+++ src/webapp/src/components/generic/Message.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  name: 'Message'
+}
+</script>
+
+<template>
+  <div class="content">
+    <article class="message is-info is-small">
+      <div class="message-header">
+        <a
+          class="button is-borderless has-background-transparent has-text-white"
+        >
+          <span class="icon">
+            <font-awesome-icon icon="info-circle"></font-awesome-icon>
+          </span>
+          <span>Info</span>
+        </a>
+      </div>
+      <div class="message-body">
+        <slot></slot>
+      </div>
+    </article>
+  </div>
+</template>
+
+<style lang="scss"></style>
