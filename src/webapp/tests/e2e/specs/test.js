5c6c905b4e04784a7c0d5f8c5cb6d5d434be3a89
Ben Hong <bencodezen@gmail.com>
2019-09-11 07:00:30 -0400

    config :package: (#810): Scaffold Cypress package

--- src/webapp/tests/e2e/specs/test.js
+++ src/webapp/tests/e2e/specs/test.js
@@ -1,16 +1,8 @@
-// For authoring Nightwatch tests, see
-// http://nightwatchjs.org/guide#usage
+// https://docs.cypress.io/api/introduction/api.html
 
-module.exports = {
-  'default e2e tests': function test(browser) {
-    // automatically uses dev Server port from /build/config.js
-    // default: http://localhost:8080
-    // see nightwatch.conf.js
-    const devServer = browser.globals.devServerUrl
-
-    browser
-      .url(devServer)
-      .waitForElementVisible('#app', 5000)
-      .end()
-  }
-}
+describe('My First Test', () => {
+  it('Visits the app root url', () => {
+    cy.visit('/')
+    cy.contains('h1', 'Welcome to Your Vue.js App')
+  })
+})
