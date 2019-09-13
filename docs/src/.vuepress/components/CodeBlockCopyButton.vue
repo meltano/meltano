2e72f92f5f9a0efa7661a610e6cb5eba93f49191
Ben Hong <bencodezen@gmail.com>
2019-09-13 08:49:27 -0400

    style :art: (#964): Prettier file

--- docs/src/.vuepress/components/CodeBlockCopyButton.vue
+++ docs/src/.vuepress/components/CodeBlockCopyButton.vue
@@ -5,20 +5,21 @@ export default {
 </script>
 
 <style>
-  .code-block-copy-button {
-    cursor: pointer;
-    display: inline-block;
-    position: absolute;
-    z-index: 999;
-    top: 10px;
-    right: 8px;
-    width: 24px;
-    height: 24px;
-    content: url(../public/copy-to-clipboard-icon.svg);
-    background-color: #282c34;
-    transition: transform .20s;
-  }
-  .code-block-copy-button:active {
-    transform: scale(.8);
-  }
+.code-block-copy-button {
+  cursor: pointer;
+  display: inline-block;
+  position: absolute;
+  z-index: 999;
+  top: 10px;
+  right: 8px;
+  width: 24px;
+  height: 24px;
+  content: url(../public/copy-to-clipboard-icon.svg);
+  background-color: #282c34;
+  transition: transform 0.2s;
+}
+
+.code-block-copy-button:active {
+  transform: scale(0.8);
+}
 </style>
