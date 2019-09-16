e33ac28cb5c9fbdf62d7ee04e38b5fb11a74d89a
mbergeron <mbergeron@gitlab.com>
2019-09-16 11:14:53 -0400

    fix a comment on the flask.js module

--- src/webapp/src/flask.js
+++ src/webapp/src/flask.js
@@ -1,10 +1,7 @@
 /*
-   This module is a shim to inject globals
-   variable in the development version of
-   the UI.
-
-   In production, these variables will be
-   set in the template by Flask.
+   This module handles the Flask context,
+   either set by the Meltano API in production
+   or from webpack in development.
 */
 
 module.exports = function() {
