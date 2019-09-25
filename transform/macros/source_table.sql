395ee738fe1d022aeaca6bcf0b507bb5d7abd332
mbergeron <mbergeron@gitlab.com>
2019-09-25 08:55:58 -0400

    remove dead code

--- transform/macros/source_table.sql
+++
@@ -1,8 +0,0 @@
-{% macro quote(identifier) %}
-  {{ return('"' ~ identifier ~ '"') }}
-{% endmacro %}
-
-{% macro source_table(table_name) %}
-  {% set table_config = table_name ~ "_table" %}
-  {{ return(quote(var("schema")) ~ "." ~ quote(var(table_config, table_name))) }}
-{% endmacro %}
