234e8eee94b006882441bc86e7d8c2308ad06459
Ben Hong <bencodezen@gmail.com>
2019-09-26 14:26:59 -0400

    test (#997): Verify that Gogole Analytics exists on production builds

---
+++ src/webapp/tests/e2e/specs/google-analytics.js
@@ -0,0 +1,12 @@
+describe('Usage data tracking', function() {
+  it('Checks for Google Analytics', function() {
+    cy.visit('http://localhost:5000/')
+    cy.window().then(win => {
+      // GA is an acronym for Google Analytics
+      const appGAId = Object.keys(win.gaData)[0]
+      const meltanoGAId = 'UA-132758957-2'
+
+      expect(appGAId).to.equal(meltanoGAId)
+    })
+  })
+})
