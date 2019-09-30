7775bb42d597b92788342f4051baa547b589e950
mbergeron <mbergeron@gitlab.com>
2019-09-30 11:04:46 -0400

    add missing files

---
+++ src/meltano/core/utils/pidfile.py
@@ -0,0 +1,44 @@
+import psutil
+from pathlib import Path
+from typing import Union
+
+
+class PIDFile:
+    def __init__(self, path: Union[str, Path]):
+        self.path = Path(path)
+        self._pid = None
+
+    @property
+    def pid(self):
+        return self._pid or self.load_pid()
+
+    def load_pid(self) -> int:
+        try:
+            raw = self.path.open("r")
+            self._pid = int(raw.read())
+        except (ValueError):
+            self.path.unlink()
+        except FileNotFoundError:
+            pass
+        finally:
+            raw.close()
+
+        return self._pid
+
+    def write_pid(self, pid: int):
+        try:
+            raw = self.path.open("w")
+            raw.write(str(pid))
+            self._pid = pid
+        finally:
+            raw.close()
+
+    @property
+    def process(self) -> psutil.Process:
+        return psutil.Process(self.pid)
+
+    def unlink(self):
+        return self.path.unlink()
+
+    def __str__(self) -> str:
+        return str(self.path)
