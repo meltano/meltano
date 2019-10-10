d24f687e2d78869bff957096f20cd1b58b491436
Taylor A. Murphy <tmurphy@gitlab.com>
2019-10-10 14:30:04 -0500

    refactor: Simplify revokes

--- src/meltano/core/permissions/utils/snowflake_revoke.py
+++ src/meltano/core/permissions/utils/snowflake_revoke.py
@@ -10,8 +10,6 @@ REVOKE_ALL_PRIVILEGES_ACCOUNTOBJECT_TEMPLATE = "REVOKE {privileges} PRIVILEGES O
 
 REVOKE_ALL_PRIVILEGES_OBJECT_TEMPLATE = "REVOKE {privileges} PRIVILEGES ON ALL {resource_type}S IN {parent_resource_type} {parent_resource_name} FROM ROLE {role}"
 
-ALTER_USER_TEMPLATE = "ALTER USER {user_name} SET {privileges}"
-
 
 class SnowflakeRevokesGenerator:
     def __init__(self, grants_to_role: Dict, roles_granted_to_user: Dict) -> None:
@@ -48,7 +46,7 @@ class SnowflakeRevokesGenerator:
                 )
         except KeyError:
             logging.debug(
-                "`member_of` not found for {}, skipping generation of GRANT ROLE statements.".format(
+                "`member_of` not found for {}, skipping generation of REVOKE ROLE statements.".format(
                     entity
                 )
             )
@@ -61,15 +59,15 @@ class SnowflakeRevokesGenerator:
         """
         Generate all the privilege revoking statements for a role.
 
-        Most of the SQL command that will be generated are privileges granted to
+        Most of the SQL command that will be generated are privileges revoked from
         roles and this function orchestrates the whole process
 
         role: the name of the role (e.g. "LOADER" or "REPORTER") the privileges
-              are GRANTed to
+              are REVOKEd from
         config: the subtree for the role as specified in the spec
         shared_dbs: a set of all the shared databases defined in the spec.
-                    Used down the road by generate_database_grants() to also grant
-                    "IMPORTED PRIVILEGES" when access is granted to a shared DB.
+                    Used down the road by generate_database_revokes() to also revoke
+                    "IMPORTED PRIVILEGES" when access is for a shared DB.
 
         Returns the SQL commands generated as a list
         """
@@ -77,7 +75,7 @@ class SnowflakeRevokesGenerator:
 
         # Track all the DBs and schemas that have been given access to (GRANT USAGE)
         # the given role. Used in order to recursively grant the required access
-        # to DBs or schemas implicetly reference when permissions are GRANTED for a
+        # to DBs or schemas implicitly referenced when permissions are GRANTED for a
         # child Schema or Table.
         # Example: A role is given read access to table MY_DB.MY_SCHEMA.MY_TABLE
         # 1. In order to access MY_TABLE, the role has to be able to access MY_DB.MY_SCHEMA
@@ -89,7 +87,10 @@ class SnowflakeRevokesGenerator:
 
         try:
             for warehouse in config["warehouses"]:
-                sql_commands.extend(self.generate_warehouse_revokes(role, warehouse))
+                new_commands = self.generate_warehouse_revokes(
+                    role=role, 
+                    warehouse=warehouse)
+                sql_commands.extend(new_commands)
         except KeyError:
             logging.debug(
                 "`warehouses` not found for role {}, skipping generation of Warehouse REVOKE statements.".format(
@@ -101,8 +102,6 @@ class SnowflakeRevokesGenerator:
                 new_commands = self.generate_database_revokes(
                     role=role,
                     database=database,
-                    grant_type="read",
-                    usage_granted=usage_granted,
                     shared_dbs=shared_dbs,
                 )
                 sql_commands.extend(new_commands)
@@ -118,8 +117,6 @@ class SnowflakeRevokesGenerator:
                 new_commands = self.generate_database_revokes(
                     role=role,
                     database=database,
-                    grant_type="write",
-                    usage_granted=usage_granted,
                     shared_dbs=shared_dbs,
                 )
                 sql_commands.extend(new_commands)
@@ -132,11 +129,9 @@ class SnowflakeRevokesGenerator:
 
         try:
             for table in config["privileges"]["tables"]["read"]:
-                new_commands, usage_granted = self.generate_table_and_view_revokes(
+                new_commands = self.generate_table_and_view_revokes(
                     role=role,
                     table=table,
-                    grant_type="read",
-                    usage_granted=usage_granted,
                     shared_dbs=shared_dbs,
                 )
                 sql_commands.extend(new_commands)
@@ -149,11 +144,9 @@ class SnowflakeRevokesGenerator:
 
         try:
             for table in config["privileges"]["tables"]["write"]:
-                new_commands, usage_granted = self.generate_table_and_view_revokes(
+                new_commands = self.generate_table_and_view_revokes(
                     role=role,
                     table=table,
-                    grant_type="write",
-                    usage_granted=usage_granted,
                     shared_dbs=shared_dbs,
                 )
                 sql_commands.extend(new_commands)
@@ -195,21 +188,16 @@ class SnowflakeRevokesGenerator:
         self,
         role: str,
         database: str,
-        grant_type: str,
-        usage_granted: Dict,
         shared_dbs: Set,
-    ) -> Tuple[List[str], Dict]:
+    ) -> List[str]:
         """
-        Generate the GRANT statements for Databases.
+        Generate the REVOKE statements for Databases and Schemas.
 
-        role: the name of the role the privileges are GRANTed to
+        role: the name of the role the privileges are REVOKEd from
         database: the name of the database (e.g. "RAW")
-        grant_type: What type of privileges are granted? One of {"read", "write"}
-        usage_granted: Passed by generate_grant_privileges_to_role() to track all
-            all the entities a role has been granted access (USAGE) to.
         shared_dbs: a set of all the shared databases defined in the spec.
 
-        Returns the SQL commands generated and the updated usage_granted as a Tuple
+        Returns the SQL commands generated as a list
         """
         sql_commands = []
         already_granted = False
@@ -262,74 +250,21 @@ class SnowflakeRevokesGenerator:
         )
         return sql_commands
 
-    def generate_schema_revokes(
-            self,
-            role: str,
-            schema: str,
-            grant_type: str,
-            usage_granted: Dict,
-            shared_dbs: Set,
-        ) -> Tuple[List[str], Dict]:
-        """
-        Generate the REVOKE statements for SCHEMAs.
-
-        role: the name of the role the privileges are GRANTed to
-        schema: the name of the Schema (e.g. "RAW.PUBLIC")
-        grant_type: What type of privileges are granted? One of {"read", "write"}
-        usage_granted: Passed by generate_grant_privileges_to_role() to track all
-            all the entities a role has been granted access (USAGE) to.
-        shared_dbs: a set of all the shared databases defined in the spec.
-
-        Returns the SQL commands generated and the updated usage_granted as a Tuple
-        """
-        sql_commands = []
-
-        # Split the schema identifier into parts {DB_NAME}.{SCHEMA_NAME}
-        #  so that we can check and use each one
-        name_parts = schema.split(".")
-
-        # Do nothing if this is a schema inside a shared database:
-        #  "Revoking individual privileges on imported databases is not allowed."
-        if name_parts[0] in shared_dbs:
-            return (sql_commands, usage_granted)
-
-        sql_commands.append(
-            {
-                "already_granted": False,
-                "sql": REVOKE_ALL_PRIVILEGES_OBJECT_TEMPLATE.format(
-                    privileges="ALL",
-                    resource_type="SCHEMA",
-                    parent_resource_type="DATABASE",
-                    parent_resource_name=name_parts[0],
-                    role=SnowflakeConnector.snowflaky(role),
-                ),
-            }
-        )
-
-        usage_granted["schemas"].add(schema.upper())
-
-        return (sql_commands, usage_granted)
-
 
     def generate_table_and_view_revokes(
         self,
         role: str,
         table: str,
-        grant_type: str,
-        usage_granted: Dict,
         shared_dbs: Set,
-    ) -> Tuple[List[str], Dict]:
+    ) -> List[str]:
         """
-        Generate the GRANT statements for TABLEs and VIEWs.
+        Generate the REVOKE statements for TABLEs and VIEWs.
 
-        role: the name of the role the privileges are GRANTed to
+        role: the name of the role the privileges are REVOKEd from
         table: the name of the TABLE/VIEW (e.g. "RAW.PUBLIC.MY_TABLE")
-        grant_type: What type of privileges are granted? One of {"read", "write"}
-        usage_granted: Passed by generate_grant_privileges_to_role() to track all
-            all the entities a role has been granted access (USAGE) to.
         shared_dbs: a set of all the shared databases defined in the spec.
 
-        Returns the SQL commands generated and the updated usage_granted as a Tuple
+        Returns the SQL commands generated
         """
         sql_commands = []
 
@@ -340,7 +275,7 @@ class SnowflakeRevokesGenerator:
         # Do nothing if this is a table inside a shared database:
         #  "Granting individual privileges on imported databases is not allowed."
         if name_parts[0] in shared_dbs:
-            return (sql_commands, usage_granted)
+            return sql_commands
 
         # Generate the INFORMATION_SCHEMA identifier for that database
         #  in order to be able to filter it out
@@ -352,7 +287,7 @@ class SnowflakeRevokesGenerator:
 
         if name_parts[1] == "*":
             # If {DB_NAME}.*.* was provided as the identifier, we have to fetch
-            #  each schema in database DB_NAME, so that we can grant privileges
+            #  each schema in database DB_NAME, so that we can revoke privileges
             #  for all tables in that schema
             # (You can not GRANT to all table with a wild card for the schema name)
             db_schemas = conn.show_schemas(name_parts[0])
@@ -360,6 +295,8 @@ class SnowflakeRevokesGenerator:
                 if schema != info_schema:
                     schemas.append(schema)
         elif '*' in name_parts[1]:
+            # Similar to above, if {DB_NAME}.partial_*.* was provided,
+            #  we need to find each of the schemas that match the pattern.
             conn = SnowflakeConnector()
             db_schemas = conn.show_schemas(name_parts[0])
             for db_schema in db_schemas:
@@ -369,8 +306,6 @@ class SnowflakeRevokesGenerator:
         else:
             schemas = [f"{name_parts[0]}.{name_parts[1]}"]
 
-
-        # If *.* then you can grant all, grant future, and exit
         for schema in schemas:
             # Revoke on ALL tables
             sql_commands.append(
@@ -400,4 +335,4 @@ class SnowflakeRevokesGenerator:
                 }
             )
 
-        return (sql_commands, usage_granted)
+        return sql_commands
