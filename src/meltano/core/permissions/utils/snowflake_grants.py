c2de27eb47c4cf1ffdb562dc92f0e97b619dfaa9
Taylor A. Murphy <tmurphy@gitlab.com>
2019-10-10 10:49:36 -0500

    feature: Add handling of prefix+wildcard schemas

--- src/meltano/core/permissions/utils/snowflake_grants.py
+++ src/meltano/core/permissions/utils/snowflake_grants.py
@@ -445,6 +445,13 @@ class SnowflakeGrantsGenerator:
             for db_schema in db_schemas:
                 if db_schema != info_schema:
                     schemas.append(db_schema)
+        elif '*' in name_parts[1]:
+            conn = SnowflakeConnector()
+            db_schemas = conn.show_schemas(name_parts[0])
+            for db_schema in db_schemas:
+                schema_name = db_schema.split('.',1)[1].lower()
+                if schema_name.startswith(name_parts[1].split('*', 1)[0]):
+                    schemas.append(db_schema)
         else:
             schemas = [schema]
 
@@ -569,6 +576,13 @@ class SnowflakeGrantsGenerator:
             for schema in db_schemas:
                 if schema != info_schema:
                     schemas.append(schema)
+        elif '*' in name_parts[1]:
+            conn = SnowflakeConnector()
+            db_schemas = conn.show_schemas(name_parts[0])
+            for db_schema in db_schemas:
+                schema_name = db_schema.split('.',1)[1].lower()
+                if schema_name.startswith(name_parts[1].split('*', 1)[0]):
+                    schemas.append(db_schema)
         else:
             schemas = [f"{name_parts[0]}.{name_parts[1]}"]
 
