ff1a26e49e4ed6c97fd2fada1ebe7c224b9d4a3f
derek-knox <dknox@gitlab.com>
2019-10-17 14:02:15 -0700

    better encapsulated toast and analytics initialization in their own dedicated modules

--- src/webapp/src/main.js
+++ src/webapp/src/main.js
@@ -1,16 +1,16 @@
 import Router from 'vue-router'
-import Toasted from 'vue-toasted'
 import Vue from 'vue'
-import VueAnalytics from 'vue-analytics'
-import axios from 'axios'
+
 import { Service } from 'axios-middleware'
+import axios from 'axios'
 
 import App from './App'
 import Auth from '@/middleware/auth'
 import FatalError from '@/middleware/fatalError'
 import FontAwesome from '@/font-awesome'
 import flaskContext from '@/flask'
-import toastedHelper from '@/toastedHelper'
+import setupAnalytics from '@/setupAnalytics'
+import setupToasted from '@/setupToasted'
 import router from './router'
 import store from './store'
 import Upgrade from '@/middleware/upgrade'
@@ -20,30 +20,21 @@ Vue.config.productionTip = false
 Vue.use(FontAwesome)
 Vue.use(Router)
 
-const toastedOptions = {
-  router,
-  position: 'bottom-right',
-  iconPack: 'fontawesome',
-  theme: 'outline',
-  duration: 9000
-}
-Vue.use(Toasted, toastedOptions)
-toastedHelper.initialize(toastedOptions)
+// Toast setup
+setupToasted()
 
+// Middleware setup
 const service = new Service(axios)
-
 Vue.use(Upgrade, {
   service,
   router,
   toasted: Vue.toasted
 })
-
 Vue.use(FatalError, {
   service,
   router,
   toasted: Vue.toasted
 })
-
 Vue.use(Auth, {
   service,
   router,
@@ -58,21 +49,7 @@ Vue.prototype.$flask = flaskContext()
 
 // Conditional analytics using flask context
 if (Vue.prototype.$flask.isSendAnonymousUsageStats) {
-  Vue.use(VueAnalytics, {
-    id: 'UA-132758957-2',
-    router,
-    set: [
-      {
-        field: 'appVersion',
-        value: Vue.prototype.$flask.version
-      },
-      {
-        // Unfortunately custom dimensions don't allow a useful alias, dimension1 is projectId
-        field: 'dimension1',
-        value: Vue.prototype.$flask.projectId
-      }
-    ]
-  })
+  setupAnalytics()
 }
 
 /* eslint-disable no-new */
