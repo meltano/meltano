f66a2baa349ec7d0e013b5fb44212422be765cac
mbergeron <mbergeron@gitlab.com>
2019-10-21 14:10:36 -0400

    add the entrypoint

---
+++ docker/prod/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ -n "$MELTANO_PROJECT_INIT" ]]; then
+    [[ -f "$MELTANO_PROJECT_INIT/meltano.yml" ]] || { meltano init "$MELTANO_PROJECT_INIT" --no_usage_stats; }
+    cd "$MELTANO_PROJECT_INIT" || exit 1
+    meltano "$@"
+else
+    meltano "$@"
+fi
