2f514c9465a85aacc1d2a5d50e240895dc0c8d18
derek-knox <dknox@gitlab.com>
2019-12-10 08:37:05 -0800

    add onChangeExtractor hook and minor refactoring so default transforms are updated when extractor changes are made in real-time

--- src/webapp/src/components/pipelines/CreateScheduleModal.vue
+++ src/webapp/src/components/pipelines/CreateScheduleModal.vue
@@ -72,6 +72,12 @@ export default {
         this.$router.push({ name: 'schedules' })
       }
     },
+    onChangeExtractor() {
+      const namespace = this.installedPlugins.extractors.find(
+        extractor => extractor.name === this.pipeline.extractor
+      ).namespace
+      this.updateDefaultTransforms(namespace)
+    },
     prefillForm() {
       // TODO implement an intelligent prefill approach
       this.pipeline.name = `pipeline-${new Date().getTime()}`
@@ -88,12 +94,7 @@ export default {
           this.installedPlugins.loaders[0])
       this.pipeline.loader = defaultLoader ? defaultLoader.name : ''
 
-      const defaultTransform = this.getHasDefaultTransforms(
-        defaultExtractor.namespace
-      )
-        ? this.transformOptions[1]
-        : this.transformOptions[0]
-      this.pipeline.transform = defaultTransform ? defaultTransform.name : ''
+      this.updateDefaultTransforms(defaultExtractor.namespace)
 
       this.pipeline.interval = !_.isEmpty(this.intervalOptions)
         ? this.intervalOptions[0]
@@ -119,10 +120,7 @@ export default {
           Vue.toasted.global.error(error.response.data.code)
         })
     },
-    updateDefaultTransform() {
-      const namespace = this.installedPlugins.extractors.find(
-        extractor => extractor.name === this.pipeline.extractor
-      ).namespace
+    updateDefaultTransforms(namespace) {
       const defaultTransform = this.getHasDefaultTransforms(namespace)
         ? this.transformOptions[1]
         : this.transformOptions[0]
@@ -171,7 +169,7 @@ export default {
                       v-model="pipeline.extractor"
                       :class="{ 'has-text-success': pipeline.extractor }"
                       :disabled="!getHasInstalledPluginsOfType('extractors')"
-                      @change="updateDefaultTransform"
+                      @change="onChangeExtractor"
                     >
                       <option
                         v-for="extractor in availableExtractors"
