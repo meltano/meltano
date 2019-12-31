4c680fc13790b275ba8eb13a9da9e633e8ce4e99
Taylor A. Murphy <tmurphy@gitlab.com>
2019-12-31 12:35:29 -0600

    Add warehouse grants test

--- tests/meltano/core/permissions/test_snowflake_grants.py
+++ tests/meltano/core/permissions/test_snowflake_grants.py
@@ -4,39 +4,68 @@ from meltano.core.permissions.utils.snowflake_grants import SnowflakeGrantsGener
 
 
 @pytest.fixture(scope="class")
-def test_grants_to_role():
+def test_role_config():
+    config = {
+        "functional_role": {
+            "warehouses": ["warehouse_2", "warehouse_3"],
+            "member_of": ["object_role_2", "object_role_3"],
+            "privileges": {
+                "databases": {"read": ["database_2"], "write": ["database_3"]}
+            },
+        }
+    }
 
-    roles = {"functional_role": {"usage": {"role": ["object_role_1", "object_role_2"]}}}
+    return config
+
+
+@pytest.fixture(scope="class")
+def test_user_config():
+    config = {
+        "user_name": {"can_login": True, "member_of": ["object_role", "user_role"]}
+    }
+    
+    return config
+
+
+@pytest.fixture(scope="class")
+def test_grants_to_role():
+    roles = {
+        "functional_role": {
+            "usage": {
+                "database": ["database_1", "database_2"],
+                "role": ["object_role_1", "object_role_2"],
+                "warehouse": ["warehouse_1", "warehouse_2"],
+            },
+            "operate": {"warehouse": ["warehouse_1", "warehouse_2"]},
+            "monitor": {"database": ["database_1", "database_2"]},
+            "create schema": {"database": ["database_1", "database_2"]},
+        }
+    }
 
     return roles
 
 
 @pytest.fixture(scope="class")
 def test_roles_granted_to_user():
-    return {"user_name": ["function_role", "user_role"]}
+    config = {"user_name": ["function_role", "user_role"]}
+
+    return config
 
 
 class TestSnowflakeGrants:
     def test_generate_grant_roles(
-        self, test_grants_to_role, test_roles_granted_to_user
+        self,
+        test_grants_to_role,
+        test_roles_granted_to_user,
+        test_role_config,
+        test_user_config,
     ):
         generator = SnowflakeGrantsGenerator(
             test_grants_to_role, test_roles_granted_to_user
         )
 
-        user_config = {
-            "user_name": {"can_login": True, "member_of": ["object_role", "user_role"]}
-        }
-
-        role_config = {
-            "functional_role": {
-                "warehouses": ["warehouse_1"],
-                "member_of": ["object_role_2", "object_role_3"],
-            }
-        }
-
         role_command_list = generator.generate_grant_roles(
-            "roles", "functional_role", role_config["functional_role"]
+            "roles", "functional_role", test_role_config["functional_role"]
         )
 
         role_lower_list = [cmd.get("sql", "").lower() for cmd in role_command_list]
@@ -46,7 +75,7 @@ class TestSnowflakeGrants:
         assert "revoke role object_role_1 from role functional_role" in role_lower_list
 
         user_command_list = generator.generate_grant_roles(
-            "users", "user_name", user_config["user_name"]
+            "users", "user_name", test_user_config["user_name"]
         )
 
         user_lower_list = [cmd.get("sql", "").lower() for cmd in user_command_list]
@@ -54,3 +83,48 @@ class TestSnowflakeGrants:
         assert "grant role object_role to user user_name" in user_lower_list
         assert "grant role user_role to user user_name" in user_lower_list
         assert "revoke role function_role from user user_name" in user_lower_list
+
+    def test_generate_warehouse_grants(
+        self,
+        test_grants_to_role,
+        test_roles_granted_to_user,
+        test_role_config,
+        test_user_config,
+    ):
+        generator = SnowflakeGrantsGenerator(
+            test_grants_to_role, test_roles_granted_to_user
+        )
+
+        warehouse_command_list = generator.generate_warehouse_grants(
+            "functional_role", test_role_config["functional_role"]["warehouses"]
+        )
+
+        warehouse_lower_list = [
+            cmd.get("sql", "").lower() for cmd in warehouse_command_list
+        ]
+
+        assert (
+            "grant usage on warehouse warehouse_2 to role functional_role"
+            in warehouse_lower_list
+        )
+        assert (
+            "grant usage on warehouse warehouse_3 to role functional_role"
+            in warehouse_lower_list
+        )
+        assert (
+            "revoke usage on warehouse warehouse_1 from role functional_role"
+            in warehouse_lower_list
+        )
+
+        assert (
+            "grant operate on warehouse warehouse_2 to role functional_role"
+            in warehouse_lower_list
+        )
+        assert (
+            "grant operate on warehouse warehouse_3 to role functional_role"
+            in warehouse_lower_list
+        )
+        assert (
+            "revoke operate on warehouse warehouse_1 from role functional_role"
+            in warehouse_lower_list
+        )
