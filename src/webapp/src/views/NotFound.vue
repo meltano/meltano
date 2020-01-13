947fdb9f0c565aefb894b1574f542b04ef40c5f2
Ben Hong <bencodezen@gmail.com>
2020-01-13 15:53:13 -0500

    feature (#1456): Add option to file new issue for error page

--- src/webapp/src/views/NotFound.vue
+++ src/webapp/src/views/NotFound.vue
@@ -11,13 +11,27 @@ export default {
         <h1 class="title">
           404
         </h1>
-        <h2 class="subtitle">
+        <h2 class="subtitle mb-1r">
           Oops... Seems like this page is missing...
         </h2>
+        <p class="mb-1r">
+          If you think this is a bug,
+          <a
+            href="https://gitlab.com/meltano/meltano/issues/new?issue"
+            target="_blank"
+          >
+            open a new issue
+          </a>
+          and include the URL you are trying to access.
+        </p>
         <a class="button" @click="$router.back()">Go back to the last page</a>
       </div>
     </div>
   </section>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.mb-1r {
+  margin-bottom: 1rem;
+}
+</style>
