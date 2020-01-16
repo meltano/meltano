e7485cf535450ba1c3adda1f105a556a3ec08542
derek-knox <dknox@gitlab.com>
2020-01-15 18:05:26 -0800

    cleaner markup and added styling for article media containers as interactive units

--- src/webapp/src/components/pipelines/ExtractorList.vue
+++ src/webapp/src/components/pipelines/ExtractorList.vue
@@ -35,72 +35,70 @@ export default {
 
 <template>
   <div>
-    <div class="box is-borderless is-shadowless is-marginless">
-      <progress
-        v-if="isLoadingExtractors"
-        class="progress is-small is-info"
-      ></progress>
+    <progress
+      v-if="isLoadingExtractors"
+      class="progress is-small is-info"
+    ></progress>
 
-      <div v-else>
-        <article
-          v-for="(extractor, index) in visibleExtractors"
-          :key="`${extractor.name}-${index}`"
-          class="media"
+    <div v-else>
+      <article
+        v-for="(extractor, index) in visibleExtractors"
+        :key="`${extractor.name}-${index}`"
+        class="media"
+        :class="{
+          'has-cursor-pointer is-hoverable': !getHasPipelineWithExtractor(
+            extractor.name
+          )
+        }"
+        :data-test-id="`${extractor.name}-extractor-card`"
+        @click="updateExtractorSettings(extractor)"
+      >
+        <figure
+          class="media-left"
           :class="{
-            'has-cursor-pointer is-hoverable': !getHasPipelineWithExtractor(
-              extractor.name
-            )
+            'is-transparent-50': getHasPipelineWithExtractor(extractor.name)
           }"
-          :data-test-id="`${extractor.name}-extractor-card`"
-          @click="updateExtractorSettings(extractor)"
         >
-          <figure
-            class="media-left"
-            :class="{
-              'is-transparent-50': getHasPipelineWithExtractor(extractor.name)
-            }"
-          >
-            <p class="image level-item is-48x48 container">
-              <ConnectorLogo
-                :connector="extractor.name"
-                :is-grayscale="getHasPipelineWithExtractor(extractor.name)"
-              />
+          <p class="image level-item is-48x48 container">
+            <ConnectorLogo
+              :connector="extractor.name"
+              :is-grayscale="getHasPipelineWithExtractor(extractor.name)"
+            />
+          </p>
+        </figure>
+        <div
+          class="media-content"
+          :class="{
+            'is-transparent-50': getHasPipelineWithExtractor(extractor.name)
+          }"
+        >
+          <div class="content">
+            <p>
+              <strong>{{ extractor.label }}</strong>
+              <br />
+              <small>{{ extractor.description }}</small>
             </p>
-          </figure>
-          <div
-            class="media-content"
-            :class="{
-              'is-transparent-50': getHasPipelineWithExtractor(extractor.name)
-            }"
-          >
-            <div class="content">
-              <p>
-                <strong>{{ extractor.label }}</strong>
-                <br />
-                <small>{{ extractor.description }}</small>
-              </p>
-            </div>
           </div>
-          <figure
-            v-if="getHasPipelineWithExtractor(extractor.name)"
-            class="media-right is-flex is-flex-column is-vcentered"
+        </div>
+        <figure
+          v-if="getHasPipelineWithExtractor(extractor.name)"
+          class="media-right is-flex is-flex-column is-vcentered"
+        >
+          <a
+            href="#pipelines"
+            class="button tooltip is-tooltip-left"
+            data-tooltip="A pipeline for this data source already exists"
+            @click.stop="() => {}"
           >
-            <a
-              href="#pipelines"
-              class="button tooltip is-tooltip-left"
-              data-tooltip="A pipeline for this data source already exists"
-              @click.stop="() => {}"
-            >
-              <span>View Pipeline</span>
-            </a>
-          </figure>
-        </article>
-      </div>
-      <progress
-        v-if="!visibleExtractors"
-        class="progress is-small is-info"
-      ></progress>
+            <span>View Pipeline</span>
+          </a>
+        </figure>
+      </article>
     </div>
+    <progress
+      v-if="!visibleExtractors"
+      class="progress is-small is-info"
+    ></progress>
   </div>
 </template>
 
