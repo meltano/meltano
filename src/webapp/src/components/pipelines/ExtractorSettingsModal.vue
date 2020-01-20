346af98de60eca99c6b177037037bb50a787abf0
derek-knox <dknox@gitlab.com>
2020-01-20 05:14:14 -0800

    lint fix, changelog update

--- src/webapp/src/components/pipelines/ExtractorSettingsModal.vue
+++ src/webapp/src/components/pipelines/ExtractorSettingsModal.vue
@@ -179,9 +179,7 @@ export default {
             })
             this.$router.push({ name: 'loaders' })
             const message = this.extractorLacksConfigSettings
-              ? `Auto Advance - No Configuration needed for ${
-                  this.extractor.name
-                }`
+              ? `Auto Advance - No Configuration needed for ${this.extractor.name}`
               : `Connection Saved - ${this.extractor.name}`
             Vue.toasted.global.success(message)
           })
