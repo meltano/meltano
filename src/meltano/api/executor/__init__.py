127096fd4bfa54afb26d011486cfd9edcb2a64d2
mbergeron <mbergeron@gitlab.com>
2020-02-07 10:23:41 -0500

    wip: sending an email

--- src/meltano/api/executor/__init__.py
+++ src/meltano/api/executor/__init__.py
@@ -4,7 +4,9 @@ import logging
 import os
 from functools import partial
 from flask_executor import Executor
+from flask_mail import Message
 
+from meltano.api.mail import mail
 from meltano.core.plugin import PluginRef, PluginType
 from meltano.core.project import Project
 
@@ -16,7 +18,7 @@ def setup_executor(app, project):
     executor.init_app(app)
 
 
-def run_elt(project: Project, schedule_payload: dict):
+def defer_run_elt(schedule_payload: dict):
     job_id = schedule_payload["name"]
     extractor = schedule_payload["extractor"]
     loader = schedule_payload["loader"]
@@ -32,10 +34,20 @@ def run_elt(project: Project, schedule_payload: dict):
         "--transform",
         transform,
     ]
-    executor.submit(
-        subprocess.run, cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
-    )
-    logging.debug(f"Defered `{' '.join(cmd)}` to the executor.")
+
+    result = subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+    # send a success email?
+    msg = Message(subject="A new Data Source is ready!",
+                  body="Congratulations my man!",
+                  recipients=["mbergeron@gitlab.com"])
+
+    mail.send(msg)
+
+
+def run_elt(project: Project, schedule_payload: dict):
+    job_id = schedule_payload["name"]
+    executor.submit(defer_run_elt, schedule_payload)
 
     return job_id
 
