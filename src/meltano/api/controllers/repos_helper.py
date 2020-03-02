9ed1e87242125b52276f9147382f0c60876ffdcf
derek-knox <dknox@gitlab.com>
2020-03-02 06:22:49 -0800

    apply feedback with enforce_secure_filename sanitization

--- src/meltano/api/controllers/repos_helper.py
+++ src/meltano/api/controllers/repos_helper.py
@@ -1,4 +1,5 @@
 import json
+from .utils import enforce_secure_filename
 
 from meltano.core.project import Project
 
@@ -6,7 +7,8 @@ from meltano.core.project import Project
 class ReposHelper:
     def get_topic(self, namespace, topic_name):
         project = Project.find()
-        topic = project.run_dir("models", namespace, f"{topic_name}.topic.m5oc")
+        filename = enforce_secure_filename(f"{topic_name}.topic.m5oc")
+        topic = project.run_dir("models", namespace, filename)
 
         with topic.open() as f:
             topic = json.load(f)
