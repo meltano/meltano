857a4918325edc68562387424af8963f071e24d2
derek-knox <dknox@gitlab.com>
2020-03-05 13:41:25 -0800

    initial refactor to payload containing both relative and absolute payload in addition to isRelative flag

--- src/webapp/src/components/analyze/date-range-picker/DateRangeCustomVsRelative.vue
+++ src/webapp/src/components/analyze/date-range-picker/DateRangeCustomVsRelative.vue
@@ -29,6 +29,9 @@ export default {
     getIsPeriodDisabled() {
       return period => period.IS_DISABLED
     },
+    getIsLast() {
+      return this.model.sign === RELATIVE_DATE_RANGE_MODELS.SIGNS.LAST.NAME
+    },
     getIsPeriodDisabled() {
       return period => period.IS_DISABLED
     },
