3a6f9ca953956a13d9618ec7e2ffd7d64b39344d
Douwe Maan <douwe@selenight.nl>
2020-03-31 17:33:17 -0600

    Refactor dashboard plugin importing logic to be more extensible

--- src/meltano/core/plugin/dashboard.py
+++ src/meltano/core/plugin/dashboard.py
@@ -14,6 +14,159 @@ from meltano.core.m5o.dashboards_service import (
 )
 
 
+class RecordImporter:
+    def __init__(self, packages_dir, project):
+        self.packages_dir = packages_dir
+        self.project = project
+
+    @property
+    def record_type(self):
+        return self.__class__.RECORD_TYPE
+
+    def import_records(self):
+        logging.debug(f"Importing {self.record_type}s...")
+
+        records = self.records_to_import()
+
+        logging.debug(f"Found {len(records)} {self.record_type}s to import")
+
+        for record in records:
+            self.import_record(record)
+
+        logging.debug(f"Successfully imported {len(records)} {self.record_type}s")
+
+    def records_to_import(self):
+        return self.get_package_records(f"{self.record_type}s")
+
+    def import_record(self, importable_record):
+        logging.debug(
+            f"Importing {self.record_type} with name '{importable_record['name']}', ID '{importable_record['id']}'"
+        )
+
+        existing_record = self._get_record(importable_record["id"])
+        if existing_record:
+            logging.debug(
+                f"Found existing {self.record_type} with ID '{existing_record['id']}'"
+            )
+
+            record = existing_record
+            save_func = self._update_record
+        else:
+            record = importable_record
+            save_func = self._save_record
+
+        original_name = record["name"]
+        counter = 1
+
+        self.save_record(record, save_func)
+
+        logging.debug(
+            f"Successfully imported {self.record_type} with ID '{record['id']}', name '{record['name']}'"
+        )
+
+    def save_record(self, record, save_func):
+        original_name = record["name"]
+        counter = 1
+
+        while True:
+            try:
+                record = save_func(record)
+                break
+            except self.__class__.ALREADY_EXISTS_ERROR as e:
+                if e.field == "slug":
+                    counter += 1
+                    record["name"] = f"{original_name} {counter}"
+
+                    # Retry!
+                    continue
+                else:
+                    record = e.record
+                    break
+
+        return record
+
+    def get_package_records(self, *args):
+        records_parser = M5oCollectionParser(
+            self.packages_dir.joinpath(*args), self.record_type
+        )
+        return records_parser.parse()
+
+    def _get_record(self, id):
+        raise NotImplementedError
+
+    def _save_record(self, record):
+        raise NotImplementedError
+
+    def _update_record(self, record):
+        raise NotImplementedError
+
+
+class ReportImporter(RecordImporter):
+    RECORD_TYPE = M5oCollectionParserTypes.Report
+    ALREADY_EXISTS_ERROR = ReportAlreadyExistsError
+    UPGRADEABLE_ATTRIBUTES = [
+        "chart_type",
+        "design",
+        "model",
+        "namespace",
+        "name",
+        "query_payload",
+    ]
+
+    def __init__(self, *args):
+        super().__init__(*args)
+
+        self.reports_service = ReportsService(self.project)
+
+    def _get_record(self, id):
+        return self.reports_service.get_report(id)
+
+    def _save_record(self, report):
+        return self.reports_service.save_report(report)
+
+    def _update_record(self, report):
+        return self.reports_service.update_report(report)
+
+
+class DashboardImporter(RecordImporter):
+    RECORD_TYPE = M5oCollectionParserTypes.Dashboard
+    ALREADY_EXISTS_ERROR = DashboardAlreadyExistsError
+    UPGRADEABLE_ATTRIBUTES = ["name", "description", "report_ids"]
+
+    def __init__(self, *args):
+        super().__init__(*args)
+
+        self.dashboards_service = DashboardsService(self.project)
+
+    def import_record(self, dashboard):
+        original_report_ids = dashboard["report_ids"]
+
+        super().import_record(dashboard)
+
+        self.add_reports_to_dashboard(dashboard, original_report_ids)
+
+    def add_reports_to_dashboard(self, dashboard, report_ids):
+        for report_id in report_ids:
+            self.dashboards_service.add_report_to_dashboard(
+                {"dashboard_id": dashboard["id"], "report_id": report_id}
+            )
+
+            logging.debug(
+                f"Added report with ID '{report_id}' to dashboard with ID '{dashboard['id']}'"
+            )
+
+    def _get_record(self, id):
+        return self.dashboards_service.get_dashboard(id)
+
+    def _save_record(self, dashboard):
+        return self.dashboards_service.save_dashboard(dashboard)
+
+    def _update_record(self, dashboard):
+        return self.dashboards_service.update_dashboard(
+            {"dashboard": {"id": dashboard["id"]}, "new_settings": dashboard}
+        )
+
+
 class DashboardPlugin(PluginInstall):
     __plugin_type__ = PluginType.DASHBOARDS
 
@@ -25,112 +178,5 @@ class DashboardPlugin(PluginInstall):
         venv = VirtualEnv(project.plugin_dir(self, "venv"))
         packages_dir = venv.site_packages_dir
 
-        logging.debug(f"Importing reports...")
-
-        reports_parser = M5oCollectionParser(
-            packages_dir.joinpath("reports"), M5oCollectionParserTypes.Report
-        )
-        reports = reports_parser.parse()
-        logging.debug(f"Found {len(reports)} reports to import")
-
-        reports_service = ReportsService(project)
-        self.import_reports(reports, reports_service=reports_service)
-        logging.debug(f"Finished importing {len(reports)} reports")
-
-        logging.debug(f"Importing dashboards...")
-
-        dashboards_parser = M5oCollectionParser(
-            packages_dir.joinpath("dashboards"), M5oCollectionParserTypes.Dashboard
-        )
-        dashboards = dashboards_parser.parse()
-        logging.debug(f"Found {len(dashboards)} dashboards to import")
-
-        dashboards_service = DashboardsService(project)
-        self.import_dashboards(dashboards, dashboards_service=dashboards_service)
-        logging.debug(f"Finished importing {len(reports)} reports")
-
-    def import_reports(self, reports, reports_service):
-        for report in reports:
-            self.import_report(report, reports_service=reports_service)
-
-    def import_dashboards(self, dashboards, dashboards_service):
-        for dashboard in dashboards:
-            original_report_ids = dashboard["report_ids"]
-
-            dashboard = self.import_dashboard(
-                dashboard, dashboards_service=dashboards_service
-            )
-
-            self.add_reports_to_dashboard(
-                dashboard, original_report_ids, dashboards_service=dashboards_service
-            )
-
-    def import_report(self, report, reports_service):
-        report, existing = self.import_record(
-            report,
-            save_func=reports_service.save_report,
-            already_exists_error=ReportAlreadyExistsError,
-        )
-
-        if existing:
-            logging.debug(
-                f"Found existing report with name '{report['name']}', ID '{report['id']}'"
-            )
-        else:
-            logging.debug(
-                f"Added report with name '{report['name']}', ID '{report['id']}'"
-            )
-
-        return report
-
-    def import_dashboard(self, dashboard, dashboards_service):
-        dashboard, existing = self.import_record(
-            dashboard,
-            save_func=dashboards_service.save_dashboard,
-            already_exists_error=DashboardAlreadyExistsError,
-        )
-
-        if existing:
-            logging.debug(
-                f"Found existing dashboard with name '{dashboard['name']}', ID '{dashboard['id']}'"
-            )
-        else:
-            logging.debug(
-                f"Added dashboard with name '{dashboard['name']}', ID '{dashboard['id']}'"
-            )
-
-        return dashboard
-
-    def import_record(self, record, save_func, already_exists_error):
-        """
-        Imports record.
-
-        Returns `(record: Dict, existing: Bool)`
-        """
-        original_name = record["name"]
-        counter = 1
-
-        while True:
-            try:
-                record = save_func(record)
-                return (record, False)
-            except already_exists_error as e:
-                if e.field == "slug":
-                    counter += 1
-                    record["name"] = f"{original_name} {counter}"
-
-                    # Retry!
-                    continue
-                else:
-                    record = e.record
-                    return (record, True)
-
-    def add_reports_to_dashboard(self, dashboard, report_ids, dashboards_service):
-        for report_id in report_ids:
-            dashboards_service.add_report_to_dashboard(
-                {"dashboard_id": dashboard["id"], "report_id": report_id}
-            )
-
-            logging.debug(
-                f"Added report with ID '{report_id}' to dashboard with ID '{dashboard['id']}'"
-            )
+        ReportImporter(packages_dir, project).import_records()
+        DashboardImporter(packages_dir, project).import_records()
