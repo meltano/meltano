4f6c9f567b8da39e286bae5554ddc350e9705fe5
Douwe Maan <douwe@selenight.nl>
2020-06-16 12:37:53 -0500

    Isolate CLI tests in classes

--- tests/meltano/cli/test_initialize.py
+++ tests/meltano/cli/test_initialize.py
@@ -6,47 +6,47 @@ from meltano.cli import cli
 from meltano.core.project import Project, ProjectNotFound
 
 
-@pytest.fixture()
-def cli_runner():
-    return CliRunner()
+class TestCliInit:
+    @pytest.fixture()
+    def cli_runner(self):
+        return CliRunner()
 
+    def test_init(self, cli_runner, tmp_path_factory, pushd):
+        new_project_root = tmp_path_factory.mktemp("new_meltano_root")
+        pushd(new_project_root)
+        Project._default = None
 
-def test_init(cli_runner, tmp_path_factory, pushd):
-    new_project_root = tmp_path_factory.mktemp("new_meltano_root")
-    pushd(new_project_root)
-    Project._default = None
+        # there are no project actually
+        with pytest.raises(ProjectNotFound):
+            Project.find()
 
-    # there are no project actually
-    with pytest.raises(ProjectNotFound):
-        Project.find()
+        # create one with the CLI
+        cli_runner.invoke(cli, ["init", "test_project", "--no_usage_stats"])
 
-    # create one with the CLI
-    cli_runner.invoke(cli, ["init", "test_project", "--no_usage_stats"])
+        pushd("test_project")
 
-    pushd("test_project")
+        project = Project.find()
 
-    project = Project.find()
-
-    files = (
-        project.root.joinpath(file).resolve()
-        for file in ("meltano.yml", "README.md", ".gitignore", "requirements.txt")
-    )
-
-    dirs = (
-        project.root.joinpath(dir)
-        for dir in (
-            "model",
-            "extract",
-            "load",
-            "transform",
-            "analyze",
-            "notebook",
-            "orchestrate",
+        files = (
+            project.root.joinpath(file).resolve()
+            for file in ("meltano.yml", "README.md", ".gitignore", "requirements.txt")
         )
-    )
 
-    for file in files:
-        assert file.is_file()
+        dirs = (
+            project.root.joinpath(dir)
+            for dir in (
+                "model",
+                "extract",
+                "load",
+                "transform",
+                "analyze",
+                "notebook",
+                "orchestrate",
+            )
+        )
 
-    for dir in dirs:
-        assert dir.is_dir()
+        for file in files:
+            assert file.is_file()
+
+        for dir in dirs:
+            assert dir.is_dir()
