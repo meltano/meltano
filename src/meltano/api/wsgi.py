790030ddbb07f07cc8e50cd43f71e13ba0998a3a
Douwe Maan <douwe@selenight.nl>
2020-07-02 12:48:40 -0500

    Add ui.forwarded_allow_ips setting

--- src/meltano/api/wsgi.py
+++ src/meltano/api/wsgi.py
@@ -15,6 +15,8 @@ _bind_port = _settings_service.get("ui.bind_port")
 bind = f"{_bind_host}:{_bind_port}"
 
 workers = _settings_service.get("ui.workers")
+forwarded_allow_ips = _settings_service.get("ui.forwarded_allow_ips")
+
 timeout = 600
 
 
