4dac72329621f5a8a8e30e4e238ce02fe344d842
Charles Julian Knight <charles@rabidaudio.com>
2020-07-17 22:10:06 +0000

    created health check route

--- tests/meltano/api/controllers/test_root.py
+++ tests/meltano/api/controllers/test_root.py
@@ -9,3 +9,11 @@ def test_get_default(api, app):
         res = api.get("/this/is__/a/__test")
 
         assert app_response.data == res.data
+
+
+def test_get_health(api, app):
+    with app.test_request_context():
+        res = api.get(url_for("api_root.health"))
+
+        assert res.status_code == 200
+        assert res.json["healthy"]
