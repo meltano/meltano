80715125eead68b801e10b2503a3213f427d7bb3
Douwe Maan <douwe@selenight.nl>
2020-09-18 15:29:03 -0500

    Set job run ID on unsaved jobs

--- src/meltano/core/job/job.py
+++ src/meltano/core/job/job.py
@@ -66,6 +66,7 @@ class Job(SystemModel):
     def __init__(self, **kwargs):
         kwargs["state"] = kwargs.get("state", State.IDLE)
         kwargs["payload"] = kwargs.get("payload", {})
+        kwargs["run_id"] = kwargs.get("run_id", uuid.uuid4())
         super().__init__(**kwargs)
 
     def is_running(self):
