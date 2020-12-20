c23a7f29575433880a4ae1c488f1d26b07a56656
= <suyash.behera458@gmail.com>
2020-12-20 06:46:22 -0800

    fix lint errors

--- tests/meltano/core/test_db_reconnection.py
+++ tests/meltano/core/test_db_reconnection.py
@@ -1,14 +1,18 @@
 from meltano.core.db import check_db_connection
 from unittest.mock import call, Mock
 from sqlalchemy.exc import OperationalError
-import pytest 
+import pytest
+
 
 class TestConnectionRetries:
     def test_ping_failure(self):
         engine = Mock()
 
         # check if OperationalError is raised if a connection can't be made
-        engine.connect.side_effect = OperationalError("test_error", "test_error", "test_error")
+        engine.connect.side_effect = OperationalError(
+            "test_error", 
+            "test_error", "test_error"
+        )
         with pytest.raises(OperationalError):
             check_db_connection(engine, 3, 0.1)
 
@@ -16,8 +20,9 @@ class TestConnectionRetries:
         engine.reset_mock()
         engine.connect.side_effect = [
             OperationalError("test_error", "test_error", "test_error"),
-            None   
+            None,
         ]
         check_db_connection(engine, 3, 0.1)
         calls = [call.connect(), call.connect()]
         engine.assert_has_calls(calls, any_order=True)
+        
