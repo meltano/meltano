a93b6405e0c9bf680a26f6a5b7d27eb58863c78f
Reuben Frankel <rfrankel@matatika.com>
2021-04-11 17:39:34 +0100

    Add plugin_type and plugin_name arguments

--- src/meltano/cli/remove.py
+++ src/meltano/cli/remove.py
@@ -1,11 +1,14 @@
 import click
+from meltano.core.plugin import PluginType
 
 from . import cli
 from .params import pass_project
 
 
 @cli.command()
+@click.argument("plugin_type", type=click.Choice(PluginType.cli_arguments()))
+@click.argument("plugin_name", required=True)
 @pass_project()
 @click.pass_context
-def remove(ctx, project):
+def remove(ctx, project, plugin_type, plugin_name):
     """Remove a plugin from your project."""
