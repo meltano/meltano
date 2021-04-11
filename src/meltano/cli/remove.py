ae027a1e813a5db4b1b1846615ad5e83ceb72c0e
Reuben Frankel <rfrankel@matatika.com>
2021-04-11 17:38:04 +0100

    Make remove command context-aware with pass_context and pass_project; add docstring

--- src/meltano/cli/remove.py
+++ src/meltano/cli/remove.py
@@ -1,6 +1,11 @@
+import click
+
 from . import cli
+from .params import pass_project
 
 
 @cli.command()
-def remove():
-    pass
+@pass_project()
+@click.pass_context
+def remove(ctx, project):
+    """Remove a plugin from your project."""
