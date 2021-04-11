378417f85f853f4342485f5e45f09d86242a81a5
Reuben Frankel <rfrankel@matatika.com>
2021-04-11 18:04:19 +0100

    Initial remove from meltano.yml implementation

--- src/meltano/cli/remove.py
+++ src/meltano/cli/remove.py
@@ -1,5 +1,7 @@
 import click
 from meltano.core.plugin import PluginType
+from meltano.core.plugin.project_plugin import ProjectPlugin
+from meltano.core.project_plugins_service import ProjectPluginsService
 
 from . import cli
 from .params import pass_project
@@ -12,3 +14,8 @@ from .params import pass_project
 @click.pass_context
 def remove(ctx, project, plugin_type, plugin_name):
     """Remove a plugin from your project."""
+
+    plugins_service = ProjectPluginsService(project)
+
+    plugin = ProjectPlugin(PluginType.from_cli_argument(plugin_type), plugin_name)
+    plugins_service.remove_from_file(plugin)
