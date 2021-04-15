d9c565c6799da0bc47cadf836d157627d2c45522
Daniel Walker <danielpdwalker@gmail.com>
2021-04-15 22:50:53 +0100

    Added test_remove_multiple to test_remove.py. Fixed typo in remove_plugin_mock, now remove_plugins_mock.

--- tests/meltano/cli/test_remove.py
+++ tests/meltano/cli/test_remove.py
@@ -1,13 +1,32 @@
 from unittest import mock
 
+import pytest
 from asserts import assert_cli_runner
 from meltano.cli import cli
+from meltano.core.plugin import PluginType
+from meltano.core.project_add_service import PluginAlreadyAddedException
 
 
 class TestCliRemove:
+    @pytest.fixture(scope="class")
+    def tap_gitlab(self, project_add_service):
+        try:
+            return project_add_service.add(PluginType.EXTRACTORS, "tap-gitlab")
+        except PluginAlreadyAddedException as err:
+            return err.plugin
+
     def test_remove(self, project, tap, cli_runner):
-        with mock.patch("meltano.cli.remove.remove_plugins") as remove_plugin_mock:
+        with mock.patch("meltano.cli.remove.remove_plugins") as remove_plugins_mock:
             result = cli_runner.invoke(cli, ["remove", tap.type, tap.name])
             assert_cli_runner(result)
 
-            remove_plugin_mock.assert_called_once_with(project, [tap])
+            remove_plugins_mock.assert_called_once_with(project, [tap])
+
+    def test_remove_multiple(self, project, tap, tap_gitlab, cli_runner):
+        with mock.patch("meltano.cli.remove.remove_plugins") as remove_plugins_mock:
+            result = cli_runner.invoke(
+                cli, ["remove", "extractors", tap.name, tap_gitlab.name]
+            )
+            assert_cli_runner(result)
+
+            remove_plugins_mock.assert_called_once_with(project, [tap, tap_gitlab])
