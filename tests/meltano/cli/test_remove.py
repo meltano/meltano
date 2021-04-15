6fc53335f974c7061481ef43d6ea6ac1a9ba4566
Daniel Walker <danielpdwalker@gmail.com>
2021-04-15 22:35:38 +0100

    Added first test, test_remove to test_remove.py

--- tests/meltano/cli/test_remove.py
+++ tests/meltano/cli/test_remove.py
@@ -1,3 +1,13 @@
+from unittest import mock
+
+from asserts import assert_cli_runner
+from meltano.cli import cli
+
+
 class TestCliRemove:
-    def pass_test(self):
-        pass
+    def test_remove(self, project, tap, cli_runner):
+        with mock.patch("meltano.cli.remove.remove_plugins") as remove_plugin_mock:
+            result = cli_runner.invoke(cli, ["remove", tap.type, tap.name])
+            assert_cli_runner(result)
+
+            remove_plugin_mock.assert_called_once_with(project, [tap])
