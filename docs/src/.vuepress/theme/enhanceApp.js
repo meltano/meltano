20b7589492e028559bf5ec5e5b94f0b4f290eae0
Taylor Murphy <tmurphy@gitlab.com>
2021-05-26 15:46:00 -0500

    Remove Hub page and redirects to page

--- docs/src/.vuepress/theme/enhanceApp.js
+++ docs/src/.vuepress/theme/enhanceApp.js
@@ -21,43 +21,5 @@ export default ({
       }
     });
   }
-    
-    router.addRoutes([
-        { path: '/plugins/extractors/bigquery.html', redirect: '/docs/hub' },
-        { path: '/plugins/extractors/bing.html-ads', redirect: '/docs/hub' },
-        { path: '/plugins/extractors/chargebee.html', redirect: '/docs/hub' },
-        { path: '/plugins/extractors/csv.html', redirect: '/docs/hub' },
-        { path: '/plugins/extractors/facebook.html', redirect: '/docs/hub' },
-        { path: '/plugins/extractors/fastly.html', redirect: '/docs/hub' },
-        { path: '/plugins/extractors/gitlab.html', redirect: '/docs/hub' },
-        { path: '/plugins/extractors/adwords.html', redirect: '/docs/hub' },
-        { path: '/plugins/extractors/google.html-analytics', redirect: '/docs/hub' },
-        { path: '/plugins/extractors/marketo.html', redirect: '/docs/hub' },
-        { path: '/plugins/extractors/mongodb.html', redirect: '/docs/hub' },
-        { path: '/plugins/extractors/mysql.html', redirect: '/docs/hub' },
-        { path: '/plugins/extractors/postgres.html', redirect: '/docs/hub' },
-        { path: '/plugins/extractors/quickbooks.html', redirect: '/docs/hub' },
-        { path: '/plugins/extractors/recharge.html', redirect: '/docs/hub' },
-        { path: '/plugins/extractors/intacct.html', redirect: '/docs/hub' },
-        { path: '/plugins/extractors/salesforce.html', redirect: '/docs/hub' },
-        { path: '/plugins/extractors/shopify.html', redirect: '/docs/hub' },
-        { path: '/plugins/extractors/slack.html', redirect: '/docs/hub' },
-        { path: '/plugins/extractors/spreadsheets.html-anywhere', redirect: '/docs/hub' },
-        { path: '/plugins/extractors/stripe.html', redirect: '/docs/hub' },
-        { path: '/plugins/extractors/zendesk.html', redirect: '/docs/hub' },
-        { path: '/plugins/extractors/zendesk--singer-io.html', redirect: '/docs/hub' },
-        { path: '/plugins/extractors/zoom.html', redirect: '/docs/hub' },
-        { path: '/plugins/loaders/bigquery.html', redirect: '/docs/hub' },
-        { path: '/plugins/loaders/csv.html', redirect: '/docs/hub' },
-        { path: '/plugins/loaders/jsonl.html', redirect: '/docs/hub' },
-        { path: '/plugins/loaders/postgres.html', redirect: '/docs/hub' },
-        { path: '/plugins/loaders/postgres--meltano.html', redirect: '/docs/hub' },
-        { path: '/plugins/loaders/postgres--transferwise.html', redirect: '/docs/hub' },
-        { path: '/plugins/loaders/redshift.html', redirect: '/docs/hub' },
-        { path: '/plugins/loaders/snowflake.html', redirect: '/docs/hub' },
-        { path: '/plugins/loaders/snowflake--meltano.html', redirect: '/docs/hub' },
-        { path: '/plugins/loaders/snowflake--transferwise.html', redirect: '/docs/hub' },
-        { path: '/plugins/loaders/sqlite.html', redirect: '/docs/hub' }
-    ])
 };
 
