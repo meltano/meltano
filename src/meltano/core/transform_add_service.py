cea030ce2ed91e35b7122732af6bb02a00d76ff7
AJ Steers <aaronsteers@gmail.com>
2021-06-10 11:54:08 -0700

    config v2 support for dbt package updates

--- src/meltano/core/transform_add_service.py
+++ src/meltano/core/transform_add_service.py
@@ -48,18 +48,33 @@ class TransformAddService:
             f.write(yaml.dump(package_yaml, default_flow_style=False, sort_keys=False))
 
     def update_dbt_project(self, plugin: ProjectPlugin):
+        """Set transform-specific variables in `dbt_project.yml`.
+
+        The provided `plugin` is expected to be a transform-type plugin, which
+        is to say a dbt package.
+        """
         settings_service = PluginSettingsService(
             self.project, plugin, plugins_service=self.plugins_service
         )
 
         package_name = settings_service.get("_package_name")
-        vars = settings_service.get("_vars")
-
-        model_def = {}
-        if vars:
-            model_def["vars"] = vars
+        package_vars = settings_service.get("_vars")
 
         dbt_project_yaml = yaml.safe_load(self.dbt_project_file.open())
+
+        model_def = {}
+
+        if package_vars:
+            # Add variables scoped to the plugin's package name
+            config_version = dbt_project_yaml.get("config-version", 1)
+            if config_version == 1:
+                model_def["vars"] = package_vars
+            else:
+                project_vars = dbt_project_yaml.get("vars", {})
+                project_vars[package_name] = package_vars
+                dbt_project_yaml["vars"] = project_vars
+
+        # Add the package's folder definition to the list of models:
         dbt_project_yaml["models"][package_name] = model_def
 
         with open(self.dbt_project_file, "w") as f:
