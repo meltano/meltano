meltano/meltano

commit f2424bd42c5e4113c5541118e7ab41c0dbb73575

modified Project to allow for knowledge of absolute path of root, enables directory verification in MMF

src/meltano/core/multiple_meltano_file.py
@@ -31,6 +31,7 @@ EXTRA_KEYS = [
     "included_config_file_contents",
     "included_config_file_component_names",
     "all_config_file_path_names",
+    "project_root",
 ]
 
 
@@ -133,21 +134,34 @@ def clean_components(config: dict):
 def get_included_directories(main_config: dict):
     # Process included paths
     included_directories: List[str] = []
-    try:
-        for path_name in main_config[INCLUDE_PATHS_KEY]:
-            # Verify each path is valid
-            # TODO abs_path = get absolute of path_name, pass to isdir() (Project() should have abs_path)
-            if os.path.isdir(
-                path_name
-            ):  # TODO paths_name must be relative to project root
-                included_directories.append(path_name)
-            else:
+    project_root = Path(main_config["project_root"])
+    included_path_names = main_config.get(INCLUDE_PATHS_KEY)
+
+    if included_path_names is None:
+        # Include paths are optional
+        pass
+    else:
+        for path_name in included_path_names:
+
+            # Enforce relative paths
+            if os.path.isabs(path_name):
                 logger.critical(
-                    f"Included path '{path_name}' is not a valid directory."
+                    f"Included path '{path_name}' is not a relative to the project root."
                 )
                 raise Exception("Invalid Included Path")
-    except KeyError:
-        pass  # Include paths are optional
+
+            # Assumes root is already resolved
+            abs_path_name = str(project_root.joinpath(path_name))
+
+            # Verify each path is valid
+            if os.path.isdir(abs_path_name):
+                included_directories.append(abs_path_name)
+            else:
+                logger.critical(
+                    f"Included path '{abs_path_name}' is not a valid directory."
+                )
+                raise Exception("Invalid Included Path")
+
     return included_directories
 
 

src/meltano/core/project.py
@@ -157,13 +157,19 @@ class Project(Versioned):
 
         return project
 
+    def parse_meltanofile(self, meltanofile):
+        # There is a better way to do this. For now, MMF needs knowledge of root to verify directories upon __init__
+        contents = yaml.safe_load(meltanofile)
+        contents["project_root"] = str(self.root)
+        return MultipleMeltanoFile.parse(contents)
+
     @property
     def meltano(self) -> Dict:
         """Return a copy of the current meltano config"""
         # fmt: off
         with self._meltano_rw_lock.read_lock(), \
-             self.meltanofile.open() as meltanofile:
-            return MultipleMeltanoFile.parse(yaml.safe_load(meltanofile))
+                self.meltanofile.open() as meltanofile:
+            return self.parse_meltanofile(meltanofile)
         # fmt: on
 
     @contextmanager
@@ -182,7 +188,7 @@ class Project(Versioned):
 
             with self.meltanofile.open() as meltanofile:
                 # read the latest version
-                meltano_update = MultipleMeltanoFile.parse(yaml.safe_load(meltanofile))
+                meltano_update = self.parse_meltanofile(meltanofile)
 
             yield meltano_update
 

commit 436c286320958b7390b4c8b2be14897a183359ab

adjustments to pass core/test_initialize.py based on break caused by f2424bd

src/meltano/core/multiple_meltano_file.py
@@ -24,7 +24,6 @@ COMPONENT_KEYS = [
     "schedules",
 ]
 INCLUDE_PATHS_KEY = "include_paths"
-MELTANO_FILE_PATH_NAME = "meltano.yml"
 EXTRA_KEYS = [
     "included_directories",
     "included_config_file_path_names",
@@ -277,8 +276,9 @@ def pop_config_file_data(
     updated_config: dict,
     config_file_path_name: str,
     included_config_file_component_names: Dict[str, dict],
+    is_main_config: bool = True,
 ):
-    if config_file_path_name == MELTANO_FILE_PATH_NAME:
+    if is_main_config:
         updated_config = clean_components(updated_config)
         return updated_config
     else:
@@ -299,7 +299,9 @@ class MultipleMeltanoFile(MeltanoFile):
         attrs["all_config_file_path_names"] = attrs[
             "included_config_file_path_names"
         ].copy()
-        attrs["all_config_file_path_names"].append(MELTANO_FILE_PATH_NAME)
+        attrs["all_config_file_path_names"].append(
+            os.path.join(attrs["root"], "meltano.yml")
+        )
         attrs["included_config_file_contents"] = get_included_config_file_components(
             attrs["included_config_file_path_names"]
         )

src/meltano/core/project.py
@@ -184,7 +184,7 @@ class Project(Versioned):
 
         # fmt: off
         with self._meltano_rw_lock.write_lock(), \
-            self._meltano_ip_lock:
+                self._meltano_ip_lock:
 
             with self.meltanofile.open() as meltanofile:
                 # read the latest version
@@ -196,12 +196,18 @@ class Project(Versioned):
             included_config_file_component_names = meltano_update.get_included_component_names()
             meltano_update = meltano_update.canonical()
 
-            for config_file_path_name in all_config_file_path_names:
+            for idx, config_file_path_name in enumerate(all_config_file_path_names):
+                config_file_path = Path(config_file_path_name)
+                config_file_path = self.root.joinpath(config_file_path)
+                is_main_config = idx == (len(all_config_file_path_names) - 1)
                 try:
-                    config_file_path = Path(config_file_path_name)
                     with atomic_write(config_file_path, overwrite=True) as configfile:
                         # update if everything is fine
-                        config_file_data = pop_config_file_data(meltano_update, config_file_path_name, included_config_file_component_names)
+                        config_file_data = pop_config_file_data(meltano_update,
+                                                                config_file_path_name,
+                                                                included_config_file_component_names,
+                                                                is_main_config,
+                                                                )
                         yaml.dump(config_file_data, configfile, default_flow_style=False, sort_keys=False)
                 except Exception as err:
                     logger.critical(f"Could not update {config_file_path_name}: {err}")