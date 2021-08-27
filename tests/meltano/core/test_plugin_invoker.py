4d116ee8d579b10297f172226d39c737f40e45f9
Florian <syn@ronin.io>
2021-08-27 11:59:00 -0500

    Fixup plugin_invoker tests

--- tests/meltano/core/test_plugin_invoker.py
+++ tests/meltano/core/test_plugin_invoker.py
@@ -10,24 +10,25 @@ from meltano.core.venv_service import VirtualEnv
 
 class TestPluginInvoker:
     @pytest.fixture
-    def plugin_invoker(self, utility, session, plugin_invoker_factory):
+    async def plugin_invoker(self, utility, session, plugin_invoker_factory):
         subject = plugin_invoker_factory(utility)
-        with subject.prepared(session):
+        async with subject.prepared(session):
             yield subject
 
     @pytest.fixture
-    def nonpip_plugin_invoker(self, nonpip_tap, session, plugin_invoker_factory):
+    async def nonpip_plugin_invoker(self, nonpip_tap, session, plugin_invoker_factory):
         subject = plugin_invoker_factory(nonpip_tap)
-        with subject.prepared(session):
+        async with subject.prepared(session):
             yield subject
 
-    def test_env(self, project, tap, session, plugin_invoker_factory):
+    @pytest.mark.asyncio
+    async def test_env(self, project, tap, session, plugin_invoker_factory):
         project.dotenv.touch()
         dotenv.set_key(project.dotenv, "DUMMY_ENV_VAR", "from_dotenv")
         dotenv.set_key(project.dotenv, "TAP_MOCK_TEST", "from_dotenv")
 
         subject = plugin_invoker_factory(tap)
-        with subject.prepared(session):
+        async with subject.prepared(session):
             env = subject.env()
 
         # .env
@@ -52,9 +53,10 @@ class TestPluginInvoker:
         assert env["PATH"].startswith(str(venv.bin_dir))
         assert "PYTHONPATH" not in env
 
-    def test_unknown_command(self, plugin_invoker):
+    @pytest.mark.asyncio
+    async def test_unknown_command(self, plugin_invoker):
         with pytest.raises(UnknownCommandError) as err:
-            plugin_invoker.invoke(command="foo")
+            await plugin_invoker.invoke_async(command="foo")
 
         assert err.value.command == "foo"  # noqa: WPS441
         assert "supports the following commands" in str(err.value)  # noqa: WPS441
@@ -85,9 +87,10 @@ class TestPluginInvoker:
         assert exec_args[0].endswith("utility-mock")
         assert exec_args[1:] == ["--option", "env-var-arg", "extra", "args"]
 
-    def test_undefined_env_var(self, plugin_invoker):
+    @pytest.mark.asyncio
+    async def test_undefined_env_var(self, plugin_invoker):
         with pytest.raises(UndefinedEnvVarError) as err:
-            plugin_invoker.invoke(command="cmd")
+            await plugin_invoker.invoke_async(command="cmd")
 
         assert (
             "Command 'cmd' referenced unset environment variable '$ENV_VAR_ARG' in an argument"
