f9d20fa15fdd66f3ea96680ae929f94881f5f994
Charles Julian Knight <julian@fixdapp.com>
2021-08-26 20:14:10 -0400

    refactor DbtTransformPluginInstaller for linter

--- src/meltano/core/plugin/dbt/base.py
+++ src/meltano/core/plugin/dbt/base.py
@@ -1,3 +1,5 @@
+"""Defines DBT-specific plugins."""
+import logging
 from pathlib import Path
 
 from meltano.core.behavior.hookable import hook
@@ -9,6 +11,8 @@ from meltano.core.plugin_install_service import PluginInstallReason
 from meltano.core.plugin_invoker import PluginInvoker
 from meltano.core.setting_definition import SettingDefinition, SettingKind
 from meltano.core.transform_add_service import TransformAddService
+
+logger = logging.getLogger(__name__)
 
 
 class DbtInvoker(PluginInvoker):
@@ -29,37 +33,38 @@ class DbtTransformPluginInstaller:
 
     async def install(self, reason, clean):
         """Install the transform into the project."""
-        if reason in (PluginInstallReason.ADD, PluginInstallReason.UPGRADE):
-            try:
-                transform_add_service = TransformAddService(self.project)
-
-                # Add repo to my-test-project/transform/packages.yml
-                packages_path = transform_add_service.packages_file.relative_to(
-                    self.project.root
-                )
-                print(
-                    f"Adding dbt package '{self.plugin.pip_url}' to '{packages_path}'..."
-                )
-                transform_add_service.add_to_packages(self.plugin)
-
-                # Add model and vars to my-test-project/transform/dbt_project.yml
-                dbt_project_path = transform_add_service.dbt_project_file.relative_to(
-                    self.project.root
-                )
-                print(f"Adding dbt model to '{dbt_project_path}'...")
-                transform_add_service.update_dbt_project(self.plugin)
-            except PluginNotFoundError:
-                raise PluginInstallError(
-                    "Transformer 'dbt' is not installed. Run `meltano add transformer dbt` to add it to your project."
-                )
-            except FileNotFoundError as err:
-                relative_path = Path(err.filename).relative_to(self.project.root)
-                raise PluginInstallError(
-                    f"File {relative_path}' could not be found. Run `meltano add files dbt` to set up a dbt project."
-                )
-        else:
-            print(
+        if reason not in {PluginInstallReason.ADD, PluginInstallReason.UPGRADE}:
+            logger.info(
                 f"Run `meltano add transform {self.plugin.name}` to re-add this transform to your dbt project."
+            )
+            return
+
+        try:
+            transform_add_service = TransformAddService(self.project)
+
+            # Add repo to my-test-project/transform/packages.yml
+            packages_path = transform_add_service.packages_file.relative_to(
+                self.project.root
+            )
+            logger.info(
+                f"Adding dbt package '{self.plugin.pip_url}' to '{packages_path}'..."
+            )
+            transform_add_service.add_to_packages(self.plugin)
+
+            # Add model and vars to my-test-project/transform/dbt_project.yml
+            dbt_project_path = transform_add_service.dbt_project_file.relative_to(
+                self.project.root
+            )
+            logger.info(f"Adding dbt model to '{dbt_project_path}'...")
+            transform_add_service.update_dbt_project(self.plugin)
+        except PluginNotFoundError:
+            raise PluginInstallError(
+                "Transformer 'dbt' is not installed. Run `meltano add transformer dbt` to add it to your project."
+            )
+        except FileNotFoundError as err:
+            relative_path = Path(err.filename).relative_to(self.project.root)
+            raise PluginInstallError(
+                f"File {relative_path}' could not be found. Run `meltano add files dbt` to set up a dbt project."
             )
 
 
