d9d939d343bd9672f68046e07b637d11e4b21b72
Florian <syn@ronin.io>
2021-09-01 16:52:57 -0500

    cleanup

--- .gitlab/ci/scripts/docker_build_before_script.sh
+++ .gitlab/ci/scripts/docker_build_before_script.sh
@@ -2,4 +2,4 @@ apk add git
 export BASE_SHA=$(git hash-object poetry.lock)
 echo Building Meltano on $BASE_SHA
 export PYTHON_IMAGE_VERSION=$(echo $PYTHON_VERSION | cut -d "." -f1-2)
-echo python_image_version is $PYTHON_IMAGE_VERSION
+echo python_image_version set to $PYTHON_IMAGE_VERSION
