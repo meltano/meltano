a96e7a61199bd5d5fbab22900a61783415ecc7d3
Florian <syn@ronin.io>
2021-09-09 15:40:33 +0000

    Resolve "Bug in CI config causing docker image push failures on python 3.9"

--- .gitlab/ci/scripts/docker_build_before_script.sh
+++ .gitlab/ci/scripts/docker_build_before_script.sh
@@ -1,5 +1,3 @@
 apk add git
 export BASE_SHA=$(git hash-object poetry.lock)
 echo Building Meltano on $BASE_SHA
-export PYTHON_IMAGE_VERSION=$(echo $PYTHON_VERSION | cut -d "." -f1-2)
-echo python_image_version set to $PYTHON_IMAGE_VERSION
