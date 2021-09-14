678d3f5a89437fc47e0725226180b2bd1e2448ac
Taylor A Murphy <taylor@meltano.com>
2021-09-14 15:02:25 +0000

    Update Slack to 1600+

--- docs/src/.vuepress/theme/components/Home.vue
+++ docs/src/.vuepress/theme/components/Home.vue
@@ -20,7 +20,7 @@
           </router-link>
 
           <SlackChannelLink class="action-button secondary">
-            <SlackIcon /> Join 1500+ on Slack
+            <SlackIcon /> Join 1600+ on Slack
           </SlackChannelLink>
 
           <a
