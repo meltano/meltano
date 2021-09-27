80d636b03009c5b982c5613e1ad9311d1eb8d7e2
kgpayne <ken@meltano.com>
2021-09-27 15:04:14 +0100

    Made test idempotent

--- tests/meltano/cli/test_select.py
+++ tests/meltano/cli/test_select.py
@@ -6,7 +6,7 @@ from meltano.cli import cli
 
 
 class TestCliSelect:
-    def test_add_select_pattern(
+    def test_update_select_pattern(
         self, project, cli_runner, tap, project_plugins_service
     ):
         with mock.patch(
@@ -20,24 +20,7 @@ class TestCliSelect:
                 # add select pattern
                 result = cli_runner.invoke(cli, ["select", tap.name, "mock", "*"])
                 assert_cli_runner(result)
-                result = cli_runner.invoke(cli, ["config", "--extras", tap.name])
-                assert_cli_runner(result)
                 # verify pattern was added
-                json_config = json.loads(result.stdout)
-                assert "mock.*" in json_config["_select"]
-
-    def test_remove_select_pattern(
-        self, project, cli_runner, tap, project_plugins_service
-    ):
-        with mock.patch(
-            "meltano.core.select_service.ProjectPluginsService",
-            return_value=project_plugins_service,
-        ):
-            with mock.patch(
-                "meltano.cli.config.ProjectPluginsService",
-                return_value=project_plugins_service,
-            ):
-                # check select pattern exists (this may not be necessary?)
                 result = cli_runner.invoke(cli, ["config", "--extras", tap.name])
                 assert_cli_runner(result)
                 json_config = json.loads(result.stdout)
@@ -47,8 +30,8 @@ class TestCliSelect:
                     cli, ["select", tap.name, "--rm", "mock", "*"]
                 )
                 assert_cli_runner(result)
+                # verify select pattern removed
                 result = cli_runner.invoke(cli, ["config", "--extras", tap.name])
                 assert_cli_runner(result)
-                # verify select pattern removed
                 json_config = json.loads(result.stdout)
                 assert "mock.*" not in json_config["_select"]
