649b6ea32d3b4e99835bdb5d4889e5756454fb66
Amanda Folson <amanda@meltano.com>
2021-11-29 15:09:51 -0500

    Update Slack numbers to 1900

--- docs/src/.vuepress/theme/components/Home.vue
+++ docs/src/.vuepress/theme/components/Home.vue
@@ -20,7 +20,7 @@
           </router-link>
 
           <SlackChannelLink class="action-button secondary">
-            <SlackIcon /> Join 1800+ on Slack
+            <SlackIcon /> Join 1900+ on Slack
           </SlackChannelLink>
 
           <a
