9d1d4739909104fa73fe6739aa64a1df0b00a99d
pnadolny13 <patnadolny@gmail.com>
2022-01-13 17:14:33 -0500

    use pyaml instead of ruamel

--- src/meltano/core/utils/validate_json_schema.py
+++ src/meltano/core/utils/validate_json_schema.py
@@ -2,10 +2,9 @@ import json
 import os
 import pathlib
 
+import yaml
 from jsonschema import validate
-from ruamel.yaml import YAML
 
-yaml = YAML()
 proj_path = pathlib.Path(__file__).resolve().parents[4]
 
 with open(os.path.join(proj_path, "schema/discovery.schema.json"), "r") as schema_file:
@@ -14,5 +13,5 @@ with open(os.path.join(proj_path, "schema/discovery.schema.json"), "r") as schem
 with open(
     os.path.join(proj_path, "src/meltano/core/bundle/discovery.yml"), "r"
 ) as yaml_file:
-    yaml_content = yaml.load(yaml_file)
+    yaml_content = yaml.safe_load(yaml_file)
     validate(instance=yaml_content, schema=schema_content)
