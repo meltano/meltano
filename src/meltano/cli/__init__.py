131e060d8ee83fbd3b33804739a98bfb2318ff0a
Florian Hines <syn@ronin.io>
2022-03-31 18:45:27 -0500

    linter fixes

--- src/meltano/cli/__init__.py
+++ src/meltano/cli/__init__.py
@@ -1,3 +1,4 @@
+"""Main entry point for the meltano CLI."""
 import logging
 import os
 import sys
@@ -41,10 +42,11 @@ logger = logging.getLogger(__name__)
 
 
 def main():
+    """Entry point for the meltano cli."""
     # mark the current process as executed via the `cli`
     os.environ["MELTANO_JOB_TRIGGER"] = os.getenv("MELTANO_JOB_TRIGGER", "cli")
     try:
-        try:
+        try:  # noqa: WPS505
             cli(obj={"project": None})
         except ProjectReadonly as err:
             raise CliError(
@@ -54,6 +56,6 @@ def main():
             raise
         except Exception as err:
             raise CliError(str(err)) from err
-    except CliError as err:
-        err.print()
+    except CliError as cli_error:
+        cli_error.print()
         sys.exit(1)
