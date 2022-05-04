40d0cfc89aa1e54ab13eb36fc957535ef6d2ed7b
Ken Payne <ken@meltano.com>
2022-05-04 15:37:33 +0100

    allow custom transformers

--- src/meltano/cli/add.py
+++ src/meltano/cli/add.py
@@ -77,7 +77,6 @@ def add(
 
     if flags["custom"]:
         if plugin_type in {
-            PluginType.TRANSFORMERS,
             PluginType.TRANSFORMS,
             PluginType.ORCHESTRATORS,
         }:
