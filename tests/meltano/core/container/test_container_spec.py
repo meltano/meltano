73a39393175f4514ea6e5cfdf9a77d8fb8c430fb
Pat Nadolny <patnadolny@gmail.com>
2022-05-10 00:00:32 +0000

    pass container spec entrypoint through to container

--- tests/meltano/core/container/test_container_spec.py
+++ tests/meltano/core/container/test_container_spec.py
@@ -7,33 +7,59 @@ import pytest
 from meltano.core.container.container_spec import ContainerSpec
 
 
-@pytest.fixture
-def spec() -> ContainerSpec:
-    return ContainerSpec(
-        "lightdash/lightdash",
-        ports={"8080": "8000"},
-    )
-
-
 class TestContainerService:
+    @pytest.mark.parametrize(
+        "spec,payload",
+        [
+            (
+                ContainerSpec(
+                    "lightdash/lightdash",
+                    ports={"8080": "8000"},
+                ),
+                {
+                    "Cmd": None,
+                    "Entrypoint": None,
+                    "Image": "lightdash/lightdash",
+                    "Env": [],
+                    "ExposedPorts": {"8000": {}},
+                    "HostConfig": {
+                        "PortBindings": defaultdict(
+                            list,
+                            {
+                                "8000": [
+                                    {
+                                        "HostPort": "8080",
+                                        "HostIP": "0.0.0.0",  # noqa: S104v
+                                    }
+                                ],
+                            },
+                        ),
+                        "Binds": [],
+                    },
+                },
+            ),
+            (
+                ContainerSpec(
+                    "lightdash/lightdash",
+                    entrypoint="bash",
+                ),
+                {
+                    "Cmd": None,
+                    "Entrypoint": "bash",
+                    "Image": "lightdash/lightdash",
+                    "Env": [],
+                    "ExposedPorts": {},
+                    "HostConfig": {
+                        "PortBindings": defaultdict(list, {}),
+                        "Binds": [],
+                    },
+                },
+            ),
+        ],
+        ids=["port-mapping", "custom-entrypoint"],
+    )
     @pytest.mark.asyncio
-    async def test_docker_config(self, spec: ContainerSpec):
+    async def test_docker_config(self, spec: ContainerSpec, payload: dict):
         """Check Docker container config from container spec."""
         config = spec.get_docker_config()
-        assert config == {
-            "Cmd": None,
-            "Image": "lightdash/lightdash",
-            "Env": [],
-            "ExposedPorts": {"8000": {}},
-            "HostConfig": {
-                "PortBindings": defaultdict(
-                    list,
-                    {
-                        "8000": [
-                            {"HostPort": "8080", "HostIP": "0.0.0.0"}  # noqa: S104
-                        ],
-                    },
-                ),
-                "Binds": [],
-            },
-        }
+        assert config == payload
