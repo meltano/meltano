352838e37d91a84ff536bae6f4190121ea675cbf
Aaron ("AJ") Steers <aj@meltano.com>
2022-05-17 16:17:29 -0700

    revert updates to pytests

--- tests/meltano/core/test_plugin_install_service.py
+++ tests/meltano/core/test_plugin_install_service.py
@@ -18,7 +18,7 @@ class TestPluginInstallService:
                             "extractors": [
                                 {
                                     "name": "tap-gitlab",
-                                    "pip_url": "git+https://github.com/MeltanoLabs/tap-gitlab.git",
+                                    "pip_url": "git+https://gitlab.com/meltano/tap-gitlab.git",
                                 },
                                 {
                                     "name": "tap-gitlab--child-1",
