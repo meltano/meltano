0103b6076004dcbf526e10fef52147bad33e1b49
Florian Hines <syn@ronin.io>
2022-05-26 16:44:36 -0500

    Update test_cli to support new syntax

--- tests/meltano/cli/test_schedule.py
+++ tests/meltano/cli/test_schedule.py
@@ -1,11 +1,9 @@
-import os
-from functools import partial
 from unittest import mock
 
 import pytest
+
 from asserts import assert_cli_runner
 from meltano.cli import cli
-from meltano.core.tracking import GoogleAnalyticsTracker
 from meltano.core.utils import iso8601_datetime
 
 
@@ -14,10 +12,11 @@ class TestCliSchedule:
     @mock.patch(
         "meltano.core.schedule_service.PluginSettingsService.get", autospec=True
     )
-    def test_schedule(self, get, session, project, cli_runner, schedule_service):
-        TEST_DATE = "2010-01-01"
-        get.return_value = TEST_DATE
+    def test_elt_schedule(self, get, session, project, cli_runner, schedule_service):
+        test_date = "2010-01-01"
+        get.return_value = test_date
 
+        # test adding a scheduled elt
         with mock.patch(
             "meltano.cli.schedule.ScheduleService", return_value=schedule_service
         ):
@@ -25,9 +24,12 @@ class TestCliSchedule:
                 cli,
                 [
                     "schedule",
-                    "schedule-mock",
+                    "elt-schedule-mock",
+                    "--extractor",
                     "tap-mock",
+                    "--loader",
                     "target-mock",
+                    "--interval",
                     "@eon",
                     "--transform",
                     "run",
@@ -37,22 +39,78 @@ class TestCliSchedule:
         assert_cli_runner(res)
         schedule = schedule_service.schedules()[0]
 
-        assert schedule.name == "schedule-mock"
+        assert schedule.name == "elt-schedule-mock"
         assert schedule.extractor == "tap-mock"
         assert schedule.loader == "target-mock"
         assert schedule.transform == "run"
         assert schedule.interval == "@eon"  # not anytime soon ;)
-        assert schedule.start_date == iso8601_datetime(TEST_DATE)
+        assert schedule.start_date == iso8601_datetime(test_date)
+
+        # test adding a scheduled job
+        with mock.patch(
+            "meltano.cli.schedule.ScheduleService", return_value=schedule_service
+        ):
+            res = cli_runner.invoke(
+                cli,
+                [
+                    "schedule",
+                    "job-schedule-mock",
+                    "--job",
+                    "mock-job",
+                    "--interval",
+                    "@eon",
+                ],
+            )
+        assert res.exit_code == 0
+
+        assert_cli_runner(res)
+        schedule = schedule_service.schedules()[1]
+
+        assert schedule.name == "job-schedule-mock"
+        assert schedule.job == "mock-job"
+        assert schedule.interval == "@eon"  # not anytime soon ;)
+
+        # verify that you can't use job and elt flags together
+        with mock.patch(
+            "meltano.cli.schedule.ScheduleService", return_value=schedule_service
+        ):
+            res = cli_runner.invoke(
+                cli,
+                [
+                    "schedule",
+                    "job-schedule-mock",
+                    "--job",
+                    "mock-job",
+                    "--extractor",
+                    "tap-mock",
+                    "--loader",
+                    "target-mock",
+                    "--interval",
+                    "@eon",
+                    "--transform",
+                    "run",
+                ],
+            )
+
+        assert res.exit_code == 1
 
     @pytest.mark.parametrize("exit_code", [0, 1, 143])
-    def test_schedule_run(self, exit_code, cli_runner, schedule):
+    def test_schedule_run(self, exit_code, cli_runner, elt_schedule, job_schedule):
         process_mock = mock.Mock(returncode=exit_code)
         with mock.patch(
             "meltano.cli.schedule.ScheduleService.run", return_value=process_mock
         ) as run_mock:
             res = cli_runner.invoke(
-                cli, ["schedule", "run", schedule.name, "--transform", "run"]
+                cli, ["schedule", "run", elt_schedule.name, "--transform", "run"]
             )
             assert res.exit_code == exit_code
+            run_mock.assert_called_once_with(elt_schedule, "--transform", "run")
 
-            run_mock.assert_called_once_with(schedule, "--transform", "run")
+        with mock.patch(
+            "meltano.cli.schedule.ScheduleService.run", return_value=process_mock
+        ) as run_mock:
+            res = cli_runner.invoke(
+                cli, ["schedule", "run", job_schedule.name, "--dry-run"]
+            )
+            assert res.exit_code == exit_code
+            run_mock.assert_called_once_with(job_schedule, "--dry-run")
