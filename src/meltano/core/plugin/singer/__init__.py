5f192293cb492d44a181d808da1c9794ac4822e3
Aaron ("AJ") Steers <aj@meltano.com>
2022-06-04 22:54:40 -0700

    add explicit import

--- src/meltano/core/plugin/singer/__init__.py
+++ src/meltano/core/plugin/singer/__init__.py
@@ -2,19 +2,20 @@ from typing import Dict
 
 from meltano.core.plugin import PluginType
 
-# from .base import *  # TODO: delete
+from .base import SingerPlugin
 from .catalog import ListExecutor, SelectExecutor
 from .mapper import SingerMapper
 from .tap import SingerTap
 from .target import BookmarkWriter, SingerTarget
 
 __all__ = [
+    "BookmarkWriter",
     "Dict",
-    "PluginType",
     "ListExecutor",
+    "PluginType",
     "SelectExecutor",
     "SingerMapper",
+    "SingerPlugin",
     "SingerTap",
-    "BookmarkWriter",
     "SingerTarget",
 ]
