2b591db74afe74fb96406c5e24748c62e3b8cd1d
Florian Hines <syn@ronin.io>
2022-06-08 14:51:53 -0500

    Add telemetry to install command

--- src/meltano/cli/install.py
+++ src/meltano/cli/install.py
@@ -1,8 +1,14 @@
+"""CLI command `meltano install`."""
+from __future__ import annotations
+
 import click
 
 from meltano.core.legacy_tracking import LegacyTracker
 from meltano.core.plugin import PluginType
 from meltano.core.project_plugins_service import ProjectPluginsService
+from meltano.core.tracking import PluginsTrackingContext, Tracker
+from meltano.core.tracking import cli as cli_tracking
+from meltano.core.tracking import cli_context_builder
 
 from . import cli
 from .params import pass_project
@@ -33,22 +39,44 @@ def install(project, plugin_type, plugin_name, clean, parallelism):
 
     \b\nRead more at https://www.meltano.com/docs/command-line-interface.html#install
     """
+    tracker = Tracker(project)
+    tracker.add_contexts(
+        cli_context_builder(
+            "install",
+            None,
+            clean=clean,
+            parallelism=parallelism,
+        )
+    )
+
     plugins_service = ProjectPluginsService(project)
 
     if plugin_type:
-        plugin_type = PluginType.from_cli_argument(plugin_type)
+        try:
+            plugin_type = PluginType.from_cli_argument(plugin_type)
+        except ValueError:
+            # if we fail because plugin_type is not valid we have no plugins to instrument
+            tracker.track_command_event(cli_tracking.STARTED)
+            tracker.track_command_event(cli_tracking.ABORTED)
+            raise
         plugins = plugins_service.get_plugins_of_type(plugin_type)
         if plugin_name:
-            plugins = [p for p in plugins if p.name in plugin_name]
+            plugins = [plugin for plugin in plugins if plugin.name in plugin_name]
     else:
         plugins = list(plugins_service.plugins())
 
     click.echo(f"Installing {len(plugins)} plugins...")
+    tracker.add_contexts(
+        PluginsTrackingContext([(candidate, None) for candidate in plugins])
+    )
+    tracker.track_command_event(cli_tracking.STARTED)
 
     success = install_plugins(project, plugins, parallelism=parallelism, clean=clean)
 
-    tracker = LegacyTracker(project)
-    tracker.track_meltano_install()
+    legacy_tracker = LegacyTracker(project)
+    legacy_tracker.track_meltano_install()
 
     if not success:
+        tracker.track_command_event(cli_tracking.FAILED)
         raise CliError("Failed to install plugin(s)")
+    tracker.track_command_event(cli_tracking.COMPLETED)
