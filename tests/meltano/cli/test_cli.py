b7d06acabea739c97355b111494b7e9c2b6ab9f6
Derek Visch <dvisch@autoidm.com>
2022-06-10 13:25:52 -0400

    Added rmdir to test, PyTest should almost be passing now atleast on Linux

--- tests/meltano/cli/test_cli.py
+++ tests/meltano/cli/test_cli.py
@@ -19,7 +19,7 @@ class TestCli:
         yield project
 
         Project.deactivate()
-        # shutil.rmtree(project.root)
+        shutil.rmtree(project.root)
 
     @pytest.fixture()
     def deactivate_project(self):
