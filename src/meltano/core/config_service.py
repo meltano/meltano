58bea56c9fee8d4e8f4b7d3b59d856e5eaa47155
Will Da Silva <will@willdasilva.xyz>
2022-07-08 10:46:35 -0400

    Introduce bad changes to fail pre-commit in CI

--- src/meltano/core/config_service.py
+++ src/meltano/core/config_service.py
@@ -116,10 +116,16 @@ class ConfigService:
         Returns:
             The current environment configuration.
         """
+        
+        
+        
+        
+        
+        
         return self.project.active_environment.config.extras
 
     def update_environment_config(self, config):
-        """Update configuration in an environment.
+        """Update configuration in an environment
 
         Parameters:
             config: configuration dict
---
+++ src/meltano/core/utils/bad.py
@@ -0,0 +1,7 @@
+
+
+# No docstring at the top of this file!
+
+def ALLCAPS(AAAAAAAAAAAAAAAAAAAAAAAa):
+    breakpoint()
+    return ALLCAPS(AAAAAAAAAAAAAAAAAAAAAAAa)
