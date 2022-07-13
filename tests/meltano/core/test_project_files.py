7bdee9531144bed50596488d4bd52cd3da34a4e7
Edgar R. M <edgarrm358@gmail.com>
2022-07-13 12:45:51 +0200

    feat: Preserve comments and flow style in `meltano.yml` (#6308)
    * Add ruamel.yaml dependency
    
    * Add failing test
    
    * Preserve comments and format in Canonical objects
    
    * Test style preservation
    
    * Use Path.read_text
    
    * Test that key order is preserved
    
    * More complete yaml fixture
    
    - Add jobs example to yaml fixture
    - Test TODOs that will get removed
    
    * Copy yaml attributes after entries have been added
    
    * Make sure project file features are present before copying attributes
    
    * Refactor into method
    
    * Ensure entry order is preserved in included files
    
    * Support jobs key in multifile setup
    
    * Restore user order for all keys in meltano.yml
    
    * Handle new keys separately
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add integration test
    
    * Revert "[pre-commit.ci] auto fixes from pre-commit.com hooks"
    
    This reverts commit e4d4705ea949db5b62d37f3b18235ef9d1920194.
    
    * Actually add integration test to matrix
    
    * Add missing block shell language
    
    * Test files left blank
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/meltano/core/test_project_files.py
+++ tests/meltano/core/test_project_files.py
@@ -4,10 +4,13 @@ import os
 import platform
 import tempfile
 from pathlib import Path
+from textwrap import dedent
 
-import pytest  # noqa: F401
+import pytest
 import yaml
 from jsonschema import validate
+
+from meltano.core.project_files import deep_merge
 
 
 @pytest.fixture
@@ -30,6 +33,19 @@ def cd_temp_dir():
     os.chdir(original_dir)
 
 
+@pytest.mark.parametrize(
+    "parent,children,expected",
+    [
+        ({"a": 1}, [{"a": 1}], {"a": 1}),
+        ({"a": 1}, [{"a": 2}], {"a": 2}),
+        ({"a": 1}, [{"a": 2, "b": 2}], {"a": 2, "b": 2}),
+        ({"a": [1, 2, 3]}, [{"a": [3, 4, 5]}], {"a": [1, 2, 3, 3, 4, 5]}),
+    ],
+)
+def test_deep_merge(parent, children, expected):
+    assert deep_merge(parent, children) == expected
+
+
 class TestProjectFiles:
     def test_resolve_subfiles(self, project_files):
         assert project_files._meltano_file_path == (project_files.root / "meltano.yml")
@@ -43,7 +59,18 @@ class TestProjectFiles:
                 "./*/**/subconfig_[0-9].yml",
             ],
             "plugins": {
-                "extractors": [{"name": "tap-meltano-yml"}],
+                "extractors": [
+                    {
+                        "name": "tap-meltano-yml",
+                        "settings": [
+                            {
+                                "name": "token",
+                                "kind": "password",
+                                "description": "Token for the API. This is a secret.",
+                            }
+                        ],
+                    }
+                ],
                 "mappers": [
                     {
                         "name": "map-meltano-yml",
@@ -64,6 +91,14 @@ class TestProjectFiles:
             ],
             "environments": [
                 {"name": "test-meltano-environment", "env": {"TEST": "TEST-MELTANO"}}
+            ],
+            "jobs": [
+                {
+                    "name": "my-job",
+                    "tasks": [
+                        "tap-meltano-yml map-meltano-yml target-meltano-yml",
+                    ],
+                },
             ],
         }
         assert project_files.include_paths == [
@@ -144,7 +179,16 @@ class TestProjectFiles:
             ],
             "plugins": {
                 "extractors": [
-                    {"name": "tap-meltano-yml"},
+                    {
+                        "name": "tap-meltano-yml",
+                        "settings": [
+                            {
+                                "name": "token",
+                                "kind": "password",
+                                "description": "Token for the API. This is a secret.",
+                            }
+                        ],
+                    },
                     {"name": "tap-subconfig-2-yml"},
                     {"name": "tap-subconfig-1-yml"},
                 ],
@@ -197,6 +241,14 @@ class TestProjectFiles:
                     "env": {"TEST": "TEST-SUBCONFIG-1-YML"},
                 },
             ],
+            "jobs": [
+                {
+                    "name": "my-job",
+                    "tasks": [
+                        "tap-meltano-yml map-meltano-yml target-meltano-yml",
+                    ],
+                },
+            ],
         }
         read_result = project_files.load()
         assert read_result == expected_result
@@ -223,7 +275,16 @@ class TestProjectFiles:
             ],
             "plugins": {
                 "extractors": [
-                    {"name": "tap-meltano-yml"},
+                    {
+                        "name": "tap-meltano-yml",
+                        "settings": [
+                            {
+                                "name": "token",
+                                "kind": "password",
+                                "description": "Token for the API. This is a secret.",
+                            }
+                        ],
+                    },
                     {"name": "modified-tap-subconfig-2-yml"},
                     {"name": "tap-subconfig-1-yml"},
                 ],
@@ -276,7 +337,151 @@ class TestProjectFiles:
                     "env": {"TEST": "TEST-SUBCONFIG-1-YML"},
                 },
             ],
+            "jobs": [
+                {
+                    "name": "my-job",
+                    "tasks": [
+                        "tap-meltano-yml map-meltano-yml target-meltano-yml",
+                    ],
+                },
+            ],
             "version": 2,
         }
         read_result = project_files.load()
         assert read_result == expected_result
+
+    def test_preserve_format(self, project_files):
+        meltano_config = project_files.load()
+        meltano_config["version"] = 3
+        meltano_config["schedules"][0]["transform"] = "only"
+        meltano_config["schedules"][0].yaml_add_eol_comment(
+            "Only update dbt models\n",
+            "transform",
+        )
+
+        project_files.update(meltano_config)
+
+        contents = project_files._meltano_file_path.read_text()
+
+        expected_contents = """\
+            # Top-level comment
+            version: 3
+            default_environment: test-meltano-environment
+            database_uri: sqlite:///.meltano/meltano.db
+
+            include_paths:
+            - ./subconfig_[0-9].yml
+            # Config files inside a subfolder
+            - ./*/subconfig_[0-9].yml
+            - ./*/**/subconfig_[0-9].yml
+
+            schedules:
+            # My schedules
+            - name: modified-test-meltano-yml
+              extractor: tap-meltano-yml
+              loader: target-meltano-yml
+              transform: only  # Only update dbt models
+              start_date: 2020-08-05 00:00:00
+              interval: '@once' # Run only once
+
+            jobs:  # My jobs
+            # An EL job with mapping
+            - name: my-job
+              tasks:
+              - >-
+                tap-meltano-yml
+                map-meltano-yml
+                target-meltano-yml
+
+            environments:
+            # My meltano environments
+            - name: test-meltano-environment
+              env:
+                TEST: TEST-MELTANO
+
+            plugins:
+              # Project plugins
+              extractors:
+              - name: tap-meltano-yml # Comment on array element
+                settings:
+                - name: token
+                  kind: password
+                  description: >-
+                    Token for the API.
+                    This is a secret.
+
+              - name: modified-tap-subconfig-2-yml
+
+              mappers:
+              # My mappers
+              - name: map-meltano-yml
+                # These are some mappings
+                mappings:
+                - name: transform-meltano-yml
+
+              loaders:
+              - name: target-meltano-yml
+              - name: modified-target-subconfig-1-yml
+
+        """
+
+        assert contents == dedent(expected_contents)
+
+        expected_subconfig_2_contents = """\
+            plugins:
+              # Subconfig 2 Plugins
+              loaders:
+              - name: target-subconfig-2-yml  # Subconfig 2 Loader
+
+            schedules:
+            # Subconfig 2 Schedules
+            - name: test-subconfig-2-yml
+              extractor: tap-subconfig-2-yml
+              loader: target-subconfig-2-yml
+              transform: skip
+              start_date: 2020-08-04 00:00:00
+              interval: '@once' # Run only once
+
+            environments:
+            # Subconfig 2 Environments
+            - name: test-subconfig-2-yml
+              env:
+                TEST: TEST-SUBCONFIG-2-YML
+        """
+
+        included_path = project_files.root / "subconfig_2.yml"
+        assert included_path.read_text() == dedent(expected_subconfig_2_contents)
+
+    def test_remove_all_file_contents(self, project_files):
+        meltano_config = project_files.load()
+        meltano_config["plugins"]["extractors"] = [
+            entry
+            for entry in meltano_config["plugins"]["extractors"]
+            if entry["name"] != "tap-subconfig-2-yml"
+        ]
+        meltano_config["plugins"]["loaders"] = [
+            entry
+            for entry in meltano_config["plugins"]["loaders"]
+            if entry["name"] != "target-subconfig-2-yml"
+        ]
+        meltano_config["schedules"] = [
+            entry
+            for entry in meltano_config["schedules"]
+            if entry["name"] != "test-subconfig-2-yml"
+        ]
+        meltano_config["environments"] = [
+            entry
+            for entry in meltano_config["environments"]
+            if entry["name"] != "test-subconfig-2-yml"
+        ]
+        project_files.update(meltano_config)
+
+        expected_subconfig_2_contents = """\
+        plugins: {}
+        schedules: []
+        jobs: []
+        environments: []
+        """
+
+        included_path = project_files.root / "subconfig_2.yml"
+        assert included_path.read_text() == dedent(expected_subconfig_2_contents)
