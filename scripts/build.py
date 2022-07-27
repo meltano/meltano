7c66308705b9fe44ee9bc91c6e7ac91157c83d8b
Will Da Silva <will@willdasilva.xyz>
2022-07-27 12:06:35 -0400

    Replace `scripts/bundle_ui.sh` with `scripts/build.py`

--- scripts/build.py
+++ scripts/build.py
@@ -1,30 +1,40 @@
+#!/usr/bin/env python
+
+"""Build script for Meltano wheels that builds and bundles the Meltano UI.
+
+This script is run automatically by Poetry when building a wheel.
+
+It can also be run manually for the purpose of UI development.
+"""
+
 import os
 import shutil
 import subprocess
+from pathlib import Path
+
+root_dir = Path(__file__).parent.parent.resolve()
+webapp_dir = root_dir / "src" / "webapp"
+dist_dir = webapp_dir / "dist"
+api_dir = root_dir / "src" / "meltano" / "api"
 
 
 def build():
+    """Build & bundle the Meltano UI - executed automatically by Poetry when building the wheel."""
+    # Operate from the root of the project
+    os.chdir(root_dir)
+
     # Build static web app
-    subprocess.run(["yarn", "install", "--immutable"], cwd="src/webapp")
-    subprocess.run(["yarn", "build"], cwd="src/webapp")
+    subprocess.run(["yarn", "install", "--immutable"], cwd=webapp_dir)  # noqa: S607
+    subprocess.run(["yarn", "build"], cwd=webapp_dir)  # noqa: S607
 
     # Copy static files
-    os.makedirs("src/meltano/api/templates", exist_ok=True)
-    shutil.copy(
-        "src/webapp/dist/index.html",
-        "src/meltano/api/templates/webapp.html",
-    )
-    shutil.copy(
-        "src/webapp/dist/index-embed.html",
-        "src/meltano/api/templates/embed.html",
-    )
+    os.makedirs(api_dir / "templates", exist_ok=True)
+    shutil.copy(dist_dir / "index.html", api_dir / "templates" / "webapp.html")
+    shutil.copy(dist_dir / "index-embed.html", api_dir / "templates" / "embed.html")
 
-    for dst in {"css", "js"}:
-        shutil.rmtree(f"src/meltano/api/static/{dst}", ignore_errors=True)
-        shutil.copytree(
-            f"src/webapp/dist/static/{dst}",
-            f"src/meltano/api/static/{dst}",
-        )
+    for dst in ("css", "js"):
+        shutil.rmtree(api_dir / "static" / dst, ignore_errors=True)
+        shutil.copytree(dist_dir / "static" / dst, api_dir / "static" / dst)
 
 
 if __name__ == "__main__":
