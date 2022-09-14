9b8194a1bc43a273ab8252fd6fcb39e444c7570d
Cody J. Hanson <cody@meltano.com>
2022-09-14 16:29:06 -0500

    Add more state_store tests, parse job history during migration

--- src/meltano/migrations/versions/f4c225a9492f_create_dedicated_job_state_table.py
+++ src/meltano/migrations/versions/f4c225a9492f_create_dedicated_job_state_table.py
@@ -7,11 +7,16 @@ Create Date: 2022-09-02 09:44:05.581824
 """
 from __future__ import annotations
 
+import uuid
+
 import sqlalchemy as sa
 from alembic import op
+from sqlalchemy import Column, MetaData, func, types
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.mutable import MutableDict
+from sqlalchemy.orm.session import Session
 
-from meltano.core.sqlalchemy import JSONEncodedDict
+from meltano.core.sqlalchemy import GUID, IntFlag, JSONEncodedDict
 from meltano.migrations.utils.dialect_typing import (
     get_dialect_name,
     max_string_length_for_dialect,
@@ -22,6 +27,134 @@ revision = "f4c225a9492f"
 down_revision = "5b43800443d1"
 branch_labels = None
 depends_on = None
+
+
+SystemMetadata = MetaData()
+SystemModel = declarative_base(metadata=SystemMetadata)
+
+# Copied from core/job_state.py
+class JobState(SystemModel):
+    """Model class that represents the current state of a given job.
+
+    Modified during `meltano elt` or `meltano run` runs whenever a
+    STATE message is emitted by a Singer target. Also written and read
+    by `meltano state` CLI invocations. Only holds the _current_ state
+    for a given job_name. Full job run history is held by the Job model.
+    """
+
+    __tablename__ = "state"
+    job_name = Column(types.String, unique=True, primary_key=True)
+
+    updated_at = Column(
+        types.TIMESTAMP, server_default=func.now(), onupdate=func.current_timestamp()
+    )
+
+    partial_state = Column(MutableDict.as_mutable(JSONEncodedDict))
+    completed_state = Column(MutableDict.as_mutable(JSONEncodedDict))
+
+    @classmethod
+    def from_job_history(cls, session: Session, job_name: str):
+        """Build JobState from job run history.
+
+        Args:
+            session: the session to use in finding job history
+            job_name: the name of the job to build JobState for
+
+        Returns:
+            JobState built from job run history
+        """
+        completed_state = {}
+        partial_state = {}
+        incomplete_since = None
+        finder = JobFinder(job_name)
+
+        # Get the state for the most recent completed job.
+        # Do not consider dummy jobs create via add_state.
+        state_job = finder.latest_with_payload(session, flags=Payload.STATE)
+        if state_job:
+            incomplete_since = state_job.ended_at
+            if "singer_state" in state_job.payload:
+                merge(state_job.payload, partial_state)
+
+        # If there have been any incomplete jobs since the most recent completed jobs,
+        # merge the state emitted by those jobs into the state for the most recent
+        # completed job. If there are no completed jobs, get the full history of
+        # incomplete jobs and use the most recent state emitted per stream
+        incomplete_state_jobs = finder.with_payload(
+            session, flags=Payload.INCOMPLETE_STATE, since=incomplete_since
+        )
+        for incomplete_state_job in incomplete_state_jobs:
+            if "singer_state" in incomplete_state_job.payload:
+                partial_state = merge(incomplete_state_job.payload, partial_state)
+        return cls(
+            job_name=job_name,
+            partial_state=partial_state,
+            completed_state=completed_state,
+        )
+
+
+# Copied from core/job/job.py
+class Job(SystemModel):  # noqa: WPS214
+    """Model class that represents a `meltano elt` run in the system database.
+
+    Includes State.STATE_EDIT rows which represent CLI invocations of the
+    `meltano state` command which wrote state to the db. Queries that are
+    meant to return only actual job runs should filter out records with
+    state == State.STATE_EDIT.
+    """
+
+    __tablename__ = "runs"
+
+    id = Column(types.Integer, primary_key=True)
+    job_name = Column(types.String)
+    run_id = Column(GUID, nullable=False, default=uuid.uuid4)
+    _state = Column(name="state", type_=types.String)
+    started_at = Column(types.DateTime)
+    last_heartbeat_at = Column(types.DateTime)
+    ended_at = Column(types.DateTime)
+    payload = Column(MutableDict.as_mutable(JSONEncodedDict))
+    payload_flags = Column(IntFlag, default=0)
+    trigger = Column(types.String, default="")
+
+    @classmethod
+    def from_job_history(cls, session: Session, job_name: str):
+        """Build JobState from job run history.
+
+        Args:
+            session: the session to use in finding job history
+            job_name: the name of the job to build JobState for
+
+        Returns:
+            JobState built from job run history
+        """
+        completed_state = {}
+        partial_state = {}
+        incomplete_since = None
+        finder = JobFinder(job_name)
+
+        # Get the state for the most recent completed job.
+        # Do not consider dummy jobs create via add_state.
+        state_job = finder.latest_with_payload(session, flags=Payload.STATE)
+        if state_job:
+            incomplete_since = state_job.ended_at
+            if "singer_state" in state_job.payload:
+                merge(state_job.payload, partial_state)
+
+        # If there have been any incomplete jobs since the most recent completed jobs,
+        # merge the state emitted by those jobs into the state for the most recent
+        # completed job. If there are no completed jobs, get the full history of
+        # incomplete jobs and use the most recent state emitted per stream
+        incomplete_state_jobs = finder.with_payload(
+            session, flags=Payload.INCOMPLETE_STATE, since=incomplete_since
+        )
+        for incomplete_state_job in incomplete_state_jobs:
+            if "singer_state" in incomplete_state_job.payload:
+                partial_state = merge(incomplete_state_job.payload, partial_state)
+        return cls(
+            job_name=job_name,
+            partial_state=partial_state,
+            completed_state=completed_state,
+        )
 
 
 def upgrade():
@@ -49,7 +182,13 @@ def upgrade():
         sa.UniqueConstraint("job_name"),
     )
 
+    session = Session(bind=op.get_bind())
+    for job_run in session.query(Job).all():
+        session.add(JobState.from_job_history(session, job_run.job_name))
+    session.commit()
+
 
 def downgrade():
     # Remove job_state table
+    # Job run history is still maintained, so no need to copy
     op.drop_table("state")
--- tests/meltano/core/test_state_store.py
+++ tests/meltano/core/test_state_store.py
@@ -1,35 +1,79 @@
 from __future__ import annotations
 
+import json
+
 import pytest
 
 from meltano.core.job_state import JobState
 from meltano.core.state_store import DBStateStoreManager
+from meltano.core.utils import merge
 
 
 class TestDBStateStoreManager:
     @pytest.fixture
-    def subject(self, session):
-        mock_state = JobState(
-            job_name="mock_job",
-            partial_state={
-                "singer_state_1": "mock_partial_state_1",
-                "singer_state_2": "mock_partial_state_1",
-            },
-            completed_state={
-                "singer_state_1": "mock_partial_state_0",
-                "singer_state_3": "mock_partial_state_3",
-            },
-        )
-        session.add(mock_state)
-        session.commit()
-        return DBStateStoreManager(session=session)
+    def subject(self, job_history_session, state_ids_with_jobs):
+        return DBStateStoreManager(session=job_history_session)
 
-    def test_get_state(self, subject: DBStateStoreManager):
-        assert subject.get("mock_job") == {
-            "singer_state_1": "mock_partial_state_1",
-            "singer_state_2": "mock_partial_state_1",
-            "singer_state_3": "mock_partial_state_3",
-        }
+    def test_get_state(
+        self, subject: DBStateStoreManager, state_ids_with_expected_states
+    ):
+        for (state_id, expected_state) in state_ids_with_expected_states:
+            assert subject.get(state_id) == expected_state
 
     def test_set_state(self, subject: DBStateStoreManager):
-        ...
+        partial_state = {"singer_state": {"partial": 1}}
+        complete_state = {"singer_state": {"complete": 1}}
+        overwritten_partial = {"singer_state": {"partial": "overwritten"}}
+        overwritten_complete = {"singer_state": {"complete": "overwritten"}}
+        second_partial = {"singer_state": {"partial_2": 2}}
+
+        mock_state_with_only_partial = JobState(
+            job_name="mock_state_with_only_partial", partial_state=partial_state
+        )
+        mock_state_with_only_complete = JobState(
+            job_name="mock_state_with_only_complete", completed_state=complete_state
+        )
+
+        subject.set(
+            mock_state_with_only_partial.job_name, json.dumps(partial_state), False
+        )
+        assert subject.get(mock_state_with_only_partial.job_name) == partial_state
+
+        subject.set(
+            mock_state_with_only_complete.job_name, json.dumps(complete_state), True
+        )
+        assert subject.get(mock_state_with_only_complete.job_name) == complete_state
+
+        subject.set(
+            mock_state_with_only_complete.job_name, json.dumps(partial_state), False
+        )
+        assert subject.get(mock_state_with_only_complete.job_name) == merge(
+            partial_state, complete_state
+        )
+
+        subject.set(
+            mock_state_with_only_complete.job_name,
+            json.dumps(overwritten_complete),
+            True,
+        )
+
+        assert (
+            subject.get(mock_state_with_only_complete.job_name) == overwritten_complete
+        )
+
+        subject.set(
+            mock_state_with_only_partial.job_name,
+            json.dumps(overwritten_partial),
+            False,
+        )
+        assert subject.get(mock_state_with_only_partial.job_name) == overwritten_partial
+
+        subject.set(
+            mock_state_with_only_partial.job_name, json.dumps(second_partial), False
+        )
+        assert subject.get(mock_state_with_only_partial.job_name) == merge(
+            overwritten_partial, second_partial
+        )
+
+    def test_get_job_names(self, subject: DBStateStoreManager, state_ids_with_jobs):
+        assert set(subject.get_job_names()) == set(state_ids_with_jobs.keys())
