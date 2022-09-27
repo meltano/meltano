361ec7a8ee1136a12b0307ddabd4372ff940cbb6
Cody J. Hanson <cody@meltano.com>
2022-09-26 19:15:54 -0500

    fix: Correct migration path for state table (#6795)

--- tests/meltano/core/test_state_store.py
+++ tests/meltano/core/test_state_store.py
@@ -28,52 +28,52 @@ class TestDBStateStoreManager:
         second_partial = {"singer_state": {"partial_2": 2}}
 
         mock_state_with_only_partial = JobState(
-            job_name="mock_state_with_only_partial", partial_state=partial_state
+            state_id="mock_state_with_only_partial", partial_state=partial_state
         )
         mock_state_with_only_complete = JobState(
-            job_name="mock_state_with_only_complete", completed_state=complete_state
+            state_id="mock_state_with_only_complete", completed_state=complete_state
         )
 
         subject.set(
-            mock_state_with_only_partial.job_name, json.dumps(partial_state), False
+            mock_state_with_only_partial.state_id, json.dumps(partial_state), False
         )
-        assert subject.get(mock_state_with_only_partial.job_name) == partial_state
+        assert subject.get(mock_state_with_only_partial.state_id) == partial_state
 
         subject.set(
-            mock_state_with_only_complete.job_name, json.dumps(complete_state), True
+            mock_state_with_only_complete.state_id, json.dumps(complete_state), True
         )
-        assert subject.get(mock_state_with_only_complete.job_name) == complete_state
+        assert subject.get(mock_state_with_only_complete.state_id) == complete_state
 
         subject.set(
-            mock_state_with_only_complete.job_name, json.dumps(partial_state), False
+            mock_state_with_only_complete.state_id, json.dumps(partial_state), False
         )
-        assert subject.get(mock_state_with_only_complete.job_name) == merge(
+        assert subject.get(mock_state_with_only_complete.state_id) == merge(
             partial_state, complete_state
         )
 
         subject.set(
-            mock_state_with_only_complete.job_name,
+            mock_state_with_only_complete.state_id,
             json.dumps(overwritten_complete),
             True,
         )
 
         assert (
-            subject.get(mock_state_with_only_complete.job_name) == overwritten_complete
+            subject.get(mock_state_with_only_complete.state_id) == overwritten_complete
         )
 
         subject.set(
-            mock_state_with_only_partial.job_name,
+            mock_state_with_only_partial.state_id,
             json.dumps(overwritten_partial),
             False,
         )
-        assert subject.get(mock_state_with_only_partial.job_name) == overwritten_partial
+        assert subject.get(mock_state_with_only_partial.state_id) == overwritten_partial
 
         subject.set(
-            mock_state_with_only_partial.job_name, json.dumps(second_partial), False
+            mock_state_with_only_partial.state_id, json.dumps(second_partial), False
         )
-        assert subject.get(mock_state_with_only_partial.job_name) == merge(
+        assert subject.get(mock_state_with_only_partial.state_id) == merge(
             overwritten_partial, second_partial
         )
 
-    def test_get_job_names(self, subject: DBStateStoreManager, state_ids_with_jobs):
-        assert set(subject.get_job_names()) == set(state_ids_with_jobs.keys())
+    def test_get_state_ids(self, subject: DBStateStoreManager, state_ids_with_jobs):
+        assert set(subject.get_state_ids()) == set(state_ids_with_jobs.keys())
