7bbc90b1b144a82e6151a7409b51ea6643201052
Will Da Silva <will@willdasilva.xyz>
2022-10-05 18:11:04 -0400

    test: Remove unnecessary flaky test `test_open_browser` (#6856)

--- src/meltano/api/workers/ui_available_worker.py
+++ src/meltano/api/workers/ui_available_worker.py
@@ -1,10 +1,11 @@
+"""Meltano UI worker thread definition."""
+
 from __future__ import annotations
 
 import logging
 import threading
 import time
 import traceback
-import webbrowser
 
 import click
 import requests
@@ -17,14 +18,21 @@ SUCCESS_STATUS_CODE = 200
 
 
 class UIAvailableWorker(threading.Thread):
-    def __init__(self, project, open_browser=False):
+    """A thread subclass for Meltano UI workers."""
+
+    def __init__(self, project):
+        """Initialize the `UIAvailableWorker` thread.
+
+        Args:
+            project: The Meltano project.
+        """
         super().__init__()
         self.project = project
-        self.open_browser = open_browser
         self.settings_service = ProjectSettingsService(self.project)
         self._terminate = False
 
-    def run(self):
+    def run(self) -> None:
+        """Run the thread, and report when the Meltano UI becomes available."""
         url = f"http://localhost:{self.settings_service.get('ui.bind_port')}"
         headers = {"Host": self.settings_service.get("ui.server_name")}
 
@@ -33,8 +41,6 @@ class UIAvailableWorker(threading.Thread):
                 response = requests.get(url, headers=headers)
                 if response.status_code == SUCCESS_STATUS_CODE:
                     click.secho(f"Meltano UI is now available at {url}", fg="green")
-                    if self.open_browser:
-                        webbrowser.open(url)
                     self._terminate = True
             except Exception:
                 logger.debug(
@@ -44,4 +50,5 @@ class UIAvailableWorker(threading.Thread):
             time.sleep(2)
 
     def stop(self):
+        """Stop the thread."""
         self._terminate = True
