91ec59ed0a45c09fc75b155c97aed3be49ad7103
Will Da Silva <will@willdasilva.xyz>
2022-11-17 10:45:28 -0500

    feat: `meltano install --force` (#6995)

--- src/meltano/core/plugin/dbt/base.py
+++ src/meltano/core/plugin/dbt/base.py
@@ -10,6 +10,7 @@ from meltano.core.plugin.error import PluginNotFoundError
 from meltano.core.plugin.project_plugin import ProjectPlugin
 from meltano.core.plugin_install_service import PluginInstallReason
 from meltano.core.plugin_invoker import PluginInvoker
+from meltano.core.project import Project
 from meltano.core.setting_definition import SettingDefinition, SettingKind
 from meltano.core.transform_add_service import TransformAddService
 
@@ -17,26 +18,55 @@ logger = logging.getLogger(__name__)
 
 
 class DbtInvoker(PluginInvoker):
+    """dbt plugin invoker."""
+
     def Popen_options(self):  # noqa: N802
+        """Get options for `subprocess.Popen`.
+
+        Returns:
+            Mapping of keyword arguments for `subprocess.Popen`.
+        """
         return {**super().Popen_options(), "cwd": self.plugin_config["project_dir"]}
 
 
 class DbtPlugin(BasePlugin):
+    """dbt plugin."""
+
     __plugin_type__ = PluginType.TRANSFORMERS
 
     invoker_class = DbtInvoker
 
 
 class DbtTransformPluginInstaller:
-    def __init__(self, project, plugin: ProjectPlugin):
+    """Plugin installer for dbt transforms."""
+
+    def __init__(self, project: Project, plugin: ProjectPlugin):
+        """Initialize the `DbtTransformPluginInstaller`.
+
+        Args:
+            project: The Meltano project.
+            plugin: The plugin to install into the specified project.
+        """
         self.project = project
         self.plugin = plugin
 
-    async def install(self, reason, clean):
-        """Install the transform into the project."""
+    async def install(
+        self, reason: PluginInstallReason, clean: bool = False, force: bool = False
+    ) -> None:
+        """Install the transform into the project.
+
+        Args:
+            reason: Install reason.
+            clean: Ignored. Present for conformity with `PipPluginInstaller`.
+            force: Ignored. Present for conformity with `PipPluginInstaller`.
+
+        Raises:
+            PluginInstallError: The plugin failed to install.
+        """
         if reason not in {PluginInstallReason.ADD, PluginInstallReason.UPGRADE}:
             logger.info(
-                f"Run `meltano add transform {self.plugin.name}` to re-add this transform to your dbt project."
+                f"Run `meltano add transform {self.plugin.name}` "
+                "to re-add this transform to your dbt project."
             )
             return
 
@@ -58,18 +88,22 @@ class DbtTransformPluginInstaller:
             )
             logger.info(f"Adding dbt model to '{dbt_project_path}'...")
             transform_add_service.update_dbt_project(self.plugin)
-        except PluginNotFoundError:
+        except PluginNotFoundError as ex:
             raise PluginInstallError(
-                "Transformer 'dbt' is not installed. Run `meltano add transformer dbt` to add it to your project."
-            )
-        except FileNotFoundError as err:
-            relative_path = Path(err.filename).relative_to(self.project.root)
+                "Transformer 'dbt' is not installed. "
+                "Run `meltano add transformer dbt` to add it to your project."
+            ) from ex
+        except FileNotFoundError as ex:
+            relative_path = Path(ex.filename).relative_to(self.project.root)
             raise PluginInstallError(
-                f"File {relative_path}' could not be found. Run `meltano add files files-dbt` to set up a dbt project."
-            )
+                f"File '{relative_path}' could not be found. "
+                "Run `meltano add files files-dbt` to set up a dbt project."
+            ) from ex
 
 
 class DbtTransformPlugin(BasePlugin):
+    """Plugin type for dbt transforms."""
+
     __plugin_type__ = PluginType.TRANSFORMS
 
     installer_class = DbtTransformPluginInstaller
@@ -79,5 +113,10 @@ class DbtTransformPlugin(BasePlugin):
         SettingDefinition(name="_vars", kind=SettingKind.OBJECT, value={}),
     ]
 
-    def is_invokable(self):
+    def is_invokable(self) -> bool:
+        """Return whether the plugin is invokable.
+
+        Returns:
+            Whether the plugin is invokable.
+        """
         return False
