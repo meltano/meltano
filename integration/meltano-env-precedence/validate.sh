154f1c6cf4a5051d07eea95f52f1f817c4112c30
Will Da Silva <will@willdasilva.xyz>
2022-11-29 11:11:39 -0500

    docs: Clarify env precedence example with echo command
    Closes #7031
    
    Co-authored-by: AJ Steers <aj@meltano.com>

--- integration/meltano-env-precedence/validate.sh
+++ integration/meltano-env-precedence/validate.sh
@@ -9,9 +9,13 @@ set +o pipefail
 
 export STACKED=1
 
+meltano install utility example
+
 # FIXME: https://github.com/meltano/meltano/issues/7023
-meltano invoke --print-var STACKED example 2>&1 | grep 'STACKED=4' > /dev/null
+[ "$(meltano invoke --print-var STACKED example:echo-stacked)" = $'STACKED=4\nStacked env var value is: 4' ]
 
-meltano --environment prod invoke --print-var STACKED example 2>&1 | grep 'STACKED=12345' > /dev/null
+[ "$(meltano --environment prod invoke --print-var STACKED example:echo-stacked)" = $'STACKED=12345\nStacked env var value is: 12345' ]
 
-meltano install utility example 2>&1 | grep 'env-var-in-pip-url-example-124' > /dev/null
+# FIXME: https://github.com/meltano/meltano/issues/7034
+[ "$(meltano invoke example:echo-config)" = 'Config value is: ${STACKED}4' ]
+[ "$(meltano --environment prod invoke example:echo-config)" = 'Config value is: ${STACKED}5' ]
