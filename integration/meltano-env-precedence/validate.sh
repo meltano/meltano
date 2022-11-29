cb7819341cdd57c3b1592b908f7d259381b0df32
Will Da Silva <will@willdasilva.xyz>
2022-11-29 11:16:20 -0500

    Ignore shellcheck failure

--- integration/meltano-env-precedence/validate.sh
+++ integration/meltano-env-precedence/validate.sh
@@ -17,5 +17,7 @@ meltano install utility example
 [ "$(meltano --environment prod invoke --print-var STACKED example:echo-stacked)" = $'STACKED=12345\nStacked env var value is: 12345' ]
 
 # FIXME: https://github.com/meltano/meltano/issues/7034
+# shellcheck disable=SC2016
 [ "$(meltano invoke example:echo-config)" = 'Config value is: ${STACKED}4' ]
+# shellcheck disable=SC2016
 [ "$(meltano --environment prod invoke example:echo-config)" = 'Config value is: ${STACKED}5' ]
