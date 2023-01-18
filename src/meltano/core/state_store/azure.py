223be61581481a18d24412b8d50f80eeb759a10a
Cody J. Hanson <cody@meltano.com>
2023-01-18 10:39:43 -0600

    Update src/meltano/core/state_store/azure.py
    Co-authored-by: Will Da Silva <will@willdasilva.xyz>

--- src/meltano/core/state_store/azure.py
+++ src/meltano/core/state_store/azure.py
@@ -2,9 +2,13 @@
 from __future__ import annotations
 
 import re
+import sys
 from contextlib import contextmanager
 
-from cached_property import cached_property  # type: ignore
+if sys.version_info >= (3, 8):
+    from functools import cached_property
+else:
+    from cached_property import cached_property
 
 from meltano.core.state_store.filesystem import BaseFilesystemStateStoreManager
 
