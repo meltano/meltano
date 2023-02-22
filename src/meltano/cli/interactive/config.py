3e5686556584e463cf6de25397d258a0e9d275a0
Will Da Silva <will@willdasilva.xyz>
2023-02-22 10:44:26 -0500

    Fix import error on Windows

--- src/meltano/cli/interactive/config.py
+++ src/meltano/cli/interactive/config.py
@@ -2,10 +2,13 @@
 
 from __future__ import annotations
 
+from contextlib import suppress
+
 # NOTE: Importing the readline module enables the use of arrow
 #       keys for text navigation during interactive config.
 #       Refer to https://docs.python.org/3/library/readline.html
-import readline  # noqa: F401
+with suppress(ImportError):
+    import readline  # noqa: F401
 
 import click
 from jinja2 import BaseLoader, Environment
