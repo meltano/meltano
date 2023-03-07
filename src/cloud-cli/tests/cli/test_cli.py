13d08eb910b47a5cd34f129d98c9ff76acf9400a
Cody J. Hanson <cody@meltano.com>
2023-03-07 17:05:23 -0600

    PR feedback

--- src/cloud-cli/tests/cli/test_cli.py
+++ src/cloud-cli/tests/cli/test_cli.py
@@ -1,13 +1,11 @@
 from __future__ import annotations
 
-from pathlib import Path
-
 from click.testing import CliRunner
 
 from meltano.cloud.cli import cloud as cli
 
 
-def test_cli(tmp_path: Path):
+def test_cli():
     result = CliRunner().invoke(cli)
     assert "Interface with Meltano Cloud" in result.stdout
     assert result.exit_code == 0
