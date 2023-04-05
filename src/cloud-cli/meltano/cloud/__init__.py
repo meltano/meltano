c752d5405977ccf66f40e506b19052ad854bdc8d
Will Da Silva <will@willdasilva.xyz>
2023-04-05 10:00:15 -0400

    fix: Cloud CLI version option
    Closes #7495

--- src/cloud-cli/meltano/cloud/__init__.py
+++ src/cloud-cli/meltano/cloud/__init__.py
@@ -1,3 +1,15 @@
 """Meltano Cloud."""
 
 from __future__ import annotations
+
+import sys
+
+if sys.version_info >= (3, 8):
+    from importlib import metadata as importlib_metadata
+else:
+    import importlib_metadata
+
+try:
+    __version__ = importlib_metadata.version("meltano-cloud-cli")
+except importlib_metadata.PackageNotFoundError:
+    __version__ = importlib_metadata.version("meltano")
