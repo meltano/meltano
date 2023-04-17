2448ae9640fa4af8f35597a1eb0ec20c0e9b90dd
Edgar R. M <edgar@meltano.com>
2023-04-17 09:44:02 -0600

    Update src/meltano/cli/docs.py

--- src/meltano/cli/docs.py
+++ src/meltano/cli/docs.py
@@ -12,5 +12,5 @@ from meltano.cli.utils import InstrumentedCmd
 )
 def docs():
     """Open the Meltano docs in the system browser."""
-    click.launch("https://docs.meltano.com/")
+    click.launch("https://docs.meltano.com/")  # pragma: no cover
     click.secho("Opening the docs...", fg="green")
