285d0982f185e5e6f61a447a720f23309352e251
pnadolny13 <patnadolny@gmail.com>
2023-05-04 11:46:41 -0400

    fix flake error for docs

--- src/meltano/core/block/parser.py
+++ src/meltano/core/block/parser.py
@@ -138,6 +138,11 @@ class BlockParser:  # noqa: D101
 
     @property
     def plugins(self) -> list[ProjectPlugin]:
+        """Return the list of plugins in the block.
+
+        Returns:
+            A list of ProjectPlugin.
+        """
         return self._plugins
 
     def _expand_jobs(self, blocks: list[str], task_sets: TaskSetsService) -> list[str]:
