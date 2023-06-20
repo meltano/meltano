5f8f9a18392a6a1217a083b7cb08b67a73cf74ec
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2023-06-20 13:16:51 -0600

    Check .env

--- integration/meltano-state-s3/validate.sh
+++ integration/meltano-state-s3/validate.sh
@@ -4,6 +4,11 @@ set -euo pipefail
 
 source "$(git rev-parse --show-toplevel)/integration/commons.sh"
 cd "${TEST_DOCS_DIR}"
+
+# Check config in .env
+grep "MELTANO_STATE_BACKEND_URI" < .env
+grep "MELTANO_STATE_BACKEND_S3_ENDPOINT_URL" < .env
+
 meltano state list | grep "dev:tap-gitlab-to-target-jsonl"
 meltano state get dev:tap-gitlab-to-target-jsonl | grep "singer_state"
 
