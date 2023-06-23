79e00912136892d3637d28fb54d16c2379f7aa86
Braedon Leonard <braedonleonard@gmail.com>
2023-06-23 14:19:41 -0400

    Handle @once intervals in "meltano cloud schedule list"

--- src/cloud-cli/meltano/cloud/cli/schedule.py
+++ src/cloud-cli/meltano/cloud/cli/schedule.py
@@ -268,6 +268,8 @@ def _approx_daily_freq(
     sample_period: timedelta = timedelta_year,
     num_digits_precision: int = 1,
 ) -> str:
+    if cron_expr == "@once":
+        return 0
     now = datetime.now(timezone.utc)
     num_runs = sum(1 for _ in croniter_range(now, now + sample_period, cron_expr))
     freq = round(num_runs / sample_period.days, num_digits_precision)
