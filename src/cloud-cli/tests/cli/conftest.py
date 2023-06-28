acb70950a4d5857a6df1488b68a3e42d84b817cd
Braedon Leonard <braedonleonard@gmail.com>
2023-06-28 11:08:13 -0400

    Make authenticated requests without an explicit check for login
    When you have an access and id token, requests are made assuming they aren't out of date. On 403, login is triggered and the request is retried automatically

--- src/cloud-cli/tests/cli/conftest.py
+++ src/cloud-cli/tests/cli/conftest.py
@@ -11,18 +11,6 @@ if t.TYPE_CHECKING:
 
 
 @pytest.fixture()
-def tenant_resource_key():
-    """Return a fake tenant resource key."""
-    return "meltano-cloud-test"
-
-
-@pytest.fixture()
-def internal_project_id():
-    """Return a fake Cloud/internal project ID."""
-    return "pytest-123"
-
-
-@pytest.fixture()
 def client(config: MeltanoCloudConfig) -> MeltanoCloudClient:
     """Return a MeltanoCloudClient instance with fake credentials."""
     return MeltanoCloudClient(config=config)
