23978bba77458c3d8cd981f6ba5fd396bb32e260
Igor Abdulović <igorabdul@gmail.com>
2023-07-01 00:27:03 +0200

    toggle jump fix

--- docs/src/theme/ColorModeToggle/index.js
+++ docs/src/theme/ColorModeToggle/index.js
@@ -1,38 +1,12 @@
 import React from "react";
 import clsx from "clsx";
-import useIsBrowser from "@docusaurus/useIsBrowser";
-import { translate } from "@docusaurus/Translate";
-import IconLightMode from "@theme/Icon/LightMode";
-import IconDarkMode from "@theme/Icon/DarkMode";
 import styles from "./styles.module.css";
 function ColorModeToggle({ className, buttonClassName, value, onChange }) {
-  const isBrowser = useIsBrowser();
-  const title = translate(
-    {
-      message: "Switch between dark and light mode (currently {mode})",
-      id: "theme.colorToggle.ariaLabel",
-      description: "The ARIA label for the navbar color mode toggle",
-    },
-    {
-      mode:
-        value === "dark"
-          ? translate({
-              message: "dark mode",
-              id: "theme.colorToggle.ariaLabel.mode.dark",
-              description: "The name for the dark color mode",
-            })
-          : translate({
-              message: "light mode",
-              id: "theme.colorToggle.ariaLabel.mode.light",
-              description: "The name for the light color mode",
-            }),
-    }
-  );
   return (
     <div
       className={clsx(styles.toggle, className, buttonClassName, "relative")}
     >
-      <label className="light-dark-toggle">
+      <label className={styles.lightDarkToggle}>
         <input
           className={styles.toggleCheckbox}
           type="checkbox"
@@ -151,26 +125,6 @@ function ColorModeToggle({ className, buttonClassName, value, onChange }) {
           </div>
         </div>
       </label>
-      {/* <button
-        className={clsx(
-          'clean-btn',
-          styles.toggleButton,
-          !isBrowser && styles.toggleButtonDisabled,
-          buttonClassName,
-        )}
-        type="button"
-        onClick={() => onChange(value === 'dark' ? 'light' : 'dark')}
-        disabled={!isBrowser}
-        title={title}
-        aria-label={title}
-        aria-live="polite">
-        <IconLightMode
-          className={clsx(styles.toggleIcon, styles.lightToggleIcon)}
-        />
-        <IconDarkMode
-          className={clsx(styles.toggleIcon, styles.darkToggleIcon)}
-        />
-      </button> */}
     </div>
   );
 }
