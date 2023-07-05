ad21f0c95ece1a86a45a871af5cea557108abbb8
Igor Abdulović <igorabdul@gmail.com>
2023-07-05 20:43:22 +0200

    Revert "search css update"
    This reverts commit 975e9522c85d4bbc6ae9b32f265d6a65b30a2409.

--- docs/src/theme/Navbar/Logo/index.js
+++ docs/src/theme/Navbar/Logo/index.js
@@ -1,16 +1,15 @@
 import React from "react";
-import clsx from "clsx";
 import Link from "@docusaurus/Link";
 import styles from "./styles.module.css";
 
 export default function NavbarLogo() {
   return (
-    <div className={clsx(styles.navbarBrand, "navbar__brand")}>
+    <div className="navbar__brand">
       <a href="https://meltano.com/">
         <div className="navbar__logo" />
       </a>
       <Link className="navbar__brand" href="/">
-        <b className={clsx("navbar__title text--truncate", styles.docsLink)}>
+        <b className={"navbar__title text--truncate " + styles.docsLink}>
           Docs
         </b>
       </Link>
