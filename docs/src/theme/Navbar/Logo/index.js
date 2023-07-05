975e9522c85d4bbc6ae9b32f265d6a65b30a2409
Igor Abdulović <igorabdul@gmail.com>
2023-07-05 17:50:26 +0200

    search css update

--- docs/src/theme/Navbar/Logo/index.js
+++ docs/src/theme/Navbar/Logo/index.js
@@ -1,15 +1,16 @@
 import React from "react";
+import clsx from "clsx";
 import Link from "@docusaurus/Link";
 import styles from "./styles.module.css";
 
 export default function NavbarLogo() {
   return (
-    <div className="navbar__brand">
+    <div className={clsx(styles.navbarBrand, "navbar__brand")}>
       <a href="https://meltano.com/">
         <div className="navbar__logo" />
       </a>
       <Link className="navbar__brand" href="/">
-        <b className={"navbar__title text--truncate " + styles.docsLink}>
+        <b className={clsx("navbar__title text--truncate", styles.docsLink)}>
           Docs
         </b>
       </Link>
