451fb80c2827ff565823e23c56e798040bd3578d
Taylor A. Murphy <taylor@meltano.com>
2023-07-25 09:59:56 -0500

    docs: quick doc fixes (#7944)

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -35,7 +35,7 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/meltano/meltano/blob/docs',
+          editUrl: 'https://github.com/meltano/meltano/blob/main/docs',
         },
         blog: {
           routeBasePath: '/changelog',
