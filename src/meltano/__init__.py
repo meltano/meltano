e3962b86cfdb86a4de0d8a7e3caf075176938d06
Will Da Silva <will@willdasilva.xyz>
2023-08-01 16:00:02 -0400

    Use default event loop policy
    The default was changed in Python 3.8 to be `ProactorEventLoop` on Windows, which is now the earliest version Meltano supports.

--- src/meltano/__init__.py
+++ src/meltano/__init__.py
@@ -1,15 +1,6 @@
 """Meltano."""
 
-
 from __future__ import annotations
 
 # Managed by commitizen
 __version__ = "2.20.0"
-
-import asyncio
-import platform
-
-if platform.system() == "Windows":
-    asyncio.set_event_loop_policy(
-        asyncio.WindowsProactorEventLoopPolicy(),  # type: ignore[attr-defined]
-    )
