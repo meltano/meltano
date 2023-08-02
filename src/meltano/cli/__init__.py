f13e683e95dd917fbfb115f887a0a281be9c8f3d
Will Da Silva <will@willdasilva.xyz>
2023-08-02 13:53:14 -0400

    feat: Add `meltano hub ping` command
    Closes #7899

--- src/meltano/cli/__init__.py
+++ src/meltano/cli/__init__.py
@@ -14,6 +14,7 @@ from meltano.cli import (  # noqa: WPS235
     dragon,
     elt,
     environment,
+    hub,
     initialize,
     install,
     invoke,
@@ -46,6 +47,7 @@ cli.add_command(docs.docs)
 cli.add_command(dragon.dragon)
 cli.add_command(elt.elt)
 cli.add_command(environment.meltano_environment)
+cli.add_command(hub.hub)
 cli.add_command(initialize.init)
 cli.add_command(install.install)
 cli.add_command(invoke.invoke)
