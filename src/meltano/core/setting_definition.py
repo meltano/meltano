e773227e5cd45e7d80f25a8e8ebec559989d17e8
Edgar Ramírez Mondragón <edgar@meltano.com>
2023-11-06 16:48:22 -0600

    fix(core): Validate input to settings with `kind: options` (#8227)
    * Add failing test
    
    * Parametrize test
    
    * Revert "Parametrize test"
    
    This reverts commit c4a9428329401a069bd840603f0481427b3dd4de.
    
    * Options is always a list of dicts
    
    * Validate options value
    
    * Accept null
    
    * option -> choice

--- src/meltano/core/setting_definition.py
+++ src/meltano/core/setting_definition.py
@@ -424,6 +424,9 @@ class SettingDefinition(NameEq, Canonical):
 
         Returns:
             Value cast according to specified setting definition kind.
+
+        Raises:
+            ValueError: If value is not of the expected type.
         """
         value = value.isoformat() if isinstance(value, (date, datetime)) else value
 
@@ -440,6 +443,14 @@ class SettingDefinition(NameEq, Canonical):
                 value = list(
                     self._parse_value(value, "array", Sequence),  # type: ignore
                 )
+
+        if (
+            value is not None
+            and self.kind == SettingKind.OPTIONS
+            and all(opt["value"] != value for opt in self.options)
+        ):
+            error_message = f"'{value}' is not a valid choice for '{self.name}'"
+            raise ValueError(error_message)
 
         processor = self.value_processor
         if value is not None and processor:
