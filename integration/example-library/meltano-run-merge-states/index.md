f15f19925aded3f2e7c8a518c3dbf2d4e30892d6
Edgar Ramírez Mondragón <edgar@meltano.com>
2023-11-10 19:12:11 -0600

    Add integration test

---
+++ integration/example-library/meltano-run-merge-states/index.md
@@ -0,0 +1,59 @@
+# Get setup
+
+This example shows how state from sequential invocations of `meltano run` can be merged together to create a single state file.
+
+This is useful for when you want to backfill/refresh a single stream, without losing the state of other streams.
+
+```shell
+meltano install
+```
+
+## Without state file merging (default)
+
+### Extract all streams
+
+```shell
+TAP_WITH_STATE_TS='2023-01-01T00:00:00Z' \
+meltano run tap-with-state target-jsonl --state-id-suffix=no-merge
+```
+
+### Extract a single stream
+
+Run a 'full refresh' pipeline of a single stream.
+
+```shell
+TAP_WITH_STATE_TS='2023-01-01T01:00:00Z' \
+TAP_WITH_STATE__SELECT_FILTER='["stream_1"]' \
+meltano run tap-with-state target-jsonl --full-refresh --state-id-suffix=no-merge
+```
+
+Note that the state will only contain the bookmark for `stream_1`.
+
+```shell
+meltano state get dev:tap-with-state-to-target-jsonl:no-merge
+```
+
+## With state file merging
+
+### Extract all streams
+
+```shell
+TAP_WITH_STATE_TS='2023-01-01T00:00:00Z' \
+meltano run tap-with-state target-jsonl --state-id-suffix=merge
+```
+
+### Filter a single stream, merging states
+
+Run a 'full refresh' pipeline of a single stream, but merge the state with the existing state.
+
+```shell
+TAP_WITH_STATE_TS='2023-01-01T01:00:00Z' \
+TAP_WITH_STATE__SELECT_FILTER='["stream_1"]' \
+meltano run tap-with-state target-jsonl --full-refresh --state-id-suffix=merge --merge-state
+```
+
+Note that the state will now contain both the new bookmark for `stream_1` and the old bookmarks for the other streams.
+
+```shell
+meltano state get dev:tap-with-state-to-target-jsonl:merge
+```
---
+++ integration/example-library/meltano-run-merge-states/tap.py
@@ -0,0 +1,131 @@
+"""Simple Singer tap."""
+
+#!/usr/bin/env python
+
+# flake8: noqa
+
+from __future__ import annotations
+
+import argparse
+import datetime
+import json
+import sys
+
+CATALOG = {
+    "streams": [
+        {
+            "tap_stream_id": f"stream_{i}",
+            "schema": {
+                "properties": {
+                    "id": {"type": "integer"},
+                    "created_at": {"type": "string", "format": "date-time"},
+                },
+            },
+            "metadata": [
+                {
+                    "breadcrumb": [],
+                    "metadata": {
+                        "selected": True,
+                    },
+                },
+            ],
+        }
+        for i in (1, 2, 3)
+    ],
+}
+
+
+def sync_stream(stream_id: str, timestamp: str):
+    """Sync a stream."""
+    sys.stdout.write(
+        json.dumps(
+            {
+                "type": "SCHEMA",
+                "stream": stream_id,
+                "schema": {
+                    "properties": {
+                        "id": {"type": "integer"},
+                        "created_at": {"type": "string", "format": "date-time"},
+                    },
+                },
+                "key_properties": ["id"],
+            },
+        )
+        + "\n",
+    )
+    sys.stdout.write(
+        json.dumps(
+            {
+                "type": "RECORD",
+                "stream": stream_id,
+                "record": {
+                    "id": 1,
+                    "created_at": timestamp,
+                },
+            },
+        )
+        + "\n",
+    )
+
+
+def is_selected(stream_id: str, catalog: dict):
+    """Check if a stream is selected."""
+    for stream in catalog["streams"]:
+        if stream["tap_stream_id"] != stream_id:
+            continue
+        metadata = stream.get("metadata", [])
+        stream_metadata = next(
+            filter(lambda m: m.get("breadcrumb") == [], metadata),
+            {},
+        )
+        print(f"stream_metadata: {stream_metadata}", file=sys.stderr)
+        return stream_metadata.get("metadata", {}).get("selected", False)
+
+    return False
+
+
+def sync(config: dict, catalog: dict, state: dict):
+    """Sync data."""
+    print(f"catalog: {catalog}", file=sys.stderr)
+    timestamp = config.get(
+        "ts",
+        datetime.datetime.now(tz=datetime.timezone.utc).isoformat(),
+    )
+    state = {"bookmarks": {}}
+
+    for i in (1, 2, 3):
+        if not is_selected(f"stream_{i}", catalog):
+            continue
+        sync_stream(f"stream_{i}", timestamp)
+        state["bookmarks"][f"stream_{i}"] = {"created_at": timestamp}
+
+    sys.stdout.write(json.dumps({"type": "STATE", "value": state}))
+
+
+def discover():
+    """Discover catalog."""
+    sys.stdout.write(json.dumps(CATALOG))
+
+
+def main():
+    """Main entrypoint."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", type=argparse.FileType("r"))
+    parser.add_argument("--catalog", type=argparse.FileType("r"))
+    parser.add_argument("--state", type=argparse.FileType("r"))
+    parser.add_argument("--discover", action="store_true")
+    args = parser.parse_args()
+
+    if args.discover:
+        discover()
+        return
+
+    config = json.load(args.config) if args.config else {}
+    catalog = json.load(args.catalog) if args.catalog else {}
+    state = json.load(args.state) if args.state else {}
+
+    sync(config, catalog, state)
+
+
+if __name__ == "__main__":
+    main()
