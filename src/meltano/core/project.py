d758aab4b0f331b76d737466bb8bd5d6d29406d8
Edgar Ramírez Mondragón <edgar@meltano.com>
2024-01-05 19:17:12 -0600

    feat: Custom `.env` path

--- src/meltano/core/project.py
+++ src/meltano/core/project.py
@@ -82,6 +82,7 @@ class Project(Versioned):
         environment: Environment | None = None,
         *,
         readonly: bool = False,
+        dotenv_file: Path | None = None,
     ):
         """Initialize a `Project` instance.
 
@@ -89,6 +90,7 @@ class Project(Versioned):
             root: The root directory of the project.
             environment: The active Meltano environment.
             readonly: Whether the project is in read-only mode.
+            dotenv_file: The path to the .env file to use.
         """
         self.root = Path(root).resolve()
         self.environment: Environment | None = environment
@@ -96,6 +98,7 @@ class Project(Versioned):
         self.sys_dir_root = Path(
             os.getenv(PROJECT_SYS_DIR_ROOT_ENV, self.root / ".meltano"),
         ).resolve()
+        self.dotenv_file = dotenv_file
 
     def refresh(self, **kwargs) -> None:  # noqa: ANN003
         """Refresh the project instance to reflect external changes.
@@ -258,7 +261,13 @@ class Project(Versioned):
 
     @classmethod
     @fasteners.locked(lock="_find_lock")
-    def find(cls, project_root: Path | str | None = None, *, activate=True):  # noqa: ANN001, ANN206
+    def find(
+        cls,
+        project_root: Path | str | None = None,
+        *,
+        activate: bool = True,
+        dotenv_file: Path | None = None,
+    ) -> Project:
         """Find a Project.
 
         Args:
@@ -267,6 +276,7 @@ class Project(Versioned):
                 directory and it's parents.
             activate: Save the found project so that future calls to `find`
                 will continue to use this project.
+            dotenv_file: The path to the .env file to use.
 
         Returns:
             the found project
@@ -282,12 +292,12 @@ class Project(Versioned):
         readonly = truthy(os.getenv(PROJECT_READONLY_ENV, "false"))
 
         if project_root := project_root or os.getenv(PROJECT_ROOT_ENV):
-            project = Project(project_root, readonly=readonly)
+            project = Project(project_root, readonly=readonly, dotenv_file=dotenv_file)
             if not project.meltanofile.exists():
                 raise ProjectNotFound(project)
         else:
             for directory in walk_parent_directories():
-                project = Project(directory, readonly=readonly)
+                project = Project(directory, readonly=readonly, dotenv_file=dotenv_file)
                 if project.meltanofile.exists():
                     break
             if not project.meltanofile.exists():
@@ -377,7 +387,13 @@ class Project(Versioned):
         Returns:
             the path to this project's .env file
         """
-        return self.root.joinpath(".env")
+        if self.dotenv_file and self.dotenv_file.is_file():
+            return (
+                self.dotenv_file
+                if self.dotenv_file.is_absolute()
+                else self.root.joinpath(self.dotenv_file)
+            )
+        return self.dotenv_file or self.root.joinpath(".env")
 
     @cached_property
     def dotenv_env(self) -> dict[str, str | None]:
--- tests/meltano/cli/test_cli.py
+++ tests/meltano/cli/test_cli.py
@@ -291,6 +291,45 @@ class TestCli:
             assert_cli_runner(cli_runner.invoke(cli, ("--cwd", str(dirpath), "dragon")))
             assert Path().resolve() == dirpath
 
+    def test_env_file_option(
+        self,
+        cli_runner: MeltanoCliRunner,
+        test_cli_project: Project,
+        tmp_path: Path,
+    ):
+        project = test_cli_project
+        with cd(project.root_dir()):
+            dotenv_path = tmp_path / ".env"
+            dotenv_path.write_text("MELTANO_DEFAULT_ENVIRONMENT=custom\n")
+            result = cli_runner.invoke(
+                cli,
+                (
+                    "--env-file",
+                    str(dotenv_path),
+                    "config",
+                    "--format=env",
+                    "meltano",
+                ),
+            )
+            assert result.exit_code == 0
+            assert "MELTANO_DEFAULT_ENVIRONMENT='custom'" in result.output
+
+        with cd(project.root_dir()):
+            dotenv_path = project.root.joinpath("prod.env")
+            dotenv_path.write_text("MELTANO_DEFAULT_ENVIRONMENT=custom\n")
+            result = cli_runner.invoke(
+                cli,
+                (
+                    "--env-file",
+                    "prod.env",
+                    "config",
+                    "--format=env",
+                    "meltano",
+                ),
+            )
+            assert result.exit_code == 0
+            assert "MELTANO_DEFAULT_ENVIRONMENT='custom'" in result.output
+
     @pytest.mark.parametrize(
         "command_args",
         (
