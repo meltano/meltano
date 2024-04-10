8bd86e3674fa52a0774af7843d795fbb8fdd6f52
Edgar Ramírez-Mondragón <edgar@meltano.com>
2024-04-10 14:22:27 -0600

    Add a copyright notice to the `UvVenvService._find_uv` function

--- src/meltano/core/venv_service.py
+++ src/meltano/core/venv_service.py
@@ -564,6 +564,20 @@ class UvVenvService(VenvService):
 
         Adapted from https://github.com/wntrblm/nox/blob/55c7eaf2eb03feb4a4b79e74966c542b75d61401/nox/virtualenv.py#L42-L54.
 
+        Copyright 2016 Alethea Katherine Flowers
+
+        Licensed under the Apache License, Version 2.0 (the "License");
+        you may not use this file except in compliance with the License.
+        You may obtain a copy of the License at
+
+           http://www.apache.org/licenses/LICENSE-2.0
+
+        Unless required by applicable law or agreed to in writing, software
+        distributed under the License is distributed on an "AS IS" BASIS,
+        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        See the License for the specific language governing permissions and
+        limitations under the License.
+
         Returns:
             A string representing the path to the `uv` executable.
         """
