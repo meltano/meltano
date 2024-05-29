d7c31c8771a1d829ddd241c5de968e01974a138d
Edgar Ramírez Mondragón <16805946+edgarrmondragon@users.noreply.github.com>
2024-05-28 19:09:47 -0600

    Fix typo

--- src/meltano/cli/schedule.py
+++ src/meltano/cli/schedule.py
@@ -215,7 +215,7 @@ def _format_elt_list_output(entry: Schedule, session: Session) -> dict:
     default="text",
 )
 @click.pass_context
-def list_schedulers(ctx: click.Context, list_format: str) -> None:  # noqa: C901
+def list_schedules(ctx: click.Context, list_format: str) -> None:  # noqa: C901
     """List available schedules."""
     project = ctx.obj["project"]
     schedule_service: ScheduleService = ctx.obj["schedule_service"]
