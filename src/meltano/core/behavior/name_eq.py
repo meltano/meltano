f46c47d4871d7492b2f2bbfd8d0c16506a1190fe
Edgar Ramírez-Mondragón <edgar@meltano.com>
2024-07-18 12:33:27 -0600

    Add `__repr__` to `NameEq`

--- src/meltano/core/behavior/name_eq.py
+++ src/meltano/core/behavior/name_eq.py
@@ -2,8 +2,18 @@ from __future__ import annotations
 
 
 class NameEq:
+    name: str
+
     def __eq__(self, other):
         return self is other or self.name == other.name
 
     def __hash__(self):
         return hash(self.name)
+
+    def __repr__(self) -> str:
+        """Return a string representation of the object.
+
+        Returns:
+            A string representation of the object.
+        """
+        return f"{self.__class__.__name__}({self.name!r})"
