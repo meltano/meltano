c4910a525db0577b3faa5e7f66ed77e40f6f78ff
Edgar Ramírez-Mondragón <edgarrm358@gmail.com>
2024-08-01 22:05:35 -0600

    Remove `format_exc_info` and make pretty exceptions work

--- src/meltano/core/logging/utils.py
+++ src/meltano/core/logging/utils.py
@@ -150,7 +150,6 @@ def setup_logging(
             structlog.stdlib.PositionalArgumentsFormatter(),
             TIMESTAMPER,
             structlog.processors.StackInfoRenderer(),
-            structlog.processors.format_exc_info,
             structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
         ],
         logger_factory=structlog.stdlib.LoggerFactory(),
