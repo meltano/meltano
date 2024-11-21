51c88b7d1e447496aa8bcd12f38d05c4049d891a
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2024-11-21 13:46:47 -0600

    chore: Set up Google gtag for docs site

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -50,6 +50,10 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        gtag: {
+          trackingID: ['G-Z3RR2S48WN'],
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
