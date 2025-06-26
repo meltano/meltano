783e0f28939d7ae59e7afc51b5949c7c987837ea
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2025-06-26 00:29:37 -0600

    Make protocol tests slightly more readable

--- tests/meltano/core/protocols/test_el_context.py
+++ tests/meltano/core/protocols/test_el_context.py
@@ -1,6 +1,9 @@
+# ruff: noqa: FBT001, FBT003
+
 from __future__ import annotations
 
 import typing as t
+from dataclasses import dataclass
 
 from meltano.core._protocols.el_context import ELContextProtocol
 from meltano.core._state import StateStrategy
@@ -9,51 +12,31 @@ if t.TYPE_CHECKING:
     from pytest_subtests import SubTests
 
 
+@dataclass
 class MyContext(ELContextProtocol):
-    def __init__(
-        self,
-        *,
-        full_refresh: bool | None,
-        state_strategy: StateStrategy,
-    ):
-        self.full_refresh = full_refresh
-        self.state_strategy = state_strategy
+    full_refresh: bool | None
+    state_strategy: StateStrategy
 
 
 class TestELContextProtocol:
     def test_should_merge_states(self, subtests: SubTests):
+        def _(full_refresh: bool, state_strategy: StateStrategy):
+            return MyContext(full_refresh, state_strategy)
+
         with subtests.test("Full refresh & auto → merge"):
-            assert MyContext(
-                full_refresh=True,
-                state_strategy=StateStrategy.AUTO,
-            ).should_merge_states()
+            assert _(True, StateStrategy.AUTO).should_merge_states()
 
         with subtests.test("Full refresh & merge → merge"):
-            assert MyContext(
-                full_refresh=True,
-                state_strategy=StateStrategy.MERGE,
-            ).should_merge_states()
+            assert _(True, StateStrategy.MERGE).should_merge_states()
 
         with subtests.test("Full refresh & overwrite → overwrite"):
-            assert not MyContext(
-                full_refresh=True,
-                state_strategy=StateStrategy.OVERWRITE,
-            ).should_merge_states()
+            assert not _(True, StateStrategy.OVERWRITE).should_merge_states()
 
         with subtests.test("Incremental & auto → overwrite"):
-            assert not MyContext(
-                full_refresh=False,
-                state_strategy=StateStrategy.AUTO,
-            ).should_merge_states()
+            assert not _(False, StateStrategy.AUTO).should_merge_states()
 
         with subtests.test("Incremental & merge → merge"):
-            assert MyContext(
-                full_refresh=False,
-                state_strategy=StateStrategy.MERGE,
-            ).should_merge_states()
+            assert _(False, StateStrategy.MERGE).should_merge_states()
 
         with subtests.test("Incremental & overwrite → overwrite"):
-            assert not MyContext(
-                full_refresh=False,
-                state_strategy=StateStrategy.OVERWRITE,
-            ).should_merge_states()
+            assert not _(False, StateStrategy.OVERWRITE).should_merge_states()
