18f075178bb1ae223181b31e844c7cb8eb67ae35
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2025-07-09 11:03:40 -0600

    refactor: Replace deprecated `asyncio.iscoroutinefunction` with `inspect.iscoroutinefunction`

--- src/meltano/core/plugin/singer/tap.py
+++ src/meltano/core/plugin/singer/tap.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 import asyncio
+import inspect
 import json
 import logging
 import shutil
@@ -60,7 +61,7 @@ async def _stream_redirect(
     while stream and not stream.at_eof():
         data = await stream.readline()
         for file_like_obj in file_like_objs:
-            if asyncio.iscoroutinefunction(file_like_obj.write):
+            if inspect.iscoroutinefunction(file_like_obj.write):
                 await file_like_obj.write(data.decode(encoding) if write_str else data)
             else:
                 file_like_obj.write(data.decode(encoding) if write_str else data)
