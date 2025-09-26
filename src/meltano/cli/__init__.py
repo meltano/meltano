bfd923df9ff7779c3c5d73daff5acdcb95d30554
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2025-09-26 10:46:41 -0600

    chore: Link to `https://melta.no` in the CLI (#9523)
    <!--
    
    Please, go through these steps when you submit a PR.
    
    1. Make sure your branch is not protected. In particular, avoid making
    PRs from the `main` branch of your fork.
    
    2. Give a descriptive title to your PR. We use semantic titles, and the
    accepted types and scopes are listed in
    https://github.com/meltano/meltano/blob/main/.github/semantic.yml.
    
       A good title should look like this:
    
       ```
    feat(cli): The `meltano run` command now accepts a `--timeout` option to
    limit the time it runs
       ```
    
    3. Provide a description of your changes.
    
    4. Put "Closes #XXXX" in your comment to auto-close the issue that your
    PR fixes (if such).
    
    -->
    
    ## Description
    
    <!-- Describe the changes introduced by this PR -->
    
    SSIA.
    
    ## Related Issues
    
    * NA
    
    ## Summary by Sourcery
    
    Chores:
    - Use HTTPS for the Meltano troubleshooting URL in the CLI error message
    and add a noqa E501 directive to bypass line-length linting

--- src/meltano/cli/__init__.py
+++ src/meltano/cli/__init__.py
@@ -79,9 +79,9 @@ setup_logging()
 logger = structlog.stdlib.get_logger(__name__)
 
 troubleshooting_message = """\
-Need help fixing this problem? Visit http://melta.no/ for troubleshooting steps, or to \
+Need help fixing this problem? Visit https://melta.no/ for troubleshooting steps, or to \
 join our friendly Slack community.
-"""
+"""  # noqa: E501
 
 
 def handle_meltano_error(error: MeltanoError) -> t.NoReturn:
