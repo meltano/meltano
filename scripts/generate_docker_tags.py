cdb49567d616d1afbdf4f8e5203cf64dc726201b
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2026-04-09 20:59:00 -0600

    Make it a PEP 723 script
    Signed-off-by: Edgar Ramírez Mondragón <edgarrm358@gmail.com>

--- scripts/generate_docker_tags.py
+++ scripts/generate_docker_tags.py
@@ -1,4 +1,10 @@
-#!/usr/bin/env python3
+#!/usr/bin/env -S uv run --script
+
+# /// script
+# dependencies = [
+#   "packaging>=26",
+# ]
+# ///
 
 """Script to generate docker tags for Meltano images.
 
