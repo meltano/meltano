e21aeea80a291e11eae59d54d7c4208a2878f722
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2026-06-28 12:25:02 -0600

    test: Mock aws configuration
    Signed-off-by: Edgar Ramírez Mondragón <edgarrm358@gmail.com>

--- tests/meltano/core/state_store/test_state_store.py
+++ tests/meltano/core/state_store/test_state_store.py
@@ -329,7 +329,13 @@ class TestGCSStateBackend:
 class TestS3StateBackend:
     @pytest.fixture(autouse=True)
     def clean_env(self, monkeypatch: pytest.MonkeyPatch):
-        monkeypatch.delenv("AWS_DEFAULT_REGION", raising=False)
+        monkeypatch.setenv("AWS_SHARED_CREDENTIALS_FILE", "/dev/null")
+        monkeypatch.setenv("AWS_CONFIG_FILE", "/dev/null")
+        monkeypatch.setenv("AWS_DEFAULT_REGION", "us-east-1")
+        monkeypatch.setenv("AWS_ACCESS_KEY_ID", "testing")
+        monkeypatch.setenv("AWS_SECRET_ACCESS_KEY", "testing")
+        monkeypatch.setenv("AWS_SECURITY_TOKEN", "testing")
+        monkeypatch.setenv("AWS_SESSION_TOKEN", "testing")
         monkeypatch.delenv("AWS_PROFILE", raising=False)
 
     @pytest.fixture
